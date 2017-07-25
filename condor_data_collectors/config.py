69d6ace0342d6cc1dbeb2170e9e4455d0e1819f2
Colson Driemel <colsond@uvic.ca>
2017-07-25 13:54:50 -0700

    convert config.py to use yaml configuration
    also added yaml copy of config file.

--- condor_data_collectors/config.py
+++ condor_data_collectors/config.py
@@ -1,6 +1,6 @@
 from os.path import join, expanduser, exists, abspath
 import sys
-import ConfigParser
+import yaml
 
 job_collection_interval = 15
 machine_collection_interval = 15
@@ -17,58 +17,63 @@ redis_port = 6379
 redis_db = 0
 redis_password = ""
 
-if exists("/etc/condor_data_collectors.cfg"):
-    path = "/etc/condor_data_collectors.cfg"
+if exists("/etc/condor_data_collectors.yaml"):
+    path = "/etc/condor_data_collectors.yaml"
 
-elif exists("/opt/cloudscheduler/condor_data_collectors/condor_data_collectors.cfg"):
-    path = "/opt/cloudscheduler/condor_data_collectors/condor_data_collectors.cfg"
+elif exists("/opt/cloudscheduler/condor_data_collectors/condor_data_collectors.yaml"):
+    path = "/opt/cloudscheduler/condor_data_collectors/condor_data_collectors.yaml"
 
-config_file = ConfigParser.ConfigParser()
+
 try:
-    config_file.read(path)
-except IOError:
+    with open(path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+except Exception as e:
     print >> sys.stderr, "Configuration file problem: There was a " \
                          "problem reading %s. Check that it is readable," \
                          "and that it exists. " % path
 
-if config_file.has_option("general", "job_collection_interval"):
-    job_collection_interval = int(config_file.get("general", "job_collection_interval"))
-
-if config_file.has_option("general", "machine_collection_interval"):
-    machine_collection_interval = int(config_file.get("general", "machine_collection_interval"))
-
-if config_file.has_option("general", "command_sleep_interval"):
-    command_sleep_interval = int(config_file.get("general", "command_sleep_interval"))
-
-if config_file.has_option("general", "job_commands_key"):
-    job_commands_key = config_file.get("general", "job_commands_key")
-
-if config_file.has_option("general", "collector_commands_key"):
-    collector_commands_key = config_file.get("general", "collector_commands_key")
-
-if config_file.has_option("general", "job_data_key"):
-    job_data_key = config_file.get("general", "job_data_key")
-
-if config_file.has_option("general", "collector_data_key"):
-    collector_data_key = config_file.get("general", "collector_data_key")
-
-if config_file.has_option("general", "job_log_file"):
-    job_log_file = config_file.get("general", "job_log_file")
-
-if config_file.has_option("general", "collector_log_file"):
-    collector_log_file = config_file.get("general", "collector_log_file")
 
 
+if "general" in cfg:
+    if "job_collection_interval" in cfg["general"]:
+        job_collection_interval = cfg["general"]["job_collection_interval"]
 
-if config_file.has_option("redis", "redis_host"):
-    redis_host = config_file.get("redis", "redis_host")
+    if "machine_collection_interval" in cfg["general"]:
+        machine_collection_interval = cfg["general"]["machine_collection_interval"]
 
-if config_file.has_option("redis", "redis_port"):
-    redis_port = config_file.get("redis", "redis_port")
+    if "command_sleep_interval" in cfg["general"]):
+        command_sleep_interval = cfg["general"]["command_sleep_interval"]
 
-if config_file.has_option("redis", "redis_db"):
-    redis_db = config_file.get("redis", "redis_db")
+    if "job_commands_key" in cfg["general"]:
+        job_commands_key = cfg["general"]["job_commands_key"]
 
-if config_file.has_option("redis", "redis_password"):
-    redis_password = config_file.get("redis", "redis_password")
+    if "collector_commands_key" in cfg["general"]:
+        collector_commands_key = cfg["general"]["collector_commands_key"]
+
+    if "job_data_key" in cfg["general"]:
+        job_data_key = cfg["general"]["job_data_key"]
+
+    if "collector_data_key":
+        collector_data_key = cfg["general"]["collector_data_key"]
+
+    if "job_log_file" in cfg["general"]:
+        job_log_file = cfg["general"]["job_log_file"]
+
+    if "collector_log_file" in cfg["general"]:
+        collector_log_file = cfg["general"]["collector_log_file"]
+
+
+if "redis" in cfg:
+    if "redis_host" in cfg["redis"]:
+        redis_host = cfg["redis"]["redis_host"]
+
+    if "redis_port" in cfg["redis"]:
+        redis_port = cfg["redis"]["redis_port"]
+
+    if "redis_db" in cfg["redis"]:
+        redis_db = cfg["redis"]["redis_db"]
+
+    if "redis_password" in cfg["redis"]:
+        redis_password = cfg["redis"]["redis_password"]
 
