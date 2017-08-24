16424919155ebd53395696b7d48d35e2f223c741
Michael Paterson <mhp@uvic.ca>
2017-08-24 10:36:17 -0700

    add cloudmanager.py

---
+++ cloudscheduler/cloudmanager.py
@@ -0,0 +1,22 @@
+import yaml
+import cloudscheduler.openstackcloud
+
+class CloudManager():
+    def __init__(self, name, resource_file):
+        self.name = name
+        self.clouds = {}
+        self.resource_file = resource_file
+
+    def setup(self):
+        try:
+            with open(self.resource_file) as f:
+                cloudresources = yaml.load(f)
+        except Exception as e:
+            print(e)
+        print(type(cloudresources))
+        print(cloudresources)
+        for cloud in cloudresources:
+            # determine type of cloud (would be nice if this could be done from the config keys?
+            newcloud = cloudscheduler.openstackcloud.OpenStackCloud(**cloud)
+            self.clouds[newcloud.name] = newcloud
+
