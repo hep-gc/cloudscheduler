b0fb1db427c7ed4ef361ed6dcd1a8cf663569a36
Colson Driemel <colsond@uvic.ca>
2017-09-27 12:10:54 -0700

    change name of user model import to improve readability

--- web_frontend/cloudscheduler/csv2/views.py
+++ web_frontend/cloudscheduler/csv2/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.contrib.auth.models import User #to get auth_user table
-from .models import user
+from .models import user as csv2_user
 
 # Returns the current authorized user from metadata
 def getAuthUser(request):
@@ -10,17 +10,17 @@ def getAuthUser(request):
 
 def getcsv2User(request):
     authorized_user = getAuthUser(request)
-    csv2_user_list = user.objects.all()
-    for csv2_user in csv2_user_list:
-        if csv2_user.username == authorized_user or csv2_user.cert_dn == authorized_user:
-            return csv2_user
+    csv2_user_list = csv2_user.objects.all()
+    for user in csv2_user_list:
+        if user.username == authorized_user or user.cert_dn == authorized_user:
+            return user
     return 
 
 
 def verifyUser(request):
     auth_user = getAuthUser(request)
     
-    csv2_user_list = user.objects.all()
+    csv2_user_list = csv2_user.objects.all()
     #try to find a user that has "auth_user" as username or cert_dn
     # the uniqueness here will be forced on user creation
     for user in csv2_user_list:
