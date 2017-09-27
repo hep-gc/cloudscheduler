e769f1198af34b4789ecac38c9b441dc91a11dc3
Colson Driemel <colsond@uvic.ca>
2017-09-27 10:27:15 -0700

    add secondary django authentication for user management

--- web_frontend/cloudscheduler/csv2/models.py
+++ web_frontend/cloudscheduler/csv2/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class csv2_user(models.Model):
+    username = models.CharField(max_length=32)
+    cert_dn = models.CharField(max_length=128, default="")   
+    password = models.CharField(max_length=128, default="")
+    join_date = models.DateField()
+
+    def __str__(self):
+        return "%s" % (self.user_name)
--- web_frontend/cloudscheduler/csv2/views.py
+++ web_frontend/cloudscheduler/csv2/views.py
@@ -1,6 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.models import User #to get auth_user table
+from .models import csv2_user
+
+# Returns the current authorized user from metadata
+def getAuthUser(request):
+    return request.META.get('REMOTE_USER')
+
+def getcsv2User(request):
+    authorized_user = getAuthUser(request)
+    auth_user_obj = User.objects.get(username=auth_user)
+    return csv2_user.objects.get(username=auth_user_obj.csv2_user)
+
+
+def verifyUser(request):
+    auth_user = getAuthUser(request)
+    auth_user_obj = User.objects.get(username=auth_user)
+    # check if the authenticated user has a csv2 username in the auth table
+    if auth_user_obj.csv2_user is not null:
+        username = auth_user_obj.csv2_user
+        # Double check to see this is a valid user
+        csv2_user_list = csv2_user.objects.all()
+        for user in csv2_user_list:
+            if user.username == username:
+                return True
+
+    # if they don't then they either are not a csv2 user or this is the first time logging in with a certificate
+    else:
+        csv2_user_list = csv2_user.objects.all()
+        #try to find a user that has "auth_user" as cert_dn
+        for user in csv2_user_list:
+            if user.cert_dn == auth_user:
+                # found a cert DN that matches the one that was used for authentication - assign csv2_user
+                auth_user_obj.csv2_user = user.username
+                auth_user_obj.save()
+                return True
+
+    return False
+
+def getSuperUserStatus(request):
+    authorized_user = getAuthUser(request)
+    return authorized_user.is_superuser
+
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the cloudscheduler v2 index.")
+    if verifyUser(request):
+        csv2_user = getcsv2User(request)
+        return HttpResponse("Hello, %s. You're at the cloudscheduler v2 index." % csv2_user)
