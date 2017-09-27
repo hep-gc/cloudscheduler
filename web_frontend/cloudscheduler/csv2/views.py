f3bedc90f041a3995703559fdb2c6c4be6cefa40
Colson Driemel <colsond@uvic.ca>
2017-09-27 12:02:52 -0700

    revamp second layer of django authentication
    It is not possible in django to append columns to the auth_user table and make them available
    under the 'User' model. This means that we will have to match the authorized user from the
    metadata each time a connection is made to either csv2_user.username or csv2_user.cert_dn.

--- web_frontend/cloudscheduler/csv2/views.py
+++ web_frontend/cloudscheduler/csv2/views.py
@@ -10,32 +10,22 @@ def getAuthUser(request):
 
 def getcsv2User(request):
     authorized_user = getAuthUser(request)
-    auth_user_obj = User.objects.get(username=auth_user)
-    return user.objects.get(username=auth_user_obj.csv2_user)
+    csv2_user_list = user.objects.all()
+    for user in csv2_user_list:
+        if user.username == authorized_user or user.cert_dn == authorized_user:
+            return user
+    return 
 
 
 def verifyUser(request):
     auth_user = getAuthUser(request)
-    auth_user_obj = User.objects.get(username=auth_user)
-    # check if the authenticated user has a csv2 username in the auth table
-    if auth_user_obj.csv2_user is not null:
-        username = auth_user_obj.csv2_user
-        # Double check to see this is a valid user
-        csv2_user_list = user.objects.all()
-        for user in csv2_user_list:
-            if user.username == username:
-                return True
-
-    # if they don't then they either are not a csv2 user or this is the first time logging in with a certificate
-    else:
-        csv2_user_list = user.objects.all()
-        #try to find a user that has "auth_user" as cert_dn
-        for user in csv2_user_list:
-            if user.cert_dn == auth_user:
-                # found a cert DN that matches the one that was used for authentication - assign csv2_user
-                auth_user_obj.csv2_user = user.username
-                auth_user_obj.save()
-                return True
+    
+    csv2_user_list = user.objects.all()
+    #try to find a user that has "auth_user" as username or cert_dn
+    # the uniqueness here will be forced on user creation
+    for user in csv2_user_list:
+        if user.username == auth_user or user.cert_dn == auth_user:
+            return True
 
     return False
 
@@ -48,4 +38,4 @@ def getSuperUserStatus(request):
 def index(request):
     if verifyUser(request):
         csv2_user = getcsv2User(request)
-        return HttpResponse("Hello, %s. You're at the cloudscheduler v2 index." % csv2_user)
+        return HttpResponse("Hello, %s. You're at the cloudscheduler v2 index." % csv2_user.username)
