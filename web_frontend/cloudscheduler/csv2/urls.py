e19b9a30ae3b9df3335e21dbcb6c2ec3665ac077
Colson Driemel <colsond@uvic.ca>
2017-09-29 14:45:20 -0700

    add url handler for user delete

--- web_frontend/cloudscheduler/csv2/urls.py
+++ web_frontend/cloudscheduler/csv2/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^manage_users/', views.manage_users, name='manage_users'),
     url(r'^create_user/', views.create_user, name='create_user'),
     url(r'^update_user/', views.update_user, name='update_user'),
+    url(r'^delete_user/', views.delete_user, name='delete_user'),
 ]
