40492336d0e4a797dbd487578148943aa5dd3431
Colson Driemel <colsond@uvic.ca>
2017-09-29 09:31:31 -0700

    add create_user url

--- web_frontend/cloudscheduler/csv2/urls.py
+++ web_frontend/cloudscheduler/csv2/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^manage_users/', views.manage_users, name='manage_users'),
+    url(r'^create_user/', views.create_user, name='create_user'),
 ]
