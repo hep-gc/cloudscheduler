56479bd3da321f2b4e9ed91200ed127afe97b987
Colson Driemel <colsond@uvic.ca>
2017-11-08 14:34:22 -0800

    add cleanup task implementation for condor jobs
    update yaml config file with log file for cscleaner

---
+++ condor_data_collectors/cscleaner.py
@@ -0,0 +1,87 @@
+import time
+import htcondor
+import json
+import logging
+import config
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+
+
+def cleanUp():
+    while(True):
+        # Setup condor classes and database connctions
+        condor_s = htcondor.Schedd()
+        condor_c = htcondor.Collector()
+        Base = automap_base()
+        engine = create_engine("mysql://" + config.db_user + ":" + config.db_password + "@" + config.db_host + ":" + str(config.db_port) + "/" + config.db_name)
+        Base.prepare(engine, reflect=True)
+        Job = Base.classes.condor_jobs
+        archJob = Base.classes.archived_condor_jobs
+        Resource = Base.classes.condor_resources
+        archResource = Base.classes.archived_condor_resources
+        session = Session(engine)
+
+        #Part 1 Clean up job ads
+        condor_job_list = condor_s.query()
+        db_job_list = session.query(Job)
+        #loop through the condor data and make a list of GlobalJobId
+        #then loop through db list checking if they are in the aforementioned list
+        condor_name_list = []
+        for ad in condor_job_list:
+            ad_dict = dict(ad)
+            condor_name_list.append(ad_dict['GlobalJobId'])
+        for job in db_job_list:
+            if job.GlobalJobId not in condor_name_list:
+                #job is missing from condor, clean it up
+                logging.info("Found Job missing from condor: %s, cleaning up." % job.GlobalJobId)
+                job_dict = job.__dict__
+                logging.info(job_dict)
+                del job_dict['_sa_instance_state'] # metadata not relevent to the job ad, must trim to init with kwargs
+                new_arch_job = archJob(**job_dict)
+                session.merge(new_arch_job)
+
+
+
+        #Part 2 Clean up machine/resource ads
+        condor_machine_list = condor_c.query()
+        db_machine_list = session.query(Resource)
+
+
+        session.commit()
+
+
+
+
+if __name__ == '__main__':
+    
+    logging.basicConfig(filename=config.cleanup_log_file,level=logging.DEBUG)
+    processes = []
+
+    p_cleanup = Process(target=cleanUp)
+    processes.append(p_cleanup)
+   
+
+    # Wait for keyboard input to exit
+    try:
+        for process in processes:
+            process.start()
+        while(True):
+            for process in processes:
+                if not process.is_alive():
+                    logging.error("%s process died!" % process.name)
+                    logging.error("Restarting %s process...")
+                    process.start()
+                time.sleep(1)
+            time.sleep(10)
+    except (SystemExit, KeyboardInterrupt):
+        logging.error("Caught KeyboardInterrupt, shutting down threads and exiting...")
+
+    for process in processes:
+        try:
+            process.join()
+        except:
+            logging.error("failed to join process %s" % process.name)
+
+
