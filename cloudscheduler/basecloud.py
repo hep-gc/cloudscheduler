e57790dd8c0af5af0ae32fab706b730cdb9305e9
Michael Paterson <mhp@uvic.ca>
2017-12-01 10:31:49 -0800

    add method for converting the attribute list string to a dict

--- cloudscheduler/basecloud.py
+++ cloudscheduler/basecloud.py
@@ -2,7 +2,8 @@ import gzip
 import uuid
 import cloudscheduler.config as csconfig
 import cloudscheduler.cloud_init_util
-import io.StringIO
+#import io.StringIO
+from io import StringIO
 
 class BaseCloud():
     def __init__(self, name, slots=0, extrayaml=""):
@@ -39,14 +40,31 @@ class BaseCloud():
         return name
 
     def prepare_userdata(self, yamllist):
-        yamllist.extend(self.extrayaml)
+        if yamllist:
+            yamllist.extend(self.extrayaml)
         userdata = cloudscheduler.cloud_init_util.build_multi_mime_message(yamllist)
         if not userdata:
             return ""
-        udbuf = io.StringIO()
+        udbuf = StringIO()
         udf = gzip.GzipFile(mode='wb', fileobj=udbuf)
         try:
             udf.write(userdata)
         finally:
             udf.close()
         return udbuf.getvalue()
+
+    def _attr_list_to_dict(self, attr_list_str):
+        """Convert string in form "key1:value1,key2:value2" into a dictionary"""
+        if not attr_list_str:
+            return {}
+        attr_dict = {}
+        for keyvaluestr in attr_list_str.split(','):
+            keyvalue = keyvaluestr.split(':')
+            if len(keyvalue) == 1:
+                attr_dict['default'] = keyvalue[0].strip()
+            elif len(keyvalue) == 2:
+                attr_dict[keyvalue[0].strip().lower()] = keyvalue[1].strip()
+            else:
+                raise ValueError("Can't split '%s' into suitable host attribute pair" % keyvalue)
+        return attr_dict
+
