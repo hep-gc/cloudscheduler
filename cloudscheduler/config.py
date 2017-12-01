8f2d0e1bfc8be4906e3164db7eed45b06e53e7df
Michael Paterson <mhp@uvic.ca>
2017-12-01 10:33:45 -0800

    added more default config values

--- cloudscheduler/config.py
+++ cloudscheduler/config.py
@@ -6,15 +6,24 @@ default_config_file_location = "/etc/cloudscheduler/cloudscheduler.yaml"
 
 class CSConfig():
     def __init__(self, **kwargs):
-        self.name = kwargs['general'].get('name', 'CloudScheduler')
-        self.redishost = kwargs['general'].get('redishost', 'localhost')
-        self.redisport = kwargs['general'].get('redisport', 6379)
-        self.job_data_keys = kwargs['general'].get('job_data_keys', ['condor-jobs'])
-        self.collector_data_keys = kwargs['general'].get('collector_data_keys', ['condor-resources'])
-        self.vm_data_key = kwargs['general'].get('vm_data_keys', ['vm-data'])
+        self.name = kwargs['general'].get('name', 'cloudscheduler')
+        self.keyname = kwargs['general'].get('keyname', None)
+        #self.redishost = kwargs['general'].get('redishost', 'localhost')
+        #self.redisport = kwargs['general'].get('redisport', 6379)
+        #self.job_data_keys = kwargs['general'].get('job_data_keys', ['condor-jobs'])
+        #self.collector_data_keys = kwargs['general'].get('collector_data_keys', ['condor-resources'])
+        #self.vm_data_key = kwargs['general'].get('vm_data_keys', ['vm-data'])
+
+        self.cloudscheduler_log_file = kwargs['general'].get('cloudscheduler_log_file', '/var/log/cloudscheduler/cloudscheduler.log')
+
+        self.db_user = kwargs['database'].get('db_user', 'csv2')
+        self.db_password = kwargs['database'].get('db_password', None)
+        self.db_host = kwargs['database'].get('db_host', 'localhost')
+        self.db_port = kwargs['database'].get('db_port', 3306)
+        self.db_name = kwargs['database'].get('db_name', 'csv2')
 
         self.default_instancetype = kwargs['job'].get('instancetype', 'm1.small')
-        self.default_image = kwargs['job'].get('image', 'Cent OS 7')
+        self.default_image = kwargs['job'].get('image', 'cernvm3-micro-2.8-6.hdd')
 
     @staticmethod
     def setup(config_file=default_config_file_location):
