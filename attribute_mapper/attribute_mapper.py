e7bc5633f469a5f635e0e385d1fc3196fd682770
Colson Driemel <colsond@uvic.ca>
2018-01-12 13:33:50 -0800

    add python attribute mapper
    this code is untested

---
+++ attribute_mapper/attribute_mapper.py
@@ -0,0 +1,76 @@
+import config
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+
+rowid_dict = {}
+attr_list_dict = {}
+
+def map_attributes(src, dest, attr_list):
+    mapped_list = []
+
+    return mapped_list
+
+def build_mapping_dictionaries():
+    global rowid_dict
+    global attr_list_dict
+
+    Base = automap_base()
+    engine = create_engine("mysql://" + config.db_user + ":" + config.db_password + "@" + config.db_host + ":" + str(config.db_port) + "/" + config.db_name)
+    Base.prepare(engine, reflect=True)
+    Mappings = Base.classes.csv2_name_mapping
+    session = Session(engine)
+
+    mapping_rows = session.query(Mappings)
+    mapping_names = Mappings.__table__.columns.keys()
+
+    # Generate rowid dict
+    for language in mapping_names:
+        row_id = 0
+        row_dict = {}
+        for row in mapping_rows:
+            # These ifs will need to be updated every time a new translation language is added since there is no way
+            # to use the contents of a variable as a variable name reliably (possible with exec())
+            if language == "csv2":
+                row_dict[row.csv2] = row_id
+            elif language == "os_limits":
+                row_dict[row.os_limits] = row_id
+            elif language == "openstack":
+                row_dict[row.openstack] = row_id
+            else:
+                print("Found column not implemented in code, breaking")
+                break
+            row_id = row_id + 1
+        rowid_dict[language] = row_dict
+
+
+    # Generate attribute list dict
+    for language in mapping_names:
+        attr_list = []
+        for row in mapping_rows:
+            # These ifs will need to be updated every time a new translation language is added since there is no way
+            # to use the contents of a variable as a variable name reliably (possible with exec())
+            if language == "csv2":
+                attr_list.append(row.csv2) 
+            elif language == "os_limits":
+                attr_list.append(row.os_limits)
+            elif language == "openstack":
+                attr_list.append(row.openstack)
+            else:
+                print("Found column not implemented in code, breaking")
+                break
+        attr_list_dict[language] = attr_list
+
+    return True
+
+def dump_dicts():
+    global attr_list_dict
+    global rowid_dict
+
+    print attr_list_dict
+    print rowid_dict
+
+
+build_mapping_dictionaries()
+dump_dicts()
---
+++ attribute_mapper/config.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import yaml
+
+log_file = "/var/log/cloudscheduler/attribute_mapper.log"
+log_level = 20 #INFO
+
+db_host = "localhost"
+db_port = 3306
+db_user = "csv2"
+db_password = ""
+
+
+
+if os.path.isfile("/etc/attribute_mapper.yaml"):
+    path = "/etc/attribute_mapper.yaml"
+
+elif os.path.isfile("/opt/cloudscheduler/attribute_mapper/attribute_mapper.yaml"):
+    path = "/opt/cloudscheduler/attribute_mapper/attribute_mapper.yaml"
+
+else:
+    path = os.path.dirname(os.path.realpath(__file__)) + "/attribute_mapper.yaml"
+
+
+try:
+    with open(path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+except Exception as e:
+    print >> sys.stderr, "Configuration file problem: There was a " \
+                         "problem reading %s. Check that it is readable," \
+                         "and that it exists. " % path
+
+
+if "general" in cfg:
+
+    if "log_file" in cfg["general"]:
+        log_file = cfg["general"]["log_file"]
+
+    if "log_level" in cfg["general"]:
+        log_level = cfg["general"]["log_level"]
+
+
+if "database" in cfg:
+    if "db_host" in cfg["database"]:
+        db_host = cfg["database"]["db_host"]
+
+    if "db_port" in cfg["database"]:
+        db_port = cfg["database"]["db_port"]
+
+    if "db_name" in cfg["database"]:
+        db_name = cfg["database"]["db_name"]
+
+    if "db_user" in cfg["database"]:
+        db_user = cfg["database"]["db_user"]
+
+    if "db_password" in cfg["database"]:
+        db_password = cfg["database"]["db_password"]
