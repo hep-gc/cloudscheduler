547d12d99ddd3bcdaf2e0f977b95da7fca3a939d
Michael Paterson <mhp@uvic.ca>
2018-02-20 10:03:38 -0800

    stubs for ec2 and gce, log format change

--- cloudscheduler/ec2cloud.py
+++ cloudscheduler/ec2cloud.py
@@ -1,5 +1,28 @@
+"""
+EC2 API Cloud Connector Module.
+"""
+
+import boto3
+import botocore
+
 import cloudscheduler.basecloud
 
 class EC2Cloud(cloudscheduler.basecloud):
-    def __init__(self):
+
+    """
+    Cloud Connector class for EC2 API based clouds like AmazonEC2, or OpenNebula.
+    """
+
+    def __init__(self, resource, extrayaml):
+        """Constructor for ec2 based clouds."""
+        cloudscheduler.basecloud.BaseCloud.__init__(self, name=resource.cloud_name,
+                                                    extrayaml=extrayaml)
+
+    def vm_create(self, group_yaml_list=None, num=1, job=None, flavor=None):
+        pass
+
+    def vm_destroy(self, vm):
+        pass
+
+    def vm_update(self):
         pass
--- cloudscheduler/googlecloud.py
+++ cloudscheduler/googlecloud.py
@@ -1,5 +1,23 @@
+"""
+Google Compute Engine Connector Module.
+"""
 import cloudscheduler.basecloud
 
 class GoogleCloud(cloudscheduler.basecloud):
-    def __init__(self):
+    """
+    Cloud Connector class for Google Compute Engine.
+    """
+
+    def __init__(self, resource, extrayaml):
+        """Constructor for GCE."""
+        cloudscheduler.basecloud.BaseCloud.__init__(self, name=resource.cloud_name,
+                                                    extrayaml=extrayaml)
+
+    def vm_create(self, group_yaml_list=None, num=1, job=None, flavor=None):
+        pass
+
+    def vm_destroy(self, vm):
+        pass
+
+    def vm_update(self):
         pass
