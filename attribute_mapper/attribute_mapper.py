e9ef751ae6447e784a1e628d754d1fe01182410b
Colson Driemel <colsond@uvic.ca>
2018-02-21 11:47:27 -0800

    refactor attribute mapper using pylint

--- attribute_mapper/attribute_mapper.py
+++ attribute_mapper/attribute_mapper.py
@@ -1,4 +1,4 @@
-import config
+import attribute_mapper.config as config
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -10,7 +10,7 @@ attr_list_dict = {}
 def map_attributes(src, dest, attr_dict):
     global rowid_dict
     global attr_list_dict
-    if len(rowid_dict)==0 or len(attr_list_dict)==0:
+    if len(rowid_dict) == 0 or len(attr_list_dict) == 0:
         build_mapping_dictionaries()
 
     mapped_dict = {}
@@ -24,7 +24,8 @@ def build_mapping_dictionaries():
     global attr_list_dict
 
     Base = automap_base()
-    engine = create_engine("mysql://" + config.db_user + ":" + config.db_password + "@" + config.db_host + ":" + str(config.db_port) + "/" + config.db_name)
+    engine = create_engine("mysql://" + config.db_user + ":" + config.db_password + \
+        "@" + config.db_host + ":" + str(config.db_port) + "/" + config.db_name)
     Base.prepare(engine, reflect=True)
     Mappings = Base.classes.csv2_attribute_mapping
     session = Session(engine)
@@ -37,8 +38,9 @@ def build_mapping_dictionaries():
         row_id = 0
         row_dict = {}
         for row in mapping_rows:
-            # These ifs will need to be updated every time a new translation language is added since there is no way
-            # to use the contents of a variable as a variable name reliably (possible with exec())
+            # These ifs will need to be updated every time a new translation language is added
+            # since there is no way to use the contents of a variable as a variable name reliably
+            # (possible with exec())
             if language == "csv2":
                 row_dict[row.csv2] = row_id
             elif language == "os_limits":
@@ -64,10 +66,11 @@ def build_mapping_dictionaries():
     for language in mapping_names:
         attr_list = []
         for row in mapping_rows:
-            # These ifs will need to be updated every time a new translation language is added since there is no way
-            # to use the contents of a variable as a variable name reliably (possible with exec())
+            # These ifs will need to be updated every time a new translation language is added
+            # since there is no way to use the contents of a variable as a variable name reliably
+            # (possible with exec())
             if language == "csv2":
-                attr_list.append(row.csv2) 
+                attr_list.append(row.csv2)
             elif language == "os_limits":
                 attr_list.append(row.os_limits)
             elif language == "os_flavors":
