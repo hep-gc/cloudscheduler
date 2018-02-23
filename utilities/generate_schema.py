dab0afb08e779cb47d6eb602622c4ff6c600acc1
Colin Leavett-Brown <crlb@uvic.ca>
2018-02-23 12:28:01 -0800

    pylint score better than 8

--- utilities/generate_schema.py
+++ utilities/generate_schema.py
@@ -1,88 +1,174 @@
 #!/usr/bin/env python
-#
-# Synopsis: utilities/generate_schema.py > lib/schema.py
-#
-# This routine pulls the current table definitions from the CSv2 database and writes the schema to stdout. To use the
-# schema definitions:
-#
-# from lib.schema import <view_or_table_name_1>, <view_or_table_name_2>, ...
-#
+"""
+Synopsis: utilities/generate_schema.py > lib/schema.py
+
+This routine pulls the current table definitions from the csv2 database and writes the
+schema to stdout. To use the schema definitions:
+
+            from lib.schema import <view_or_table_name_1>, <view_or_table_name_2>, ...
+"""
+
 from subprocess import Popen, PIPE
 from tempfile import mkdtemp
 import os
 import string
 import sys
 
-remove_brackets = string.maketrans('()', '  ')
+REMOVE_BRACKETS = string.maketrans('()', '  ')
 
 def main(args):
-  gvar = {'backup_data': [
-    'csv2_attribute_mapping',
-    ],
-    'cmd_path': os.path.abspath(args[0]),
-    }
+    """
+    This does everything:
+    o Writes the schema header to stdout.
+    o Retrieves the list of tables from the csv2 database.
+    o Then for each table:
+      - Resets the variable _stdout to just the table header.
+      - Retrieves the column list for the table.
+      - Then for each column:
+        + Appends the column definition to _stdout.-
+      - Appends the table footer to _stdout.
+      - Writes the table definition to stdout.
+    """
+    gvar = {
+        'backup_data': [
+            'csv2_attribute_mapping',
+            ],
+        'cmd_path': os.path.abspath(args[0]),
+        }
 
-  gvar['path_info'] = gvar['cmd_path'].split('/')
-  gvar['ix'] = gvar['path_info'].index('cloudscheduler')
-  gvar['temp_dir'] = mkdtemp()
-  gvar['secrets_file'] = '%s/ansible-systems/heprc/staticvms/roles/csv2/vars/csv2_secrets.yaml' % '/'.join(gvar['path_info'][:gvar['ix']])
-  gvar['vp_file'] = '%s/.pw/staticvms' % '/'.join(gvar['path_info'][:3])
+    gvar['path_info'] = gvar['cmd_path'].split('/')
+    gvar['ix'] = gvar['path_info'].index('cloudscheduler')
+    gvar['temp_dir'] = mkdtemp()
+    gvar['secrets_file'] = '%s/ansible-systems/heprc/staticvms/roles/csv2/vars/csv2_secrets.yaml' \
+        % '/'.join(gvar['path_info'][:gvar['ix']])
+    gvar['vp_file'] = '%s/.pw/staticvms' % '/'.join(gvar['path_info'][:3])
 
-  p1 = Popen(['ansible-vault', 'view', gvar['secrets_file'], '--vault-password-file', gvar['vp_file']], stdout=PIPE, stderr=PIPE)
-  p2 = Popen(['awk', '/^mariadb_root:/ {print $2}'], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
-  stdout, stderr = p2.communicate()
-  if stderr != '':
-    'Failed to retrieve DB password.'
-    exit(1)
-
-  gvar['pw'] = stdout.strip()
-
-  p1 = Popen(['mysql', '-uroot', '-p%s' % gvar['pw'], '-e', 'show tables;', 'csv2'], stdout=PIPE, stderr=PIPE)
-  p2 = Popen(['awk', '!/Tables_in_csv2/ {print $1}'], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
-  stdout, stderr = p2.communicate()
-  if stderr != '':
-    'Failed to retrieve table list.'
-    exit(1)
-
-  print "if 'Table' not in locals() and 'Table' not in globals():\n  from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey\n  metadata = MetaData()\n"
-
-
-  tables = stdout.split()
-  for table in tables:
-    td = [  "%s = Table('%s', metadata,\n" % (table, table) ]
-
-    p1 = Popen(['mysql', '-uroot', '-p%s' % gvar['pw'], '-e', 'show columns from %s;' % table, 'csv2'], stdout=PIPE, stderr=PIPE)
-    p2 = Popen(['awk', '!/^+/'], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p2.communicate()
+    _p1 = Popen(
+        [
+            'ansible-vault',
+            'view',
+            gvar['secrets_file'],
+            '--vault-password-file',
+            gvar['vp_file']
+            ],
+        stdout=PIPE,
+        stderr=PIPE
+        )
+    _p2 = Popen(
+        [
+            'awk',
+            '/^mariadb_root:/ {print $2}'
+            ],
+        stdin=_p1.stdout,
+        stdout=PIPE,
+        stderr=PIPE
+        )
+    stdout, stderr = _p2.communicate()
     if stderr != '':
-      'Failed to retrieve table list.'
-      exit(1)
+        print('Failed to retrieve DB password.')
+        exit(1)
 
-    columns = stdout.split("\n")
-    for ix in range(1, len(columns)):
-      w = columns[ix].split()
-      if len(w) > 3:
-        td.append("  Column('%s'," % w[0])
-        if w[1][:8] == 'varchar(':
-          w2 = w[1].translate(remove_brackets).split()
-          td.append(" String(%s)" % w2[1])
-        elif w[1][:4] == 'int(' or w[1][:6] == 'bigint' or w[1] == 'date' or w[1] == 'datetime' or w[1][:7] == 'decimal' or w[1][:8] == 'smallint' or w[1][:7] == 'tinyint':
-          td.append(" Integer")
-        elif w[1] == 'text' or w[1] == 'longtext' or w[1] == 'mediumtext':
-          td.append(" String")
-        else:
-          print '????????????????????????????????', columns[ix]
-          0/0
+    gvar['pw'] = stdout.strip()
 
-        if w[3] == 'PRI':
-          td.append(", primary_key=True")
+    _p1 = Popen(
+        [
+            'mysql',
+            '-uroot',
+            '-p%s' % gvar['pw'],
+            '-e',
+            'show tables;',
+            'csv2'
+            ],
+        stdout=PIPE,
+        stderr=PIPE
+        )
+    _p2 = Popen(
+        [
+            'awk',
+            '!/Tables_in_csv2/ {print $1}'
+            ],
+        stdin=_p1.stdout,
+        stdout=PIPE,
+        stderr=PIPE
+        )
+    stdout, stderr = _p2.communicate()
+    if stderr != '':
+        print('Failed to retrieve table list.')
+        exit(1)
 
-        if ix < len(columns) - 2:
-          td.append("),\n")
-        else:
-          td.append(")\n  )\n")
-          
-    print ''.join(td)
+    print(
+        "if 'Table' not in locals() and 'Table' not in globals():\n" + \
+        "  from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey\n" + \
+        "  metadata = MetaData()\n"
+        )
+
+
+    tables = stdout.split()
+    for table in tables:
+        _stdout = ["%s = Table('%s', metadata,\n" % (table, table)]
+
+        _p1 = Popen(
+            [
+                'mysql',
+                '-uroot',
+                '-p%s' % gvar['pw'],
+                '-e',
+                'show columns from %s;' % table,
+                'csv2'
+                ],
+            stdout=PIPE,
+            stderr=PIPE
+            )
+        _p2 = Popen(
+            [
+                'awk',
+                '!/^+/'
+                ],
+            stdin=_p1.stdout,
+            stdout=PIPE,
+            stderr=PIPE
+            )
+        stdout, stderr = _p2.communicate()
+        if stderr != '':
+            print('Failed to retrieve table list.')
+            exit(1)
+
+        columns = stdout.split("\n")
+        for _ix in range(1, len(columns)):
+            _w = columns[_ix].split()
+            if len(_w) > 3:
+                _stdout.append("  Column('%s'," % _w[0])
+                if _w[1][:8] == 'varchar(':
+                    _w2 = _w[1].translate(REMOVE_BRACKETS).split()
+                    _stdout.append(" String(%s)" % _w2[1])
+
+                elif _w[1][:4] == 'int(' or \
+                _w[1][:6] == 'bigint' or \
+                _w[1] == 'date' or \
+                _w[1] == 'datetime' or \
+                _w[1][:7] == 'decimal' or \
+                _w[1][:8] == 'smallint' or \
+                _w[1][:7] == 'tinyint':
+                    _stdout.append(" Integer")
+
+                elif _w[1] == 'text' or \
+                _w[1] == 'longtext' or \
+                _w[1] == 'mediumtext':
+                    _stdout.append(" String")
+
+                else:
+                    print('Unknown data type for column: %s' % columns[_ix])
+                    exit(1)
+
+                if _w[3] == 'PRI':
+                    _stdout.append(", primary_key=True")
+
+                if _ix < len(columns) - 2:
+                    _stdout.append("),\n")
+                else:
+                    _stdout.append(")\n  )\n")
+
+        print(''.join(_stdout))
 
 if __name__ == "__main__":
-  main(sys.argv)
+    main(sys.argv)
--- utilities/schema_xref.py
+++ utilities/schema_xref.py
@@ -1,131 +1,201 @@
 #!/usr/bin/env python
-#
-# Synopsis: utilities/generate_schema.py > lib/schema.py
-#
-# This routine pulls the current table definitions from the CSv2 database and writes the schema to stdout. To use the
-# schema definitions:
-#
-# from lib.schema import <view_or_table_name_1>, <view_or_table_name_2>, ...
-#
+"""
+Write cross reference to stdout. Synopsis: utilities/schema_xref.py
+"""
+
 from subprocess import Popen, PIPE
 from tempfile import mkdtemp
 import getpass
 import os
-import string
 import sys
 
-remove_brackets = string.maketrans('()', '  ')
+def scan_dir(gvar, path):
+    """
+    Recursive function to walk the file tree of the csv2 repository, and for each python
+    source file (*.py) or view definition (view_*), report which tables and columns are
+    referenced. References are returned in the gvar['tables'] variable when all files
+    have been scanned.
+    """
+    for item in os.listdir(path):
+        if item[0] == '.':
+            continue
 
-def scan_dir (gvar, path):
-  for item in os.listdir(path):
-    if item[0] == '.':
-      continue
+        current_path = '%s/%s' % (path, item)
 
-    current_path = '%s/%s' % (path, item)
+        if os.path.isdir(current_path):
+            if current_path[-7:] != '/backup' and current_path[-4:] != '/lib':
+                scan_dir(gvar, current_path)
 
-    if os.path.isdir(current_path):
-      if current_path[-7:] != '/backup' and current_path[-4:] != '/lib':
-        scan_dir(gvar, current_path)
+        elif os.path.getsize(current_path) > 0 and \
+        (item[:5] == 'view_' or item[-3:] == '.py'):
 
-    elif os.path.getsize(current_path) > 0 and (item[:5] == 'view_' or item[-3:] == '.py'):
+            _fd = open(current_path)
+            current_path_content = _fd.read()
+            _fd.close()
 
-      fd = open(current_path)
-      current_path_content = fd.read()
-      fd.close()
+            for table in gvar['tables']:
+                if '\'%s\'' % table in current_path_content or \
+                '"%s"' % table in current_path_content or \
+                ' %s ' % table in current_path_content or \
+                '[%s]' % table in current_path_content:
+                    gvar['tables'][table]['paths'][current_path] = 0
+                    lines = current_path_content.split('\n')
+                    for column in gvar['tables'][table]['columns']:
+                        if current_path not in gvar['tables'][table]['columns'][column]:
+                            gvar['tables'][table]['columns'][column][current_path] = []
 
-      for table in gvar['tables']:
-        if '\'%s\'' % table in current_path_content or '"%s"' % table in current_path_content or ' %s ' % table in current_path_content or '[%s]' % table in current_path:
-          gvar['tables'][table]['paths'][current_path] = 0
-          lines = current_path_content.split('\n')
-          for column in gvar['tables'][table]['columns']:
-            if current_path not in gvar['tables'][table]['columns'][column]:
-              gvar['tables'][table]['columns'][column][current_path] = []
+                        for _ix, _v in enumerate(lines):
+                            if column in lines[_ix]:
+                                gvar['tables'][table]['paths'][current_path] += 1
+                                gvar['tables'][table]['columns'][column][current_path].append(_ix+1)
 
-            for ix in range(len(lines)):
-              if column in lines[ix]: 
-                gvar['tables'][table]['paths'][current_path] += 1
-                gvar['tables'][table]['columns'][column][current_path].append(ix+1)
+def main(args):
+    """
+    Retrieve the schema (table and column definitions) of the csv2 database and then
+    call scan_dir to scan all source files for references. When the scan is complete,
+    print the report.
+    """
+    gvar = {
+        'backup_data': [
+            'csv2_attribute_mapping',
+            ],
+        'cmd_path': os.path.abspath(args[0]),
+        }
 
-def main (args):
-  gvar = {'backup_data': [
-    'csv2_attribute_mapping',
-    ],
-    'cmd_path': os.path.abspath(args[0]),
-    }
+    gvar['path_info'] = gvar['cmd_path'].split('/')
+    gvar['ix'] = gvar['path_info'].index('cloudscheduler')
+    gvar['root_dir'] = '/'.join(gvar['path_info'][:gvar['ix']+1])
+    gvar['temp_dir'] = mkdtemp()
+    gvar['secrets_file'] = '%s/ansible-systems/heprc/staticvms/roles/csv2/vars/csv2_secrets.yaml' \
+        % '/'.join(gvar['path_info'][:gvar['ix']])
+    gvar['vp_file'] = '%s/.pw/staticvms' % '/'.join(gvar['path_info'][:3])
 
-  gvar['path_info'] = gvar['cmd_path'].split('/')
-  gvar['ix'] = gvar['path_info'].index('cloudscheduler')
-  gvar['root_dir'] = '/'.join(gvar['path_info'][:gvar['ix']+1])
-  gvar['temp_dir'] = mkdtemp()
-  gvar['secrets_file'] = '%s/ansible-systems/heprc/staticvms/roles/csv2/vars/csv2_secrets.yaml' % '/'.join(gvar['path_info'][:gvar['ix']])
-  gvar['vp_file'] = '%s/.pw/staticvms' % '/'.join(gvar['path_info'][:3])
+    if os.path.exists(gvar['secrets_file']) and os.path.exists(gvar['vp_file']):
+        _p1 = Popen(
+            [
+                'ansible-vault',
+                'view',
+                gvar['secrets_file'],
+                '--vault-password-file',
+                gvar['vp_file']
+                ],
+            stdout=PIPE,
+            stderr=PIPE
+            )
+        _p2 = Popen(
+            [
+                'awk',
+                '/^mariadb_cloudscheduler:/ {print $2}'
+                ],
+            stdin=_p1.stdout,
+            stdout=PIPE,
+            stderr=PIPE
+            )
+        stdout, stderr = _p2.communicate()
+        if stderr != '':
+            print('Failed to retrieve DB password.')
+            exit(1)
 
-  if os.path.exists(gvar['secrets_file']) and os.path.exists(gvar['vp_file']):
-    p1 = Popen(['ansible-vault', 'view', gvar['secrets_file'], '--vault-password-file', gvar['vp_file']], stdout=PIPE, stderr=PIPE)
-    p2 = Popen(['awk', '/^mariadb_cloudscheduler:/ {print $2}'], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p2.communicate()
+        gvar['pw'] = stdout.strip()
+    else:
+        gvar['pw'] = getpass.getpass('Enter MariaDB password for user csv2:')
+
+    _p1 = Popen(
+        [
+            'mysql',
+            '-ucsv2',
+            '-p%s' % gvar['pw'],
+            '-e',
+            'show full tables;',
+            'csv2'
+            ],
+        stdout=PIPE,
+        stderr=PIPE
+        )
+    _p2 = Popen(
+        [
+            'awk',
+            '!/Tables_in_csv2/ {print $1}'
+            ],
+        stdin=_p1.stdout,
+        stdout=PIPE,
+        stderr=PIPE
+        )
+    stdout, stderr = _p2.communicate()
     if stderr != '':
-      'Failed to retrieve DB password.'
-      exit(1)
-
-    gvar['pw'] = stdout.strip()
-  else:
-    gvar['pw'] = getpass.getpass('Enter MariaDB password for user csv2:')
-
-  p1 = Popen(['mysql', '-ucsv2', '-p%s' % gvar['pw'], '-e', 'show full tables;', 'csv2'], stdout=PIPE, stderr=PIPE)
-  p2 = Popen(['awk', '!/Tables_in_csv2/ {print $1 " " $2}'], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
-  stdout, stderr = p2.communicate()
-  if stderr != '':
-    print('Failed to retrieve table list.')
-    exit(1)
+        print('Failed to retrieve table list.')
+        exit(1)
 
 
-  gvar['tables'] = {}; tables = stdout.split('\n')
-  for _table in tables:
-    if len(_table) < 1:
-      continue
+    gvar['tables'] = {}
+    tables = stdout.split('\n')
+    for table in tables:
+        if len(table) < 1:
+            continue
 
-    table, table_type = _table.split()
-#   if table_type == 'VIEW':
-#     continue
+        gvar['tables'][table] = {'columns': {}, 'paths': {}}
 
-    gvar['tables'][table] = { 'columns': {}, 'paths': {} }
+        _p1 = Popen(
+            [
+                'mysql',
+                '-ucsv2',
+                '-p%s' % gvar['pw'],
+                '-e',
+                'show columns from %s;' % table,
+                'csv2'
+                ],
+            stdout=PIPE,
+            stderr=PIPE
+            )
+        _p2 = Popen(
+            [
+                'awk',
+                '!/^+/'
+                ],
+            stdin=_p1.stdout,
+            stdout=PIPE,
+            stderr=PIPE
+            )
+        stdout, stderr = _p2.communicate()
+        if stderr != '':
+            print('Failed to retrieve columns for table "%s".' % table)
+            exit(1)
 
-    p1 = Popen(['mysql', '-ucsv2', '-p%s' % gvar['pw'], '-e', 'show columns from %s;' % table, 'csv2'], stdout=PIPE, stderr=PIPE)
-    p2 = Popen(['awk', '!/^+/'], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p2.communicate()
-    if stderr != '':
-      print('Failed to retrieve columns for table "%s".' % table)
-      exit(1)
+        columns = stdout.split("\n")
+        for _ix in range(1, len(columns)):
+            _w = columns[_ix].split()
+            if len(_w) > 3:
+                gvar['tables'][table]['columns'][_w[0]] = {}
 
-    columns = stdout.split("\n")
-    for ix in range(1, len(columns)):
-      w = columns[ix].split()
-      if len(w) > 3:
-        gvar['tables'][table]['columns'][w[0]] = {}
+    scan_dir(gvar, gvar['root_dir'])
 
-  scan_dir(gvar, gvar['root_dir'])
+    for table in sorted(gvar['tables']):
+        print('Table: %s' % table)
+        print_header_table_no_columns = True
+        print_header_table_with_columns = True
+        for current_path in sorted(gvar['tables'][table]['paths']):
+            if gvar['tables'][table]['paths'][current_path] < 1:
+                if print_header_table_no_columns:
+                    print(' This table but none of its\' columns were referenced \
+                        in the following files:')
+                    print_header_table_no_columns = False
 
-  for table in sorted(gvar['tables']):
-    print('Table: %s' % table)
-    print_header_table_no_columns = True; print_header_table_with_columns = True
-    for current_path in sorted(gvar['tables'][table]['paths']):
-      if gvar['tables'][table]['paths'][current_path] < 1:
-        if print_header_table_no_columns:
-          print(' This table but none of its\' columns were referenced in the following files:')
-          print_header_table_no_columns = False
+                print('   %s' % current_path)
 
-        print('   %s' % current_path)
+        for column in sorted(gvar['tables'][table]['columns']):
+            if print_header_table_with_columns:
+                print(' Columns:')
+                print_header_table_with_columns = False
 
-    for column in sorted(gvar['tables'][table]['columns']):
-        if print_header_table_with_columns:
-          print(' Columns:')
-          print_header_table_with_columns = False
-    
-        print('   %s' % column)
-        for current_path in sorted(gvar['tables'][table]['columns'][column]):
-          if len(gvar['tables'][table]['columns'][column][current_path]) > 0:
-            print('              %s %s' % (current_path, gvar['tables'][table]['columns'][column][current_path]))
+            print('   %s' % column)
+            for current_path in sorted(gvar['tables'][table]['columns'][column]):
+                if gvar['tables'][table]['columns'][column][current_path]:
+                    print(
+                        '              %s %s' % (
+                            current_path,
+                            gvar['tables'][table]['columns'][column][current_path]
+                            )
+                        )
 
 if __name__ == "__main__":
-  main(sys.argv)
+    main(sys.argv)
