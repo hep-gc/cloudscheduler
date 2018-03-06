3fdf24c13d72644784b77b80f73d0de8867aadcf
Colson Driemel <colsond@uvic.ca>
2018-03-06 09:30:47 -0800

    add password reset to admin user management form

--- web_frontend/cloudscheduler/csv2/user_views.py
+++ web_frontend/cloudscheduler/csv2/user_views.py
@@ -55,18 +55,18 @@ def create_user(request):
             #check #1
             if user == registered_user.username or user == registered_user.cert_cn:
                 #render manage users page with error message
-                return manage_users(request, response_code=1, message="Username unavailable")
+                return redirect('manage_users', response_code=1, message="Username unavailable")
             #check #2
             if cert_cn is not None and (cert_cn == registered_user.username or cert_cn == registered_user.cert_cn):
-                return manage_users(request, response_code=1, message="Username unavailable or conflicts with a registered Distinguished Name")
+                return redirect('manage_users', response_code=1, message="Username unavailable or conflicts with a registered Distinguished Name")
         #check #3 part 1
         if pass1 is None or pass2 is None:
-            return manage_users(request, response_code=1, message="Password is empty")
+            return redirect('manage_users', response_code=1, message="Password is empty")
         if len(pass1)<4:
-            return manage_users(request, response_code=1, message="Password must be at least 4 characters")
+            return redirect('manage_users', response_code=1, message="Password must be at least 4 characters")
         #check #4
         if pass1 != pass2:
-            return manage_users(request, response_code=1, message="Passwords do not match")
+            return redirect('manage_users', response_code=1, message="Passwords do not match")
 
         # After checks are made use bcrypt to encrypt password.
         hashed_pw = bcrypt.hashpw(pass1.encode(), bcrypt.gensalt(prefix=b"2a"))
@@ -74,10 +74,10 @@ def create_user(request):
         #if all the checks passed and the hashed password has been generated create a new user object and save import
         new_usr = csv2_user(username=user, password=hashed_pw, cert_cn=cert_cn, is_superuser=False)
         new_usr.save()
-        return manage_users(request, response_code=0, message="User added")
+        return redirect('manage_users', response_code=0, message="User added")
     else:
         #not a post, return to manage users page
-        return manage_users(request)
+        return redirect('manage_users')
 
 def update_user(request):
     if not verifyUser(request):
@@ -92,30 +92,49 @@ def update_user(request):
         new_username = request.POST.get('username')
         cert_cn = request.POST.get('common_name')
         su_status = request.POST.get('is_superuser')
+        new_pass1 = request.POST.get('password1')
+        new_pass2 = request.POST.get('password2')
+
         if not su_status:
             su_status=False
         else:
             su_status=True
 
-        # Need to perform two checks
+        # Need to perform three checks
         # 1. Check that the new username is valid (ie no username or cert_cn by that name)
         #   if the username hasn't changed we can skip this check since it would have been done on creation.
         # 2. Check that the cert_cn is not equal to any username or other cert_cn
+        # 3. Check that the password is not empty and in that case that it is also a valid password
 
         for registered_user in csv2_user_list:
             #check #1
             if not new_username == user_to_update.username:
                 if user == registered_user.username or user == registered_user.cert_cn:
                     #render manage users page with error message
-                    return manage_users(request, response_code=1, message="Unable to update user: new username unavailable")
+                    return redirect('manage_users', response_code=1, message="Unable to update user: new username unavailable")
             #check #2
             if cert_cn is not None and registered_user.username != user_to_update.username and (cert_cn == registered_user.username or cert_cn == registered_user.cert_cn):
-                return manage_users(request, response_code=1, message="Unable to update user: Username unavailable or conflicts with a registered Distinguished Name")
+                return redirect('manage_users', response_code=1, message="Unable to update user: Username unavailable or conflicts with a registered Distinguished Name")
+
+            #check #3 part 1
+            if new_pass1 is not None and new_pass2 is not None:
+                # part 2
+                if len(new_pass1)>3:
+                    # part 3
+                    if new_pass1 == new_pass2:
+                        #update pass
+                        user_to_update.password = bcrypt.hashpw(new_pass1.encode(), bcrypt.gensalt(prefix=b"2a"))
+                    else:
+                        # passwords don't match
+                        return redirect('manage_users', response_code=1, message="Passwords don't match, please try again or leave password empty to update other fields.")
+                else:
+                    # passwords too short
+                    return redirect('manage_users', response_code=1, message="Passwords are too short, please try again or leave password empty to update other fields.")
         user_to_update.username = new_username
         user_to_update.cert_cn = cert_cn
         user_to_update.is_superuser = su_status
         user_to_update.save()
-        return manage_users(request, response_code=0, message="User updated")
+        return redirect('manage_users', response_code=0, message="User updated")
 
     else:
         #not a post, return to manage users page
@@ -131,7 +150,7 @@ def delete_user(request):
         user = request.POST.get('username')
         user_obj = csv2_user.objects.filter(username=user)
         user_obj.delete()
-        return manage_users(request, response_code=0, message="User deleted")
+        return redirect('manage_users', response_code=0, message="User deleted")
     return False
 
 def user_settings(request):
@@ -272,7 +291,7 @@ def reset_user_pass(request, username):
                 'response_code': 0,
                 'message': "Update Successful"
             }
-        return manage_users(request, message="Password Update Successful.")
+        return redirect('manage_users', response_code=0, message="Password Update Successful.")
 
     else:
         #render reset_user_pass template
