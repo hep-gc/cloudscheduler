cd216747a8e51dfb2d1ea9ba6667cf6c86e5673e
colsond <colsond@uvic.ca>
2018-03-21 11:52:56 -0700

    reorganize cloud views

--- web_frontend/cloudscheduler/csv2/cloud_views.py
+++ web_frontend/cloudscheduler/csv2/cloud_views.py
@@ -9,32 +9,133 @@ from .view_utils import getAuthUser, getcsv2User, verifyUser, getSuperUserStatus
 from utils import db_utils
 from collections import defaultdict
 import bcrypt
-# 
-# This function should recieve a post request with a payload of yaml to add to a given group
-# (group_yaml)
-#
-def add_group_yaml(request):
-    return None
 
-# 
-# This function should recieve a post request with a payload of yaml to add to a given cloud
-# (group_resources_yaml)
+
+def list(request, group_name=None, response_code=0, message=None):
+    if not verifyUser(request):
+        raise PermissionDenied
+
+
+    active_user = getcsv2User(request)
+    user_groups = db_utils.get_user_groups(active_user)
+
+    #check to see if specified group is a valid one for this user, if it is set it as active
+    if request.method == 'POST':
+        group_name = request.POST.get('group')
+        if group_name is not None:
+            if group_name in user_groups:
+                active_user.active_group = group_name
+                active_user.save()
+
+    if len(user_groups)==0:
+        # active user isn't registered to any groups, display blank page with msg
+        #TODO#
+        pass
+    # get data based on active group, if no active group pick the first from the list
+    if active_user.active_group is None:
+        active_user.active_group = user_groups[0]
+        active_user.save()
+
+    #get cloud info
+    cloud_list = {'ResultProxy': [dict(r) for r in db_utils.get_group_resources(group_name=active_user.active_group)]}
+
+    context = {
+            'active_user': active_user,
+            'active_group': active_user.active_group,
+            'user_groups': user_groups,
+            'cloud_list': cloud_list,
+            'response_code': response_code,
+            'message': message
+        }
+
+    return _render(request, 'csv2/clouds.html', context)
+
+
+def prepare(request):
+
+    context = {
+            'response_code': 0,
+            'message': None
+    }
+    
+    return _render(request, 'csv2/clouds.html', context)
+
+
 #
-def add_yaml(request):
-    return None
+# This function generates a the status of a given groups operations
+# VM status, job status, and machine status should all be available for a given group on this page
+#
+def status(request, group_name=None):
+    if not verifyUser(request):
+        raise PermissionDenied
+
+    active_user = getcsv2User(request)
+    user_groups = db_utils.get_user_groups(active_user)
+
+    #check to see if specified group is a valid one for this user, if it is set it as active
+    if request.method == 'POST':
+        group_name = request.POST.get('group')
+        if group_name is not None:
+            if group_name in user_groups:
+                active_user.active_group = group_name
+                active_user.save()
+
+    if len(user_groups)==0:
+        # active user isn't registered to any groups, display blank page with msg
+        #TODO#
+        pass
+    # get data based on active group, if no active group pick the first from the list
+    if active_user.active_group is None:
+        active_user.active_group = user_groups[0]
+        active_user.save()
+
+    #get cloud info
+    cloud_list = db_utils.get_group_resources(group_name=active_user.active_group)
+
+    #get vms
+    vm_list = db_utils.get_vms(group_name=active_user.active_group)
+    
+    #vm count per cloud
+    count_list = db_utils.get_counts(group_name=active_user.active_group)
+
+    #get default limits
+    cloud_limits = db_utils.get_limits(group_name=active_user.active_group)
+
+    #get jobs
+    job_list = db_utils.get_condor_jobs(group_name=active_user.active_group)
+
+    status_list = db_utils.get_cloud_status(group_name=active_user.active_group)
+
+    context = {
+            'active_user': active_user,
+            'active_group': active_user.active_group,
+            'user_groups': user_groups,
+            'cloud_list': cloud_list,
+            'count_list': count_list,
+            'cloud_limits': cloud_limits,
+            'job_list': job_list,
+            'status_list': status_list,
+            #'machine_list': machine_list, #Not yet implemented
+
+        }
+
+    return _render(request, 'csv2/status.html', context)
+
 
 # 
 # This function should recieve a post request with a payload of cloud configuration
 # to add to a given group's pool of resources (group_resources)
 #
-def add_resources(request):
+def update(request):
     if not verifyUser(request):
         raise PermissionDenied
     if not getSuperUserStatus(request):
         raise PermissionDenied
 
+    active_user = getcsv2User(request)
+
     if request.method == 'POST':
-        group_name = request.POST.get('group_name')
+        group_name = active_user.active_group
         cloud_name = request.POST.get('cloud_name')
         authurl = request.POST.get('authurl')
         username = request.POST.get('username')
@@ -68,114 +169,12 @@ def add_resources(request):
         db_utils.put_group_resources(action=action, group=group_name, cloud=cloud_name, url=authurl, uname=username, pword=hashed_pw, keyname=keyname, cacertificate=cacertificate, region=region, user_domain_name= user_domain_name, project_domain_name=project_domain_name, cloud_type=cloud_type, cores_ctl=cores_ctl, ram_ctl=ram_ctl)
 
 
-    context = {
-            'response_code': 0,
-            'message': "Cloud updated"
-    }
+    if action == 'add':
+        message = 'Cloud "%s" added.' % cloud_name
+    elif action == 'delete':
+        message = 'Cloud "%s" deleted.' % cloud_name
+    else:
+        message = 'Cloud "%s" updated.' % cloud_name
 
-    return _render(request, 'csv2/clouds.html', context)
+    list(request, message=message)
 
-
-
-
-#
-# This function generates a the status of a given groups operations
-# VM status, job status, and machine status should all be available for a given group on this page
-#
-def status(request, group_name=None):
-    if not verifyUser(request):
-        raise PermissionDenied
-
-    active_user = getcsv2User(request)
-    user_groups = db_utils.get_user_groups(active_user)
-
-    #check to see if specified group is a valid one for this user, if it is set it as active
-    if request.method == 'POST':
-        group_name = request.POST.get('group_select')
-        if group_name is not None:
-            if group_name in user_groups:
-                active_user.active_group = group_name
-                active_user.save()
-
-    if len(user_groups)==0:
-        # active user isn't registered to any groups, display blank page with msg
-        #TODO#
-        pass
-    # get data based on active group, if no active group pick the first from the list
-    if active_user.active_group is None:
-        active_user.active_group = user_groups[0]
-        active_user.save()
-
-    #get cloud info
-    cloud_list = db_utils.get_group_resources(group_name=active_user.active_group)
-
-    #get vms
-    vm_list = db_utils.get_vms(group_name=active_user.active_group)
-    
-    #vm count per cloud
-    count_list = db_utils.get_counts(group_name=active_user.active_group)
-
-    #get default limits
-    cloud_limits = db_utils.get_limits(group_name=active_user.active_group)
-
-    #get jobs
-    job_list = db_utils.get_condor_jobs(group_name=active_user.active_group)
-
-    status_list = db_utils.get_cloud_status(group_name=active_user.active_group)
-
-
-
-    context = {
-            'active_user': active_user,
-            'active_group': active_user.active_group,
-            'user_groups': user_groups,
-            'cloud_list': cloud_list,
-            'count_list': count_list,
-            'cloud_limits': cloud_limits,
-            'job_list': job_list,
-            'status_list': status_list,
-            #'machine_list': machine_list, #Not yet implemented
-
-        }
-
-    return _render(request, 'csv2/status.html', context)
-
-
-def list(request, group_name=None):
-    if not verifyUser(request):
-        raise PermissionDenied
-
-
-    active_user = getcsv2User(request)
-    user_groups = db_utils.get_user_groups(active_user)
-
-    #check to see if specified group is a valid one for this user, if it is set it as active
-    if request.method == 'POST':
-        group_name = request.POST.get('group_select')
-        if group_name is not None:
-            if group_name in user_groups:
-                active_user.active_group = group_name
-                active_user.save()
-
-    if len(user_groups)==0:
-        # active user isn't registered to any groups, display blank page with msg
-        #TODO#
-        pass
-    # get data based on active group, if no active group pick the first from the list
-    if active_user.active_group is None:
-        active_user.active_group = user_groups[0]
-        active_user.save()
-
-    #get cloud info
-    cloud_list = {'ResultProxy': [dict(r) for r in db_utils.get_group_resources(group_name=active_user.active_group)]}
-
-    context = {
-            'active_user': active_user,
-            'active_group': active_user.active_group,
-            'user_groups': user_groups,
-            'cloud_list': cloud_list,
-            'response_code': 0,
-            'message': None
-        }
-
-    return _render(request, 'csv2/clouds.html', context)
--- web_frontend/cloudscheduler/csv2/urls.py
+++ web_frontend/cloudscheduler/csv2/urls.py
@@ -1,14 +1,19 @@
 from django.conf.urls import url
 
-from . import views, user_views, cloud_views
+from . import views, user_views, cloud_views, group_views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'^user/create', user_views.create, name='create'),
     url(r'^user/delete', user_views.delete, name='delete'),
     url(r'^user/list', user_views.manage, name='manage'),
     url(r'^user/update', user_views.update, name='update'),
-    url(r'^status/', cloud_views.status, name='status'),
-    url(r'^clouds/', cloud_views.list, name='list'),
-    url(r'^cloud/update', cloud_views.add_resources, name='update'),
+
+    url(r'^clouds/',       cloud_views.list,    name='list'),
+    url(r'^cloud/prepare', cloud_views.prepare, name='prepare'),
+    url(r'^$',             cloud_views.status,  name='list'),
+    url(r'^status/',       cloud_views.status,  name='status'),
+    url(r'^cloud/update',  cloud_views.update,  name='update'),
+
+    url(r'^groups/', group_views.list, name='list'),
+    url(r'^group/update', group_views.update, name='update'),
 ]
