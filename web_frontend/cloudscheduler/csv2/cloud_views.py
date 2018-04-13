3fecc3ad6e1da58939189b6411e022cef5af5e56
colsond <colsond@uvic.ca>
2018-04-13 13:28:06 -0700

    provide current cloud, error checking, and simplified slider controls

--- web_frontend/cloudscheduler/csv2/cloud_views.py
+++ web_frontend/cloudscheduler/csv2/cloud_views.py
@@ -14,6 +14,12 @@ from sqlalchemy.sql import select
 from lib.schema import *
 import sqlalchemy.exc
 
+MODIFY_IGNORE_KEYS = (
+    'action',
+    'cores_slider',
+    'csrfmiddlewaretoken',
+    'ram_slider',
+    )
 
 def _db_execute(db_connection, request):
     try:
@@ -25,7 +31,7 @@ def _db_execute(db_connection, request):
         return False, ex
 
 
-def _get_user_group_list(request, db_session, db_map):
+def _set_user_groups(request, db_session, db_map):
     active_user = getcsv2User(request)
     user_groups = db_map.classes.csv2_user_groups
     user_group_rows = db_session.query(user_groups).filter(user_groups.username==active_user)
@@ -34,36 +40,40 @@ def _get_user_group_list(request, db_session, db_map):
         for row in user_group_rows:
             user_groups.append(row.group_name)
 
-    return active_user,user_groups
+    if not user_groups:
+        return 1,'user "%s" is not a member of any group.' % active_user,active_user,user_groups
 
-
-def list(request, group_name=None, response_code=0, message=None):
-    if not verifyUser(request):
-        raise PermissionDenied
-
-
-    # open the database.
-    db_engine,db_session,db_connection,db_map = db_open()
-
-    # Retrieve the active user and associated group list.
-    active_user,user_groups = _get_user_group_list(request, db_session, db_map)
-
-    #check to see if specified group is a valid one for this user, if it is set it as active
+    # if the POST request specified a group, validate and set the specified group as the active group.
     if request.method == 'POST':
         group_name = request.POST.get('group')
         if group_name is not None:
             if group_name in user_groups:
                 active_user.active_group = group_name
                 active_user.save()
+            else:
+                return 1,'cannnot switch to invalid group "%s".' % group_name,active_user,user_groups
 
-    if len(user_groups)==0:
-        # active user isn't registered to any groups, display blank page with msg
-        #TODO#
-        pass
-    # get data based on active group, if no active group pick the first from the list
+    # if no active group, set first group as default.
     if active_user.active_group is None:
         active_user.active_group = user_groups[0]
         active_user.save()
+
+    return 0,None,active_user,user_groups
+
+
+def list(request, cloud=None, group_name=None, response_code=0, message=None, active_user=None, user_groups=None):
+    if not verifyUser(request):
+        raise PermissionDenied
+
+    # open the database.
+    db_engine,db_session,db_connection,db_map = db_open()
+
+    # Retrieve the active user, associated group list and optionally set the active group.
+    if not active_user:
+        response_code,message,active_user,user_groups = _set_user_groups(request, db_session, db_map)
+        if response_code != 0:
+            db_connection.close()
+            return _render(request, 'csv2/clouds.html', {'response_code': 1, 'message': message})
 
     #get cloud info
     s = select([view_group_resources]).where(view_group_resources.c.group_name == active_user.active_group)
@@ -71,11 +81,28 @@ def list(request, group_name=None, response_code=0, message=None):
 
     db_connection.close()
 
+    # Position the page.
+    obj_act_id = request.path.split('/')
+    if cloud:
+        if cloud == '-':
+            current_cloud = ''
+        else:
+            current_cloud = cloud
+    elif len(obj_act_id) > 2 and len(obj_act_id[3]) > 0:
+        current_cloud = str(obj_act_id[3])
+    else:
+        if len(cloud_list['ResultProxy']) > 0:
+            current_cloud = str(cloud_list['ResultProxy'][0]['cloud_name'])
+        else:
+            current_cloud = ''
+
+    # Render the page.
     context = {
             'active_user': active_user,
             'active_group': active_user.active_group,
             'user_groups': user_groups,
             'cloud_list': cloud_list,
+            'current_cloud': current_cloud,
             'response_code': response_code,
             'message': message
         }
@@ -98,6 +125,11 @@ def modify(request):
         # open the database.
         db_engine,db_session,db_connection,db_map = db_open()
 
+        # Retrieve the active user, associated group list and optionally set the active group.
+        response_code,message,active_user,user_groups = _set_user_groups(request, db_session, db_map)
+        if response_code != 0:
+            return list(request, cloud='-', response_code=1, message=message, active_user=active_user, user_groups=user_groups)
+
         # map the field list.
         metadata = MetaData(bind=db_engine)
         table = Table('csv2_group_resources', metadata, autoload=True)
@@ -111,78 +143,43 @@ def modify(request):
                     if key != 'password' or request.POST[key]:
                         values[1][key] = request.POST[key]
             else:
-                if key != 'action' and key != 'csrfmiddlewaretoken':
-                    return list(request, response_code=1, message='cloud modify request contained bad parameter "%s".' % key)
+                if key not in MODIFY_IGNORE_KEYS:
+                    return list(request, cloud='-', response_code=1, message='cloud modify request contained bad parameter "%s".' % key, active_user=active_user, user_groups=user_groups)
 
         if 'group_name' not in values[0]:
             values[0]['group_name'] = getcsv2User(request).active_group
 
+#       print(">>>>> object=cloud, action=%s, values=%s" % (request.POST['action'], values))
+
         if request.POST['action'] == 'add':
-            if(db_session.query(exists().where( \
-                table.c.group_name==values[0]['group_name'] and \
-                table.c.cloud_name==values[0]['cloud_name'] \
-                )).scalar()):
-
-                db_connection.close()
-
-                return list(
-                request,
-                response_code=1,
-                message='request to add existing cloud "%s/%s".' % (values[0]['group_name'], values[0]['cloud_name'])
-                )
+            success,message = _db_execute(db_connection, table.insert().values({**values[0], **values[1]}))
+            db_connection.close()
+            if success:
+                return list(request, cloud=values[0]['cloud_name'], response_code=0, message='cloud "%s/%s" successfully added.' % (values[0]['group_name'], values[0]['cloud_name']), active_user=active_user, user_groups=user_groups)
             else:
-                success,message = _db_execute(db_connection, table.insert().values({**values[0], **values[1]}))
-                db_connection.close()
-                if success:
-                    return list(
-                        request,
-                        response_code=0,
-                        message='cloud "%s/%s" successfully added.' % (values[0]['group_name'], values[0]['cloud_name'])
-                        )
-                else:
-                    return list(
-                        request,
-                        response_code=1,
-                        message='cloud "%s/%s" add failed - %s.' % (values[0]['group_name'], values[0]['cloud_name'], message)
-                        )
+                return list(request, cloud=values[0]['cloud_name'], response_code=1, message='cloud "%s/%s" add failed - %s.' % (values[0]['group_name'], values[0]['cloud_name'], message), active_user=active_user, user_groups=user_groups)
 
         elif request.POST['action'] == 'delete':
             success,message = _db_execute(
                 db_connection,
-                table.delete(table.c.group_name==values[0]['group_name'] and table.c.cloud_name==values[0]['cloud_name'])
+                table.delete((table.c.group_name==values[0]['group_name']) & (table.c.cloud_name==values[0]['cloud_name']))
                 )
             db_connection.close()
             if success:
-                return list(
-                    request,
-                    response_code=0,
-                    message='cloud "%s/%s" successfully deleted.' % (values[0]['group_name'], values[0]['cloud_name'])
-                    )
+                return list(request, cloud=values[0]['cloud_name'], response_code=0, message='cloud "%s/%s" successfully deleted.' % (values[0]['group_name'], values[0]['cloud_name']), active_user=active_user, user_groups=user_groups)
             else:
-                return list(
-                    request,
-                    response_code=1,
-                    message='cloud "%s/%s" delete failed - %s.' % (values[0]['group_name'], values[0]['cloud_name'], message)
-                    )
+                return list(request, cloud=values[0]['cloud_name'], response_code=1, message='cloud "%s/%s" delete failed - %s.' % (values[0]['group_name'], values[0]['cloud_name'], message), active_user=active_user, user_groups=user_groups)
 
         elif request.POST['action'] == 'modify':
-            success,message = _db_execute(db_connection, table.update().where(table.c.group_name==values[0]['group_name'] and table.c.cloud_name==values[0]['cloud_name']).values(values[1]))
+            success,message = _db_execute(db_connection, table.update().where((table.c.group_name==values[0]['group_name']) & (table.c.cloud_name==values[0]['cloud_name'])).values(values[1]))
             db_connection.close()
             if success:
-                return list(
-                    request,
-                    response_code=0,
-                    message='cloud "%s/%s" successfully modified.' % (values[0]['group_name'], values[0]['cloud_name'])
-                    )
+                return list(request, cloud=values[0]['cloud_name'], response_code=0, message='cloud "%s/%s" successfully modified.' % (values[0]['group_name'], values[0]['cloud_name']), active_user=active_user, user_groups=user_groups)
             else:
-                return list(
-                    request,
-                    response_code=1,
-                    message='cloud "%s/%s" modify failed - %s.' % (values[0]['group_name'], values[0]['cloud_name'], message)
-                    )
+                return list(request, cloud=values[0]['cloud_name'], response_code=1, message='cloud "%s/%s" modify failed - %s.' % (values[0]['group_name'], values[0]['cloud_name'], message), active_user=active_user, user_groups=user_groups)
 
         else:
-            return list(request, response_code=1, message='invalid action "%s" specified.' % request.POST['action'])
+            return list(request, cloud=values[0]['cloud_name'], response_code=1, message='invalid action "%s" specified.' % request.POST['action'], active_user=active_user, user_groups=user_groups)
     else:
         if request.method == 'POST':
             return list(request, response_code=1, message='invalid method "%s" specified.' % request.method)
@@ -190,9 +187,6 @@ def modify(request):
             return list(request, response_code=1, message='no action specified.')
         else:
             return list(request, response_code=1, message='no cloud name specified.')
-
-    return list(request, response_code=1, message='internal server error.')
-
 
 def prepare(request):
     """
@@ -219,25 +213,10 @@ def status(request, group_name=None):
     # open the database.
     db_engine,db_session,db_connection,db_map = db_open()
 
-    # Retrieve the active user and associated group list.
-    active_user,user_groups = _get_user_group_list(request, db_session, db_map)
-
-    #check to see if specified group is a valid one for this user, if it is set it as active
-    if request.method == 'POST':
-        group_name = request.POST.get('group')
-        if group_name is not None:
-            if group_name in user_groups:
-                active_user.active_group = group_name
-                active_user.save()
-
-    if len(user_groups)==0:
-        # active user isn't registered to any groups, display blank page with msg
-        #TODO#
-        pass
-    # get data based on active group, if no active group pick the first from the list
-    if active_user.active_group is None:
-        active_user.active_group = user_groups[0]
-        active_user.save()
+    # Retrieve the active user, associated group list and optionally set the active group.
+    response_code,message,active_user,user_groups = _set_user_groups(request, db_session, db_map)
+    if response_code != 0:
+        return _render(request, 'csv2/status.html', {'response_code': 1, 'message': message})
 
     # get vm and job counts per cloud
     s = select([view_cloud_status]).where(view_cloud_status.c.group_name == active_user.active_group)
