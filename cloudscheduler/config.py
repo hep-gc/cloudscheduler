12c5c9fd91fe1c818e091e83951e1bf4a0a39d22
Michael Paterson <mhp@uvic.ca>
2018-04-18 14:23:26 -0700

    make setup static and remove self get log obj

--- cloudscheduler/config.py
+++ cloudscheduler/config.py
@@ -37,13 +37,15 @@ class CSConfig:
         self.default_image = kwargs['job'].get('image', 'cernvm3-micro-2.8-6.hdd')
         self.default_network = kwargs['job'].get('network', 'private')
 
-    def setup(self, config_file="/etc/cloudscheduler/cloudscheduler.yaml"):
+    @staticmethod
+    def setup(config_file="/etc/cloudscheduler/cloudscheduler.yaml"):
         """
         Reading a yaml and stuffing into dictionary for use in the constructor.
 
         :param config_file: the file path where the config is stored on disk
         :return: the config object
         """
+        log = logging.getLogger(__name__)
         # Prime the dict with it's sections:
         lconfig = {'general': {}, 'job': {}}
         # Load up any changed values from file
@@ -54,9 +56,9 @@ class CSConfig:
                 for k, val in config_file.items():
                     lconfig[k] = val
         except FileNotFoundError as ex:
-            self.log.exception(ex)
+            log.exception(ex)
         except yaml.parser.ParserError as ex:
-            self.log.exception(ex)
+            log.exception(ex)
         return lconfig
 
 
