71500b36f1c111786c5d6bf82ae5a384ed8fee06
Michael Paterson <mhp@uvic.ca>
2018-05-09 13:27:21 -0700

    pull request fixes

--- cloudscheduler/vm.py
+++ cloudscheduler/vm.py
@@ -6,16 +6,22 @@ class VM:
     """
     Represents a Virtual Machine.
     """
-    def __init__(self, vmid, hostname=""):
+    def __init__(self, vm):
 
         """
         VM Constructor.
         :param vmid: id of vm
         :param hostname: hostname of vm
         """
-        self.hostname = hostname
-        self.vmid = vmid
-        self.status = "Starting"
+        self.hostname = vm.hostname
+        self.vmid = vm.vmid
+        self.group_name = vm.group_name
+        self.cloud_name = vm.cloud_name
+        self.status = vm.status
+        self.flavor_id = vm.flavor_id
+        self.terminate = vm.terminate
+        self.last_updated = vm.last_updated
+
 
     def __repr__(self):
         return self.hostname
