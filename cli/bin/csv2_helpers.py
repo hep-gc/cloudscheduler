7bf8ce15891fe22b9f1983cfdebce84f4f70bd22
Colin Leavett-Brown <crlb@uvic.ca>
2018-05-25 08:19:17 -0700

    cloudscheduler helpers provides generate_bash_completion_script

---
+++ cli/bin/csv2_helpers.py
@@ -0,0 +1,31 @@
+from csv2_common import check_keys, show_table
+
+import json
+import os
+import shutil
+import yaml
+
+def generate_bash_completion_script(gvar):
+    """
+    List settings.
+    """
+
+    print('_cloudscheduler()\n{\n    local cur prev objects actions\n    COMPREPLY=()\n    ' \
+        'cur="${COMP_WORDS[COMP_CWORD]}"\n    prev="${COMP_WORDS[COMP_CWORD-1]}"\n\n    ' \
+        '#\n    # Complete the following ojects:\n    #\n    ' \
+        'objects="%s"\n\n    #\n    ' \
+        '# For each object, complete the following actions:\n    #\n    case "${prev}" in' % \
+        ' '.join(sorted(gvar['actions'].keys()))
+        )
+
+    for object in sorted(gvar['actions']):
+        print('        %s)\n            actions="%s"\n' \
+            '            COMPREPLY=( $(compgen -W "${actions}" -- ${cur}) )\n' \
+            '            return 0\n            ;;' % \
+            (object, ' '.join(sorted(gvar['actions'][object][1].keys())))
+            )
+
+    print('        *)\n        ;;\n    ' \
+        'esac\n\n    COMPREPLY=($(compgen -W "${objects}" -- ${cur}))\n    ' \
+        'return 0\n}\ncomplete -F _cloudscheduler cloudscheduler'
+        )
