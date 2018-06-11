6b4d8aa4032e5e08bc763eda331849340679361c
Michael Paterson <mhp@uvic.ca>
2018-06-11 14:16:27 -0700

    capture the yaml priority for groups and clouds

--- cloudscheduler/cloudmanager.py
+++ cloudscheduler/cloudmanager.py
@@ -56,7 +56,7 @@ class CloudManager():
                        cloud_yaml.enabled == 1)
             cloud_yaml_list = []
             for yam in cloud_yamls:
-                cloud_yaml_list.append([yam.yaml_name, yam.yaml, yam.mime_type])
+                cloud_yaml_list.append([yam.yaml_name, yam.yaml, yam.mime_type, yam.priority])
             cloud_vms = session.query(cloud_vm).filter(cloud_vm.group_name == self.name,
                                                        cloud_vm.cloud_name == cloud.cloud_name)
             try:
