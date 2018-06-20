0116291e3adebe72769c0e56a3d82ec5525f7448
Colson Driemel <colsond@uvic.ca>
2018-06-20 10:07:48 -0700

    update url to new view name

--- web_frontend/cloudscheduler/glintwebui/keypair_urls.py
+++ web_frontend/cloudscheduler/glintwebui/keypair_urls.py
@@ -4,7 +4,7 @@ from . import keypair_views
 
 urlpatterns = [
     url(r'^(?P<group_name>.+)/$', keypair_views.manage_keys, name='manage_keys'),
-    url(r'^', views.manage_keys, name='manage_keys'),
+    url(r'^', keypair_views.manage_keys, name='manage_keys'),
     url(r'^save_keypairs/(?P<group_name>.+)/$', keypair_views.save_keypairs, name='save_keypairs'),
     url(r'^add_key/$', keypair_views.upload_keypair, name='upload_keypair'),
     url(r'^new_key/$', keypair_views.new_keypair, name='new_keypair')
