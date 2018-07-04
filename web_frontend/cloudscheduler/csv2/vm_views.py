754a96543a5ce513d4295d201dd1d1fb46f323e8
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2018-07-04 11:43:46 -0700

    Removed uneeded checks and logic for vm_select

--- web_frontend/cloudscheduler/csv2/vm_views.py
+++ web_frontend/cloudscheduler/csv2/vm_views.py
@@ -126,65 +126,62 @@ def update(request):
         raise PermissionDenied
 
     if request.method == 'POST' and 'vm_option' in request.POST:
-        if 'vm_selected' in request.POST:
-            # open the database.
-            db_engine, db_session, db_connection, db_map = db_ctl = db_open()
+        # open the database.
+        db_engine, db_session, db_connection, db_map = db_ctl = db_open()
 
-            # Retrieve the active user, associated group list and optionally set the active group.
-            rc, msg, active_user, user_groups = set_user_groups(request, db_ctl)
-            if rc != 0:
-                db_close(db_ctl)
-                return list(request, response_code=1, message='%s %s' % (lno('CV12'), msg), active_user=active_user, user_groups=user_groups)
+        # Retrieve the active user, associated group list and optionally set the active group.
+        rc, msg, active_user, user_groups = set_user_groups(request, db_ctl)
+        if rc != 0:
+            db_close(db_ctl)
+            return list(request, response_code=1, message='%s %s' % (lno('CV12'), msg), active_user=active_user, user_groups=user_groups)
 
-            # Validate input fields.
-            rc, msg, fields, tables, columns = validate_fields(request, [VM_KEYS], db_ctl, ['csv2_vms,n', 'condor_machines,n'], active_user)
-            if rc != 0:
-                db_close(db_ctl)
-                return list(request, response_code=1, message='%s cloud update %s' % (lno('VV00'), msg), active_user=active_user, user_groups=user_groups)
+        # Validate input fields.
+        rc, msg, fields, tables, columns = validate_fields(request, [VM_KEYS], db_ctl, ['csv2_vms,n', 'condor_machines,n'], active_user)
+        if rc != 0:
+            db_close(db_ctl)
+            return list(request, response_code=1, message='%s cloud update %s' % (lno('VV00'), msg), active_user=active_user, user_groups=user_groups)
 
+        if fields['vm_option'] == 'kill':
+            table = tables['csv2_vms']
+            verb = 'killed'
+        elif fields['vm_option'] == 'retire':
+            table = tables['condor_machines']
+            verb = 'retired'
+        elif fields['vm_option'] == 'manctl':
+            table = tables['csv2_vms']
+            verb = 'set to manual control'
+        elif fields['vm_option'] == 'sysctl':
+            table = tables['csv2_vms']
+            verb = 'set to system control'
+        else:
+            return list(request, response_code=1, message='%s vm update, option "%s" is invalid.' % (lno('GV41'), fields['vm_option']))
+
+        # Retrieve VM information.
+        s = select([view_vms]).where((view_vms.c.group_name == active_user.active_group) and (view_vms.c.foreign_vm == 0))
+        vm_list = qt(db_connection.execute(s), filter=qt_filter_get(['cloud_name', 'hostname', 'poller_status'], fields, aliases=ALIASES))
+
+        count = 0
+        for vm in vm_list:
             if fields['vm_option'] == 'kill':
-                table = tables['csv2_vms']
-                verb = 'killed'
+                update = table.update().where(table.c.vmid == vm['vmid']).values({'terminate': 1})
             elif fields['vm_option'] == 'retire':
-                table = tables['condor_machines']
-                verb = 'retired'
+                update = table.update().where(table.c.hostname == vm['hostname']).values({'condor_off': 1})
             elif fields['vm_option'] == 'manctl':
-                table = tables['csv2_vms']
-                verb = 'set to manual control'
+                update = table.update().where(table.c.vmid == vm['vmid']).values({'manual_control': 1})
             elif fields['vm_option'] == 'sysctl':
-                table = tables['csv2_vms']
-                verb = 'set to system control'
+                update = table.update().where(table.c.vmid == vm['vmid']).values({'manual_control': 0})
+
+            rc, msg = db_execute(db_ctl, table.update().where(table.c.vmid == vm['vmid']).values(control), allow_no_rows=True)
+            if rc == 0:
+                count += msg
             else:
-                return list(request, response_code=1, message='%s vm update, option "%s" is invalid.' % (lno('GV41'), fields['vm_option']))
+                db_close(db_ctl)
+                return list(request, response_code=1, message='%s VM update (%s) failed - %s' % (lno('GV41'), fields['vm_option'], msg))
 
-            # Retrieve VM information.
-            s = select([view_vms]).where((view_vms.c.group_name == active_user.active_group) and (view_vms.c.foreign_vm == 0))
-            vm_list = qt(db_connection.execute(s), filter=qt_filter_get(['cloud_name', 'hostname', 'poller_status'], fields, aliases=ALIASES))
-
-            count = 0
-            for vm in vm_list:
-                if fields['vm_option'] == 'kill':
-                    update = table.update().where(table.c.vmid == vm['vmid']).values({'terminate': 1})
-                elif fields['vm_option'] == 'retire':
-                    update = table.update().where(table.c.hostname == vm['hostname']).values({'condor_off': 1})
-                elif fields['vm_option'] == 'manctl':
-                    update = table.update().where(table.c.vmid == vm['vmid']).values({'manual_control': 1})
-                elif fields['vm_option'] == 'sysctl':
-                    update = table.update().where(table.c.vmid == vm['vmid']).values({'manual_control': 0})
-
-                rc, msg = db_execute(db_ctl, table.update().where(table.c.vmid == vm['vmid']).values(control), allow_no_rows=True)
-                if rc == 0:
-                    count += msg
-                else:
-                    db_close(db_ctl)
-                    return list(request, response_code=1, message='%s VM update (%s) failed - %s' % (lno('GV41'), fields['vm_option'], msg))
-
-            db_close(db_ctl, commit=True)
-            return list(request, response_code=0, message='vm update, VMs %s=%s.' % (verb, count))
+        db_close(db_ctl, commit=True)
+        return list(request, response_code=0, message='vm update, VMs %s=%s.' % (verb, count))
 
     ### Bad request.
-        else:
-            return list(request, response_code=1, message='%s vm update, no VMs were selected to perform the operation %s.' % (lno('CV15'), request.POST['vm_option']))
     else:
         if request.method != 'POST':
             return list(request, response_code=1, message='%s cloud update, invalid method "%s" specified.' % (lno('CV15'), request.method))
