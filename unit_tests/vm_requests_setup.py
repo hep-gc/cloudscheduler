1ba85164b420c4add3b2a5973f19d4d1d01a6c50
Jodie Weldon <jodiew@uvic.ca>
2018-07-06 12:51:26 -0700

    Add vm tests

---
+++ unit_tests/vm_requests_setup.py
@@ -0,0 +1,84 @@
+from unit_test_common import execute_csv2_request, initialize_csv2_request, ut_id, generate_secret
+import sys
+import vm_requests_cleanup
+
+def main(gvar, user_secret):
+    if not gvar:
+        gvar = {}
+        if len(sys.argv) > 1:
+            initialize_csv2_request(gvar, sys.argv[0], selections=sys.argv[1])
+        else:
+            initialize_csv2_request(gvar, sys.argv[0])
+    if not user_secret:
+        user_secret = generate_secret()
+    
+    vm_requests_cleanup.main(gvar)
+
+    # unprivileged user in no groups
+    execute_csv2_request(
+        gvar, 0, None, 'user "{}" successfully added.'.format(ut_id(gvar, 'vtu1')),
+        '/user/add/', form_data={
+            'username': ut_id(gvar, 'vtu1'),
+            'password1': user_secret,
+            'password2': user_secret,
+            'cert_cn': ut_id(gvar, 'vm test user one')
+        }
+    )
+    
+    # privileged user in no groups
+    execute_csv2_request(
+        gvar, 0, None, 'user "{}" successfully added.'.format(ut_id(gvar, 'vtu2')),
+        '/user/add/', form_data={
+            'username': ut_id(gvar, 'vtu2'),
+            'password1': user_secret,
+            'password2': user_secret,
+            'cert_cn': ut_id(gvar, 'vm test user two'),
+            'is_superuser': 1
+        }
+    )
+
+    # group with users
+    execute_csv2_request(
+        gvar, 0, None, 'group "{}" successfully added.'.format(ut_id(gvar, 'vtg1')),
+        '/group/add/', form_data={
+            'group_name': ut_id(gvar, 'vtg1'),
+            'condor_central_manager': 'unit-test-group-one.ca'
+        }
+    )
+
+    # group without users
+    execute_csv2_request(
+        gvar, 0, None, 'group "{}" successfully added.'.format(ut_id(gvar, 'vtg2')),
+        '/group/add/', form_data={
+            'group_name': ut_id(gvar, 'vtg2'),
+            'condor_central_manager': 'unit-test-group-two.ca'
+        }
+    )
+
+    # unprivileged user in vtg1
+    execute_csv2_request(
+        gvar, 0, None, 'user "{}" successfully added.'.format(ut_id(gvar, 'vtu3')),
+        '/user/add/', form_data={
+            'username': ut_id(gvar, 'vtu3'),
+            'password1': user_secret,
+            'password2': user_secret,
+            'cert_cn': ut_id(gvar, 'vm test user three'),
+            'group_name.1': ut_id(gvar, 'vtg1')
+        }
+    )
+    
+    # privileged user in vtg1
+    execute_csv2_request(
+        gvar, 0, None, 'user "{}" successfully added.'.format(ut_id(gvar, 'vtu4')),
+        '/user/add/', form_data={
+            'username': ut_id(gvar, 'vtu4'),
+            'password1': user_secret,
+            'password2': user_secret,
+            'cert_cn': ut_id(gvar, 'vm test user four'),
+            'is_superuser': 1,
+            'group_name.1': ut_id(gvar, 'vtg1')
+        }
+    )
+
+if __name__ == "__main__":
+    main(None)
