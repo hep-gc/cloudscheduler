71de979ed5fa87cc1501ff957b52bba3ed02f6f7
Jodie Weldon <jodiew@uvic.ca>
2018-07-16 13:50:04 -0700

    Fix 404s in vm update tests

--- unit_tests/test_vm_update.py
+++ unit_tests/test_vm_update.py
@@ -18,50 +18,51 @@ def main(gvar, user_secret):
     )
 
     execute_csv2_request(
-        gvar, 2, None, 'HTTP response code 404.',
+        gvar, 1, None, 'user "{}" is not a member of any group.'.format(ut_id(gvar, 'vtu1')),
         '/vm/update/',
         server_user=ut_id(gvar, 'vtu1'), server_pw=user_secret
     )
 
     execute_csv2_request(
-        gvar, 1, None, 'user "{}" is not a member of any group.'.format(ut_id(gvar, 'vtu1')),
-        '/vm/update/invalid-unit-test::invalid-unit-test::invalid-unit-test',
-        server_user=ut_id(gvar, 'vtu1'), server_pw=user_secret
-    )
-
-    execute_csv2_request(
         gvar, 1, None, 'user "{}" is not a member of any group.'.format(ut_id(gvar, 'vtu2')),
-        '/vm/update/invalid-unit-test::invalid-unit-test::invalid-unit-test',
+        '/vm/update/',
         server_user=ut_id(gvar, 'vtu2'), server_pw=user_secret
     )
 
     execute_csv2_request(
-        gvar, 1, 'VV##', 'invalid method "GET" specified.',
-        '/vm/update/invalid-unit-test::invalid-unit-test::invalid-unit-test',
+        gvar, 1, 'VV05', 'invalid method "GET" specified.',
+        '/vm/update/',
         server_user=ut_id(gvar, 'vtu3'), server_pw=user_secret
     )
 
     execute_csv2_request(
-        gvar, 1, 'VV##', 'request contained a bad parameter "invalid-unit-test".',
-        '/vm/update/invalid-unit-test::invalid-unit-test::invalid-unit-test', form_data={'invalid-unit-test': 'invalid-unit-test'},
+        gvar, 1, 'VV06', 'the vm-option is required and must be one of the following: [\'kill\', \'retire\', \'manctl\', \'sysctl\'].',
+        '/vm/update/', form_data={'invalid-unit-test': 'invalid-unit-test'},
         server_user=ut_id(gvar, 'vtu3'), server_pw=user_secret
     )
 
     execute_csv2_request(
         gvar, 1, None, 'cannot switch to invalid group "invalid-unit-test".',
-        '/vm/update/invalid-unit-test::invalid-unit-test::invalid-unit-test', form_data={'group': 'invalid-unit-test'},
+        '/vm/update/', form_data={'group': 'invalid-unit-test'},
         server_user=ut_id(gvar, 'vtu3'), server_pw=user_secret
     )
 
     execute_csv2_request(
         gvar, 1, None, 'cannot switch to invalid group "{}".'.format(ut_id(gvar, 'vtg2')),
-        '/vm/update/invalid-unit-test::invalid-unit-test::invalid-unit-test', form_data={'group': ut_id(gvar, 'vtg2')},
+        '/vm/update/', form_data={'group': ut_id(gvar, 'vtg2')},
         server_user=ut_id(gvar, 'vtu3'), server_pw=user_secret
     )
 
     execute_csv2_request(
-        gvar, 0, None, None,
-        '/vm/update/invalid-unit-test::invalid-unit-test::invalid-unit-test', form_data={'group': ut_id(gvar, 'vtg1')},
+        gvar, 1, 'VV06', "the vm-option is required and must be one of the following: ['kill', 'retire', 'manctl', 'sysctl'].",
+        '/vm/update/', form_data={'group': ut_id(gvar, 'vtg1'), 'vm-option': 'invalid-unit-test'},
+        server_user=ut_id(gvar, 'vtu3'), server_pw=user_secret
+    )
+
+    # TODO: come back to this later, I don't think the functionality is there yet
+    execute_csv2_request(
+        gvar, 1, 'VV06', "the vm-option is required and must be one of the following: ['kill', 'retire', 'manctl', 'sysctl'].",
+        '/vm/update/', form_data={'vm-option': 'sysctl', 'invalid-unit-test': 'invalid-unit-test'},
         server_user=ut_id(gvar, 'vtu3'), server_pw=user_secret
     )
 
