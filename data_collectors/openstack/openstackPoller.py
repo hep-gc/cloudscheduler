88987067f06f46fb90bd0ef0e6c55263b6aa9b54
Colin Leavett-Brown <crlb@uvic.ca>
2018-08-24 13:23:48 -0700

    fix attribute specification

--- data_collectors/openstack/openstackPoller.py
+++ data_collectors/openstack/openstackPoller.py
@@ -44,7 +44,7 @@ def delete_obsolete_items(type, inventory, db_session, base_class, item_key):
 
             uncommitted_updates = 0
             for item in obsolete_items:
-                if item.item_key not in inventory[group_name][cloud_name]:
+                if item[item_key] not in inventory[group_name][cloud_name]:
                     logging.info("Cleaning up %s: %s from group:cloud - %s::%s" % (type, item.item_key, item.group_name, item.cloud_name))
                     try:
                         db_session.delete(item)
