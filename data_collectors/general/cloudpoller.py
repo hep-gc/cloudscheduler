b077cc6f01858fa79d111ea1dbd24b6b598dcf60
Colson Driemel <colsond@uvic.ca>
2018-10-05 09:55:39 -0700

    add Provider.OPENSTACK for keystone v3 connections

--- data_collectors/general/cloudpoller.py
+++ data_collectors/general/cloudpoller.py
@@ -3,31 +3,38 @@ import logging
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 
+from libcloud.common.openstack_identity import OpenStackIdentity_3_0_Connection
+from libcloud.common.openstack_identity import OpenStackIdentityTokenScope
+
 
 
 def get_openstack_driver(cloud):
     Openstack = get_driver(Provider.OPENSTACK)
 
-    #authsplit = cloud.authurl.split('/')
-    #try:
-    #    version = int(float(authsplit[-1][1:])) if len(authsplit[-1]) > 0 else int(float(authsplit[-2][1:]))
-    #except ValueError:
-    #    logging.error("Bad OpenStack URL, could not determine version, skipping %s", cloud.authurl)
-    #    return False
-    #if version == 2:
-    #    driver = Openstack(
-    #        cloud.username, cloud.password,
-    #        ex_force_base_url=cloud.authurl,
-    #        ex_force_auth_version=version, 
-    #        ex_tenant_name=cloud.project
-    #    )
-    #else:
-    driver = Openstack(
-        cloud.username, cloud.password,
-        ex_force_base_url=cloud.authurl,
-        ex_force_auth_version=version,
-        ex_tenant_name=cloud.project
-    )
+    authsplit = cloud.authurl.split('/')
+    try:
+        version = int(float(authsplit[-1][1:])) if len(authsplit[-1]) > 0 else int(float(authsplit[-2][1:]))
+    except ValueError:
+        logging.error("Bad OpenStack URL, could not determine version, skipping %s", cloud.authurl)
+        return False
+    if version == 2:
+        driver = Openstack(
+            cloud.username, cloud.password,
+            ex_force_base_url=cloud.authurl,
+            ex_force_auth_version=version, 
+            ex_tenant_name=cloud.project
+        )
+    else:
+        version = '3.x_password'
+        cls = get_driver(Provider.OPENSTACK)
+        driver = cls(
+            cloud.username, cloud.password,
+            ex_force_auth_version=version,
+            ex_force_auth_url=cloud.authurl,
+            ex_force_service_type='compute',
+            ex_force_service_region='Victoria',
+            ex_tenant_name=cloud.project
+        )
     return driver
 
 class Cloud:
@@ -41,7 +48,7 @@ class Cloud:
 
 
 
-test_cloud = Cloud()
+test_cloud = Cloud("colsond", "", "https://otter.heprc.uvic.ca:15000/v3/", "testing")
 os_drvr = get_openstack_driver(test_cloud)
 
 
