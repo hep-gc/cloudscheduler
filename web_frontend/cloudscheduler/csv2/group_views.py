fd671a3f6855baa522df6d92c7e42b88e1e94f07
Kevin Casteels <kcasteels@gmail.com>
2018-12-04 13:04:04 -0800

    Fixed group switching on Defaults page

--- web_frontend/cloudscheduler/csv2/group_views.py
+++ web_frontend/cloudscheduler/csv2/group_views.py
@@ -237,7 +237,7 @@ def defaults(request):
     # Retrieve the active user, associated group list and optionally set the active group.
     rc, msg, active_user, user_groups = set_user_groups(config, request)
     if rc == 0:
-        if request.method == 'POST':
+        if request.method == 'POST' and 'group' not in request.POST:
                 # Validate input fields.
                 rc, msg, fields, tables, columns = validate_fields(config, request, [GROUP_DEFAULTS_KEYS], ['csv2_group_defaults'], active_user)
                 
