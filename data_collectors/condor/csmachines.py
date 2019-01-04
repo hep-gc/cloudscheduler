819f40a1a453467e8561f8cff7274a960f4d7f34
root <root@csv2-dev2.heprc.uvic.ca>
2019-01-04 09:59:30 -0800

    fix access to query results in csmachines

--- data_collectors/condor/csmachines.py
+++ data_collectors/condor/csmachines.py
@@ -308,13 +308,22 @@ def command_poller():
                 # Query database for machines to be retired.
                 abort_cycle = False
                 for resource in db_session.query(view_condor_host).filter(view_condor_host.c.condor_host==condor_host, view_condor_host.c.retire==1):
-                    logging.info("Retiring machine %s" % resource.name)
+                    # Since we are querying a view we dont get an automapped object and instead get a 'result' tuple of the following format
+                    #index=attribute
+                    #0=group
+                    #1=cloud
+                    #2=vmid
+                    #3=retireflag
+                    #4=terminate flag
+                    #5=machine
+                    #6=condor_host
+                    logging.info("Retiring machine %s" % resource[5])
                     try:
-                        condor_classad = condor_session.query(master_type, 'Name=="%s"' % resource.machine)[0]
+                        condor_classad = condor_session.query(master_type, 'Name=="%s"' % resource[5])[0]
                         master_result = htcondor.send_command(condor_classad, htcondor.DaemonCommands.DaemonsOffPeaceful)
 
                         #get vm entry and update retire = 2
-                        vm_row = db.session.query(VM).filter(VM.group_name==resource.group_name, VM.cloud_name==resource.cloud_name, VM.vmid==resource.vmid)[0]
+                        vm_row = db.session.query(VM).filter(VM.group_name==resource[0], VM.cloud_name==resource[1], VM.vmid==resource[2])[0]
                         vm_row.retire=2
                         db_session.merge(vm_row)
                         uncommitted_updates = uncommitted_updates + 1
@@ -330,7 +339,7 @@ def command_poller():
 
                     except Exception as exc:
                         logging.error(exc)
-                        logging.exception("Failed to issue DaemonsOffPeacefull to  machine: %s, skipping..." % resource.name)
+                        logging.exception("Failed to issue DaemonsOffPeacefull to  machine: %s, skipping..." % resource[5])
                         continue
 
             if uncommitted_updates > 0:
