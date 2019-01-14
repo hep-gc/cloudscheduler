d0871b5aaefb029aff1649017cfb6ed597e35bf4
root <root@csv2-dev.heprc.uvic.ca>
2019-01-14 14:52:27 -0800

    fix bug in vm list that raised a permission denied unless you were a super user

--- web_frontend/cloudscheduler/csv2/vm_views.py
+++ web_frontend/cloudscheduler/csv2/vm_views.py
@@ -81,7 +81,7 @@ def list(
         return render(request, 'csv2/clouds.html', {'response_code': 1, 'message': message})
 
     # Retrieve the active user, associated group list and optionally set the active group.
-    rc, msg, active_user, user_groups = set_user_groups(config, request)
+    rc, msg, active_user, user_groups = set_user_groups(config, request, super_user=False)
     if rc != 0:
         config.db_close()
         return render(request, 'csv2/clouds.html', {'response_code': 1, 'message': msg})
