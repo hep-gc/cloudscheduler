3c1caf5c694f2a42869ef55b8f11670e0c412444
root <root@csv2-dev.heprc.uvic.ca>
2019-01-17 11:37:24 -0800

    settings prepare now returns server version

--- web_frontend/cloudscheduler/cloudscheduler_web/settings.py
+++ web_frontend/cloudscheduler/cloudscheduler_web/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from cloudscheduler.lib.db_config import *
-config = Config('/etc/cloudscheduler/cloudscheduler.yaml', 'web_frontend', db_config_dict=True)
+config = Config('/etc/cloudscheduler/cloudscheduler.yaml', 'web_frontend', db_config_dict=True, pool_size=0, max_overflow=10)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
