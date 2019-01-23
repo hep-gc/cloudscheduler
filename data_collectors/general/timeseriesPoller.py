132d1f6b5a4134ed5804a30675c15cde43492cc2
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2019-01-23 15:06:29 -0800

    timeseries poller gets cloud status values from db and formats them

---
+++ data_collectors/general/timeseriesPoller.py
@@ -0,0 +1,117 @@
+import multiprocessing
+from multiprocessing import Process
+import logging
+import time
+import sys
+import os
+import inspect
+
+from cloudscheduler.lib.db_config import *
+from cloudscheduler.lib.ProcessMonitor import ProcessMonitor
+from cloudscheduler.lib.schema import view_cloud_status
+from cloudscheduler.lib.schema import view_job_status
+
+from cloudscheduler.lib.poller_functions import start_cycle, wait_cycle
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+
+
+def timeseries_data_transfer():
+
+	multiprocessing.current_process().name = "Time Series Poller"
+
+	# Variable setup goes here for presistant data like cycle times and configuration
+	# You will need to define new poll times for whatever you decide to call this file in csv2_configuration
+	# A new row will also need to be added to csv2_system_status to track any crashes/errors that occur in this file
+	# once that new row is added you will need to replace "N/A" with the name of the column for
+	# "orange_count_row" in ProccessMonitor initialization in __main__
+
+
+	config = Config('/etc/cloudscheduler/cloudscheduler.yaml', os.path.basename(sys.argv[0]))
+	#config.db_open()
+	#db_session = config.db_session
+
+
+	cycle_start_time = 0
+	new_poll_time = 0
+	poll_time_history = [0,0,0,0]
+
+
+	while True:
+		try:
+			new_poll_time, cycle_start_time = start_cycle(new_poll_time, cycle_start_time)
+
+			#DO ALL THE THINGS
+			config.db_open()
+			db_session = config.db_session
+			
+			cloud_status = db_session.query(view_cloud_status)
+			column_list = [item["name"] for item in cloud_status.column_descriptions]
+
+			job_status = db_session.query(view_job_status)
+			job_column_list = ["jobs","jobs_idle","jobs_running","jobs_complete","jobs_held","jobs_other"]
+			points = []
+			
+			for line in cloud_status:
+				column = 0
+				group = line[0]
+				cloud = line[1]
+				for data in line[2:]:
+					print("{0},cloud={1},group={2}, value={3}".format(column_list[column], cloud, group, data))
+					column += 1
+			
+			for line in job_status:
+				column = 0
+				group = line[0]
+				for data in line[1:]:
+					print("{0},group={1}, value={2}".format(job_column_list[column], group, data))
+					column += 1
+
+
+
+			config.db_close()
+			del db_session
+
+
+
+			wait_cycle(cycle_start_time, poll_time_history, config.sleep_interval_status)
+
+
+		except Exception as exc:
+			logging.error("Error during general operations:")
+			logging.error(exc)
+			logging.error("Exiting...")
+			exit(1)
+
+	return None
+
+
+if __name__ == '__main__':
+
+	process_ids = {
+		'timeseries data transfer': timeseries_data_transfer,
+	}
+	
+	procMon = ProcessMonitor(file_name=os.path.basename(sys.argv[0]), pool_size=3, orange_count_row='csv2_timeseries_error_count', process_ids=process_ids)
+	config = procMon.get_config()
+	logging = procMon.get_logging()
+
+	logging.info("**************************** starting timeseries data poller *********************************")
+
+	# Wait for keyboard input to exit
+	try:
+		#start processes
+		procMon.start_all()
+		while True:
+			procMon.check_processes()
+			time.sleep(config.sleep_interval_main_long)
+
+	except (SystemExit, KeyboardInterrupt):
+		logging.error("Caught KeyboardInterrupt, shutting down threads and exiting...")
+
+	except Exception as ex:
+		logging.exception("Process Died: %s", ex)
+
+	procMon.join_all()
