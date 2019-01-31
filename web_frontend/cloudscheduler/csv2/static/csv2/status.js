0bae32b7f967a3a3121fa04831807719db64dddf
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2019-01-31 15:41:20 -0800

    added time range selection to plot

--- web_frontend/cloudscheduler/csv2/static/csv2/status.js
+++ web_frontend/cloudscheduler/csv2/static/csv2/status.js
@@ -1,23 +1,65 @@
-var date = new Date();
-date.setMilliseconds(0);
-var x = date.getHours();
-date.setHours(x+8);
 
-
-
-/* Add click events to table elements*/
+/* Add click events to table elements and range selection*/
 function addEventListeners(className) {
 	var inputList = document.getElementsByClassName(className);
 		var i;
 		for (i = 0; i < inputList.length; i++){
 			inputList[i].addEventListener('click', function(){
-		    		togglePlot(this);
+				if(className == 'plottable') togglePlot(this);
+				else selectRange(this);
 			});
 		}
 }
 
+function dropDown(){
+	document.getElementById("range-select").classList.toggle("selected");
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+/* Change time range for plot*/
+function selectRange(range){
+	const curr_range = document.getElementsByClassName("range-value");
+	curr_range[0].firstChild.data = range.firstChild.data;
+	const dropdowns = document.getElementsByClassName("range");
+	for (var i = 0; i < dropdowns.length; i++) {
+		if (dropdowns[i].classList.contains('selected')) {
+			dropdowns[i].classList.remove('selected');
+      		}
+	}
+	range.classList.toggle("selected");
+	const curr = Date.now();
+	var to = new Date();
+	var from = new Date();
+	var multiple = 60000;
+	if(range.parentElement.classList.contains("days")) multiple = 86400000;
+	to.setTime(curr-(range.dataset.to*multiple));
+	from.setTime(curr-(range.dataset.from*multiple));
+	TSPlot.layout.xaxis.range = [from, to];
+	Plotly.relayout('plotly-TS', TSPlot.layout);
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+	if (!event.target.matches('.range-btn') && document.getElementsByClassName("dropdown-content")[0].classList.contains('show')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var k = false;
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+				k = true;
+	      		}
+    		}
+		if(k == true){
+			document.getElementById("range-select").classList.toggle("selected");
+		}
+	}
+}
+
 /* Toggle traces, show plot*/
 function togglePlot(trace){
+	console.log(trace.dataset.path);
 	if(TSPlot.showing == true){
 		/* Check if trace is already plotted*/
 		var x;
@@ -29,14 +71,12 @@ function togglePlot(trace){
 			}
 		}
 		if(index != -1){
-			/* Remove trace if it is already plotted*/
 			if(TSPlot.traces.length == 1) {
 				TSPlot.hide();
 			}else{
 				Plotly.deleteTraces('plotly-TS', index);
 			}
 		}else{
-			/* Add trace to plot*/
 			getTraceData(trace, true);
 		}
 	}else{
@@ -44,27 +84,24 @@ function togglePlot(trace){
 		/* Create plot*/
 		TSPlot.show();
 		getTraceData(trace, false);
-		//TSPlot.initialize(newtrace);
 	}
 	
 }
 
 /* Fetch trace data from db and add to plot*/
 function getTraceData(trace, showing){
-	var line = trace.dataset.path.split(" ");
-	var group = line[0];
-	var measurement;
-	var query;
+	const line = trace.dataset.path.split(" ");
+	const group = line[0];
 	if(line.length == 3){
 		var cloud = line[1];
-		measurement = line[2];
-		query = `SELECT * FROM "${measurement}" WHERE "cloud"='${cloud}' AND "group"='${group}'`;
+		var measurement = line[2];
+		var query = `SELECT time,value FROM "${measurement}" WHERE "cloud"='${cloud}' AND "group"='${group}'`;
 	}else{
-		measurement = line[1];
-		query = `SELECT * FROM "${measurement}" WHERE "group"='${group}'`;
+		var cloud = 'total';
+		var measurement = line[1];
+		var query = `SELECT time,value FROM "${measurement}" WHERE "group"='${group}'`;
 	}
-	console.log(query);
-	var csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+	const csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 	fetch('cloud/status/plot',{
 		method: 'POST',
 		headers: {'Accept': 'application/json', 'X-CSRFToken': csrftoken},
@@ -76,22 +113,20 @@ function getTraceData(trace, showing){
 		return response.json();
 	})
 	.then(function(data){
-		console.log(data);
-		var responsedata = data.results[0].series[0].values;
-		console.log(responsedata);
+		const responsedata = data.results[0].series[0].values;
 		const unpackData = (arr, index) => {
 			var newarr = arr.map(x => x[index]);
 			return newarr
 		}
-	    	const aTrace = {
+	    	const newtrace = {
 			type: 'scatter',
-			mode: 'lines+markers',
+			mode: 'lines',
 			name: trace.dataset.path,
 			x: unpackData(responsedata, 0),
-			y: unpackData(responsedata, responsedata[0].length-1)
+			y: unpackData(responsedata, 1)
 		}
-		if(showing == true) return Plotly.addTraces('plotly-TS', aTrace);
-		else return TSPlot.initialize(aTrace);
+		if(showing == true) return Plotly.addTraces('plotly-TS', newtrace);
+		else return TSPlot.initialize(newtrace);
 	})
 	.catch(error => console.log('Error:', error));
 	
@@ -108,13 +143,12 @@ var TSPlot = {
 		},
 		xaxis: {
 			type: 'date',
-			precision: 'seconds'
 		},
 		margin: {
 			l: 50,
 			r: 50,
-			t: 50,
-			b: 50
+			t: 40,
+			b: 40
 		}
 	},
 
@@ -123,6 +157,10 @@ var TSPlot = {
 
 	/* Create new plot with trace in div*/
 	initialize: function(trace) {
+		var to = Date.now();
+		var from = new Date();
+		from.setTime(to-43200000);
+		TSPlot.layout.xaxis.range = [from, to];
 		TSPlot.traces = [trace];		
 		Plotly.newPlot('plotly-TS', TSPlot.traces, TSPlot.layout, {responsive: true, displayModeBar: false});	
 	},
