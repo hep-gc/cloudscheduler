c191d30677ee6b654def0d1bb3487039601eecff
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2019-02-06 12:39:17 -0800

    page refreshes every 30s, keeps plot open and updates with newest data point

--- web_frontend/cloudscheduler/csv2/static/csv2/status.js
+++ web_frontend/cloudscheduler/csv2/static/csv2/status.js
@@ -1,5 +1,11 @@
 var date = Date.now();
 
+function initialize(){
+	/*Add event listeners once page loads*/
+	addEventListeners("plottable");
+	addEventListeners("range");
+}
+
 /* Add click events to table elements and range selection*/
 function addEventListeners(className) {
 	var inputList = document.getElementsByClassName(className);
@@ -79,6 +85,11 @@ function togglePlot(trace){
 				TSPlot.hide();
 			}else{
 				Plotly.deleteTraces('plotly-TS', index);
+				var trace_array = [];
+				for(var y = 0; y < TSPlot.traces.length; y++){
+					trace_array.push(TSPlot.traces[y].name)
+				}
+				sessionStorage.setItem("traces", JSON.stringify(trace_array));
 			}
 		}else{
 			getTraceData(trace, true);
@@ -142,8 +153,67 @@ function getTraceData(trace, showing){
 		}
 		if(showing == true) return Plotly.addTraces('plotly-TS', newtrace);
 		else return TSPlot.initialize(newtrace);
+	}).then(function(data){
+		if(showing == true){
+			var trace_array = [];
+			for(var y = 0; y < TSPlot.traces.length; y++){
+				trace_array.push(TSPlot.traces[y].name)
+			}
+			sessionStorage.setItem("traces", JSON.stringify(trace_array));
+		}
 	})
 	.catch(error => console.log('Error:', error));
+	
+}
+
+/* Refresh status table every 30s*/
+/*var TimerSwitch = 1;
+function set_refresh(time) {
+	TimerSwitch = 1;
+	setTimeout(function() {
+		refresh_plot();
+		fetch(location.href,{	
+			method: 'GET',
+			headers: {'Accept': 'text/html'},
+		})	
+		.then((response) => response.text())
+		.then((html) => {
+			var parser = new DOMParser();
+			var doc = parser.parseFromString(html, "text/html");
+			document.getElementById("status").innerHTML = doc.getElementById("status").innerHTML;
+			checkForPlottedTraces();
+			TimerVal = 30;
+			set_refresh(30000);
+		})
+		.catch((error) => {
+			console.warn(error);
+		});
+	
+	},time);
+	document.getElementById('vms-iframe').src='';
+}
+
+function stop_refresh() {
+	TimerSwitch = 0;
+	window.stop();
+}
+
+function pass_url(url) {
+	stop_refresh();
+	document.getElementById('vms-iframe').src='/vm/list/'+url;
+}*/
+
+function checkForPlottedTraces(){
+	if (typeof(Storage) !== "undefined"){
+		if(sessionStorage.length != 0){
+			var plotted_traces = JSON.parse(sessionStorage.getItem("traces"));
+			for(var x = 0; x < plotted_traces.length; x++){
+				var stat = document.querySelectorAll('td[data-path="'+plotted_traces[x]+'"]');
+				stat[0].classList.toggle("plotted");
+			}
+		}
+	}else console.log("Sorry! No Web Storage support..");
+	
 	
 }
 
@@ -151,83 +221,61 @@ function getTraceData(trace, showing){
 /* Refresh plot every 30 seconds with new data from db*/
 var plot_timer;
 function refresh_plot() {
-	plot_timer = setTimeout(function() {
-		if(TSPlot.showing == true){
-			/* Only refresh if plot is showing and current range is Last hour or less*/
-			if((TSPlot.layout.xaxis.range[1] - TSPlot.layout.xaxis.range[0]) <= 3600000 && TSPlot.layout.xaxis.range[1] == date){
-				date = Date.now();
-				var traces = TSPlot.traces;
-				var newdata = {
-					y: [],
-					x: []
-				};
-				var index = [];
-				var query = createQuery(traces[0].name,true)
-				index.push(0);
-				for (var i = 1; i < traces.length; i++){
-					index.push(i);
-					query += ';'
-					query += createQuery(traces[i].name,true)
+
+	if(TSPlot.showing == true){
+		/* Only refresh if plot is showing and current range is Last hour or less*/
+		if((TSPlot.layout.xaxis.range[1] - TSPlot.layout.xaxis.range[0]) <= 3600000 && TSPlot.layout.xaxis.range[1] == date){
+			date = Date.now();
+			var traces = TSPlot.traces;
+			var newdata = {
+				y: [],
+				x: []
+			};
+			var index = [];
+			var query = createQuery(traces[0].name,true)
+			index.push(0);
+			for (var i = 1; i < traces.length; i++){
+				index.push(i);
+				query += ';'
+				query += createQuery(traces[i].name,true)
+			}
+			const csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+			fetch('cloud/status/plot',{
+				method: 'POST',
+				headers: {'Accept': 'application/json', 'X-CSRFToken': csrftoken},
+				credentials: 'same-origin',
+				body: query,
 				}
-				const csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
-				fetch('cloud/status/plot',{
-					method: 'POST',
-					headers: {'Accept': 'application/json', 'X-CSRFToken': csrftoken},
-					credentials: 'same-origin',
-					body: query,
+			)
+			.then(function(response){
+				return response.json();
+			})
+			.then(function(data){
+				for(var k = 0; k < traces.length; k++){
+					const responsedata = data.results[k].series[0].values;
+					const unpackData = (arr, index) => {
+						var newarr = arr.map(x => x[index]);
+						return newarr
 					}
-				)
-				.then(function(response){
-					return response.json();
-				})
-				.then(function(data){
-					for(var k = 0; k < traces.length; k++){
-						const responsedata = data.results[k].series[0].values;
-						const unpackData = (arr, index) => {
-							var newarr = arr.map(x => x[index]);
-							return newarr
-						}
-						var updatetrace = {
-							x: unpackData(responsedata, 0),
-							y: unpackData(responsedata, 1)
-						}
-					
-						/* Update trace*/
-						newdata.y.push(unpackData(responsedata, 1));
-						newdata.x.push(unpackData(responsedata, 0));
+					var updatetrace = {
+						x: unpackData(responsedata, 0),
+						y: unpackData(responsedata, 1)
 					}
-					/* Update plot range*/
-					return updateTraces(newdata, index);
-				})
-				.catch(error => console.log('Error:', error));
-			/*
-				fetch(location.href,{	
-					method: 'GET',
-					headers: {'Accept': 'text/html'},
-				})	
-		    		.then((response) => response.text())
-		    		.then((html) => {
-					doc = new DOMParser().parseFromString(html, "text/html");
-					//console.log(document.getElementById("status"));
-					//console.log(html);
-					var update = doc.querySelector("#status").innerHTML;
-					console.log(update);
-					document.getElementById("status").innerHTML = update;
-					addEventListeners("plottable");
-					addEventListeners("range");
-		    		})
-		    		.catch((error) => {
-						console.warn(error);
-					});*/
-			}					
-			
-		}
-		refresh_plot();
-	},30000);
-	
+				
+					/* Update trace*/
+					newdata.y.push(unpackData(responsedata, 1));
+					newdata.x.push(unpackData(responsedata, 0));
+				}
+				/* Update plot range*/
+				return updateTraces(newdata, index);
+			})
+			.catch(error => console.log('Error:', error));
+		}					
+		
+	}
 }
 
-/* Update plot traces with new data*/
+/* Update plot traces with most recent data points*/
 function updateTraces(newdata, index){
 	Plotly.extendTraces('plotly-TS', newdata, index);
 	TSPlot.layout.xaxis.range[1] = date; 
@@ -263,10 +311,11 @@ var TSPlot = {
 		TSPlot.layout.xaxis.range = [from, to];
 		TSPlot.traces = [trace];		
 		Plotly.newPlot('plotly-TS', TSPlot.traces, TSPlot.layout, {responsive: true, displayModeBar: false});
-		refresh_plot();	
+		//refresh_plot();
+		sessionStorage.setItem("traces", JSON.stringify(trace.name));
 	},
 
-	/* Hide plot and start timer*/
+	/* Hide plot*/
 	hide: function() {
 		TSPlot.showing = false;
 		TSPlot.traces = [];
@@ -274,21 +323,19 @@ var TSPlot = {
 		document.getElementById("plot").style.display = 'none';
 		const curr_range = document.getElementsByClassName("range-btn");
 		curr_range[0].innerHTML = 'Last 1 hour<span class="space"></span><span class="caret"></span>';
-		set_refresh(1000*document.getElementById("CDTimer").innerHTML);
-		clearTimeout(plot_timer);
 		var list = document.getElementsByClassName('plotted');
-		for (i = 0; i < list.length; i++) {
-			var value = list[i];
+		var init_length = list.length;
+		for (var i = 0; i < init_length; i++) {
+			var value = list[0];
 			value.classList.remove('plotted');
-    		}
+		}
+		sessionStorage.clear();
 	},
 
-	/* Show plot and pause timer*/
+	/* Show plot*/
 	show: function(){
 		TSPlot.showing = true;
 		document.getElementById("plot").style.display = 'block';
-		clearTimeout(timer_id);
-		stop_refresh();
 	}
 
 }//TSPlot
