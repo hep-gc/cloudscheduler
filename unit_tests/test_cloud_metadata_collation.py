ae4fba94275e427cfc3ad0da944dcd18678054fe
Colin Leavett-Brown <crlb@uvic.ca>
2019-02-19 10:59:59 -0800

    number the tests

--- unit_tests/test_cloud_metadata_collation.py
+++ unit_tests/test_cloud_metadata_collation.py
@@ -11,42 +11,49 @@ def main(gvar, user_secret):
         else:
             initialize_csv2_request(gvar, argv[0])
     
+    # 01
     execute_csv2_request(
         gvar, 2, None, 'HTTP response code 401, unauthorized.',
         '/cloud/metadata-collation/',
         server_user='invalid-unit-test', server_pw=user_secret
     )
 
+    # 02
     execute_csv2_request(
         gvar, 1, None, 'user "{}" is not a member of any group.'.format(ut_id(gvar, 'ctu1')),
         '/cloud/metadata-collation/',
         server_user=ut_id(gvar, 'ctu1'), server_pw=user_secret
     )
 
+    # 03
     execute_csv2_request(
         gvar, 1, None, 'user "{}" is not a member of any group.'.format(ut_id(gvar, 'ctu2')),
         '/cloud/metadata-collation/',
         server_user=ut_id(gvar, 'ctu2'), server_pw=user_secret
     )
 
+    # 04
     execute_csv2_request(
         gvar, 1, None, 'cannot switch to invalid group "invalid-unit-test".',
         '/cloud/metadata-collation/', form_data={'group': 'invalid-unit-test'},
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
+    # 05
     execute_csv2_request(
         gvar, 1, None, 'cannot switch to invalid group "{}".'.format(ut_id(gvar, 'ctg2')),
         '/cloud/metadata-collation/', form_data={'group': ut_id(gvar, 'ctg2')},
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
+    # 06
     execute_csv2_request(
         gvar, 1, 'CV19', 'request contained a bad parameter "invalid-unit-test".',
         '/cloud/metadata-collation/', form_data={'invalid-unit-test': 'invalid-unit-test'},
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
+    # 07
     execute_csv2_request(
         gvar, 0, None, None,
         '/cloud/metadata-collation/',
@@ -55,6 +62,7 @@ def main(gvar, user_secret):
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
+    # 08
     execute_csv2_request(
         gvar, 0, None, None,
         '/cloud/metadata-collation/',
@@ -63,11 +71,12 @@ def main(gvar, user_secret):
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
+    # 09
     execute_csv2_request(
         gvar, 0, None, None,
         '/cloud/metadata-collation/',
         list='cloud_metadata_list', filter={'metadata_name': ut_id(gvar, 'cty1'), 'cloud_name': ut_id(gvar, 'ctc2'), 'group_name': ut_id(gvar, 'ctg1')},
-        values={'type': 'cloud'},
+        values={'type': 'group'},
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
