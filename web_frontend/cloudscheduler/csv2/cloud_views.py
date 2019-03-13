3011fd8e091d1be5884a6652d259253885c077e1
Kevin Casteels <kcasteels@gmail.com>
2019-03-13 14:32:29 -0700

    Added version string as GET to css and js imports to force no caching

--- web_frontend/cloudscheduler/csv2/cloud_views.py
+++ web_frontend/cloudscheduler/csv2/cloud_views.py
@@ -607,8 +607,8 @@ def list(request, active_user=None, response_code=0, message=None):
             'response_code': response_code,
             'message': message,
             'enable_glint': config.enable_glint,
-            'is_superuser': active_user.is_superuser 
-
+            'is_superuser': active_user.is_superuser,
+            'version': config.get_version()
         }
 
     config.db_close()
