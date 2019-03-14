00dab8fb22642cba9f64a10d050eb4fc231739b1
root <root@csv2.heprc.uvic.ca>
2019-03-14 10:42:07 -0700

    remove start time calculation for openstack vms

--- data_collectors/openstack/openstackPoller.py
+++ data_collectors/openstack/openstackPoller.py
@@ -1244,17 +1244,6 @@ def vm_poller():
                                     ip_addrs.append(addr['addr'])
                                 elif addr['OS-EXT-IPS:type'] == 'floating':
                                     floating_ips.append(addr['addr'])
-                        strt_time = vm.__dict__["OS-SRV-USG:launched_at"]
-                        try:
-                            from_zone = tz.gettz('UTC')
-                            to_zone = tz.gettz('America/Vancouver')
-                            dt_strt_time = datetime.datetime.strptime(strt_time, '%Y-%m-%dT%H:%M:%S.%f')
-                            dt_strt_time = dt_strt_time.replace(tzinfo=from_zone)
-                            local_strt_time = dt_strt_time.astimezone(to_zone)
-                            vm_start_time = local_strt_time.strftime('%s')
-                        except:
-                            logging.info("No start time because VM still booting: %s, %s - setting start time equal to current time." % (type(strt_time), strt_time))
-                            vm_start_time = new_poll_time
                         vm_dict = {
                             'group_name': cloud.group_name,
                             'cloud_name': cloud.cloud_name,
@@ -1268,7 +1257,6 @@ def vm_poller():
                             'power_state': vm.__dict__.get("OS-EXT-STS:power_state"),
                             'vm_ips': str(ip_addrs),
                             'vm_floating_ips': str(floating_ips),
-                            'start_time': vm_start_time,
                             'last_updated': new_poll_time
                         }
 
