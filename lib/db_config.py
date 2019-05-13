bdb900b566c0a2f4c19b6ac73bde7304c31dd8b5
root <root@csv2-dev.heprc.uvic.ca>
2019-05-13 13:10:25 -0700

    remove 'Version' from version string

--- lib/db_config.py
+++ lib/db_config.py
@@ -271,9 +271,9 @@ class Config:
             if tag_ix == -1:
                 version = 'Build: %d' % len(commits)
             elif tag_ix == 0:
-                version = 'Version: %s' % tag
+                version = '%s' % tag
             else:
-                version = 'Version: %s + %d commits' % (tag, tag_ix)
+                version = ' %s + %d commits' % (tag, tag_ix)
         except Exception as exc:
             print("Error Determining version")
 
