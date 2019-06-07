a76d245049d095fc7a44b14fb4c70c3204ac8a59
root <root@csv2-dev.heprc.uvic.ca>
2019-06-07 12:34:07 -0700

    use the database to validate the configuration options

--- web_frontend/cloudscheduler/csv2/server_views.py
+++ web_frontend/cloudscheduler/csv2/server_views.py
@@ -6,90 +6,25 @@ from django.views.decorators.csrf import requires_csrf_token
 from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
 
+from cloudscheduler.lib.schema import csv2_configuration
 from cloudscheduler.lib.view_utils import \
+    cskv,  \
     lno,  \
-    manage_group_users, \
-    manage_user_group_verification, \
     qt, \
     render, \
-    set_user_groups, \
-    table_fields, \
-    validate_fields
+    set_user_groups
 
 from collections import defaultdict
 import bcrypt
 
-from sqlalchemy import exists
+from sqlalchemy import Table, MetaData
 from sqlalchemy.sql import select
-from cloudscheduler.lib.schema import *
-import sqlalchemy.exc
+#import sqlalchemy.exc
 
 from cloudscheduler.lib.web_profiler import silk_profile as silkp
 
 # lno: SV - error code identifier.
 MODID = 'SV'
-
-#-------------------------------------------------------------------------------
-
-CONFIG_KEYS = {
-    'auto_active_group': True,
-    # Named argument formats (anything else is a string).
-    'format': {
-        'batch_commit_size':                'integer',
-        'category':                         ('csv2_configuration', 'category'),
-        'delete_cycle_interval':            'integer',
-        'enable_glint':                     ['True', 'False'],
-        'enable_profiling':                 ['True', 'False'],
-        'log_level':                        'integer',
-        'max_start_vm_cloud':               'integer',
-        'no_limit_default':                 'integer',
-        'orange_threshold':                 'integer',
-        'sleep_interval_cleanup':           'integer',
-        'sleep_interval_command':           'integer',
-        'sleep_interval_flavor':            'integer',
-        'sleep_interval_image':             'integer',
-        'sleep_interval_job':               'integer',
-        'sleep_interval_keypair':           'integer',
-        'sleep_interval_limit':             'integer',
-        'sleep_interval_machine':           'integer',
-        'sleep_interval_main_long':         'integer',
-        'sleep_interval_main_short':        'integer',
-        'sleep_interval_network':           'integer',
-        'sleep_interval_status':            'integer',
-        'sleep_interval_vm':                'integer',
-        'unregistered_machine_time_limit':  'integer',
-
-        'cache_expire_time':                'integer',
-        'celery_backend':                   'string',
-        'celery_url':                       'string',
-        'cert_auth_bundle_path':            'string',
-        'defaults_sleep_interval':          'integer',
-        'image_collection_interval':        'integer',
-        #'log_file':                         'string',
-        'redis_db':                         'integer',
-        'redis_host':                       'string',
-        'redis_port':                       'integer',
-        'sleep_interval_main_long':         'integer',
-        'sleep_interval_main_short':        'integer',
-        'static_files_root':                'string',
-        'csv2_host_id':                     'integer',
-
-
-
-        'config_key':                       'reject',
-        'type':                             'reject',
-        'value':                            'reject',
-
-        'cacerts':                          'ignore',
-        'csrfmiddlewaretoken':              'ignore',
-        'default_job_group':                'ignore',
-        'group':                            'ignore',
-        'log_file':                         'ignore',
-    },
-    'mandatory': [
-        'category',
-    ],
-}
 
 #-------------------------------------------------------------------------------
 
@@ -105,46 +40,97 @@ def configuration(request):
     # Retrieve the active user, associated group list and optionally set the active group.
     rc, msg, active_user = set_user_groups(config, request)
     if rc == 0:
-        if (request.method == 'POST') and ((not 'group' in request.POST) or len(request.POST) > 2):
-                # Validate input fields.
-                rc, msg, fields, tables, columns = validate_fields(config, request, [CONFIG_KEYS], ['csv2_configuration,n'], active_user)
-                if rc == 0:
-                    # Update the server configuration.
-                    table = tables['csv2_configuration']
-                    category = fields['category']
-                    
-                    # Remove unwanted fields
-                    fields.pop('category')
-                    fields.pop('csrfmiddlewaretoken')
-                    fields.pop('group_name')
+        if request.method == 'POST':
+                if 'category' in request.POST:
+                    if 'config_key_values' in request.POST:
+                        rc, msg, key_values = cskv(request.POST['config_key_values'])
+                        category = request.POST['category']
 
-                    if len(fields) == 0:
-                        message = '{} server config must specify at least one field to update.'.format(lno(MODID))
                     else:
-                        for field in fields:
-                            rc, msg = config.db_session_execute(table.update().where((table.c.category==category) & (table.c.config_key==field)).values({table.c.config_value:fields[field]}))
-                            if rc != 0:
-                                config.db_session.rollback()
-                                message = '{} server config update failed - {}'.format(lno(MODID), msg)
-                                break
-                        if rc == 0:
-                            config.db_session.commit()
-                            message = 'server config successfully updated'
+                        rc = 0
+                        key_values = {}
+                        for key in request.POST:
+                            key_values[key] = request.POST[key]
+
+                        key_values.pop('csrfmiddlewaretoken', None)
+                        category = key_values.pop('category', None)
+
+                    if rc == 0:
+                        config_list = qt(config.db_connection.execute('select config_key,config_type,config_value from csv2_configuration where category="%s"' % category))
+                        if len(config_list) > 0:
+                            config_keys = {}
+                            for config_item in config_list:
+                                config_keys[config_item['config_key']] = {'type': config_item['config_type'], 'value': config_item['config_value']}
+
+                            message = None
+                            for key in key_values:
+                                if key in config_keys:
+                                    if config_keys[key]['type'] == 'int':
+                                        try:
+                                            ignore = int(key_values[key])
+                                        except:
+                                            message = '%s server config update failed - value specified ("%s") for category="%s", config_key="%s" must be an integer.' % (lno(MODID), key_values[key], category, key)
+                                            break
+                                           
+                                    elif config_keys[key]['type'] == 'str':
+                                        pass
+
+                                    elif config_keys[key]['type'] == 'bool':
+                                        bool = key_values[key].lower()
+                                        if bool == '0' or bool == 'no' or bool == 'off' or bool == 'false':
+                                            key_values[key] = 'False'
+                                        elif bool == '1' or bool == 'yes' or bool == 'on' or bool == 'true':
+                                            key_values[key] = 'True'
+                                        else:
+                                            message = '%s server config update failed - value specified ("%s") for category="%s", config_key="%s" must be a boolean value.' % (lno(MODID), key_values[key], category, key)
+                                            break
+
+                                    elif config_keys[key]['type'] == 'decimal' or config_keys[key]['type'] == 'float':
+                                        try:
+                                            ignore = float(key_values[key])
+                                        except:
+                                            message = '%s server config update failed - value specified ("%s") for category="%s", config_key="%s" must be a decimal or floating point number.' % (lno(MODID), key_values[key], category, key)
+                                            break
+
+                                    else:
+                                        raise Exception('unsupported config_type in csv2_configurations - category="%s", config_key="%s", config_type="%s".' % (category, key, config_keys[key]))
+
+                                else:
+                                    message = '%s server config update failed - category="%s", invalid key "%s" specified.' % (lno(MODID), category, key)
+                                    break
+
+                            if not message:
+                                keys = []
+                                table = Table('csv2_configuration', MetaData(bind=config.db_engine), autoload=True)
+                                for key in key_values:
+                                    if key_values[key] != config_keys[key]['value']:
+                                        keys.append(key)
+                                        rc, msg = config.db_session_execute(table.update().where((table.c.category==category) & (table.c.config_key==key)).values({table.c.config_value:key_values[key]}))
+                                        if rc != 0:
+                                            config.db_session.rollback()
+                                            message = '{} server config update failed - {}'.format(lno(MODID), msg)
+                                            break
+
+                                if len(keys) > 0:
+                                    config.db_session.commit()
+                                    message = 'server config update successfully updated the following keys: %s' % ', '.join(keys)
+
+                        else:
+                            message = '%s server config update failed - invalid category "%s" specified.' % (lno(MODID), category)
                 else:
-                    message = '{} server config update {}'.format(lno(MODID), msg)
+                    message = '%s server config update failed - no category specified.' % lno(MODID)
+                    
     else:
         message='{} {}'.format(lno(MODID), msg)
 
     if message and message[:2] == 'SV':
-        config_list = []
-        config_categories = []
         response_code = 1
     else:
-        s = select([csv2_configuration])
-        config_list = qt(config.db_connection.execute(s))
-        config_categories = list({v['category']:v for v in config_list})
         response_code = 0
 
+    s = select([csv2_configuration])
+    config_list = qt(config.db_connection.execute(s))
+    config_categories = list({v['category']:v for v in config_list})
 
     # Render the page.
     context = {
