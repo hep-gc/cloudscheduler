2193c30525a10dd0c37e7aec6138493a9ec5bb84
Colin Leavett-Brown <crlb@uvic.ca>
2019-07-24 13:13:38 -0700

    readthedocs bug fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,3 +56,6 @@ html_theme = 'pyramid'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Bug fix for readthedocs.io as suggested by https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
+master_doc = 'index'
