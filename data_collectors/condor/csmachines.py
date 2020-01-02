8f2a0a02018c287950ad4625237a2eae3a360eb7
root <root@csv2a.heprc.uvic.ca>
2020-01-02 10:39:36 -0800

    fix retiring bug in csv2
    The machine poller was basing its decision to retire off the 'retiring' flag (removed) in view_condor_host.
    This flag wasn't calculated using the classad activity and was giving a false positive. The logic has been
    reworked to retire VMs properly based on their slots.

--- data_collectors/condor/csmachines.py
+++ data_collectors/condor/csmachines.py
@@ -471,7 +471,7 @@ def command_poller():
 
                 # Query database for machines to be retired.
                 abort_cycle = False
-                for resource in db_session.query(view_condor_host).filter(view_condor_host.c.htcondor_fqdn==condor_host, view_condor_host.c.retire>=1, view_condor_host.c.retiring==0):
+                for resource in db_session.query(view_condor_host).filter(view_condor_host.c.htcondor_fqdn==condor_host, view_condor_host.c.retire>=1):
                     # Since we are querying a view we dont get an automapped object and instead get a 'result' tuple of the following format
                     #index=attribute
                     #0=group
@@ -482,19 +482,17 @@ def command_poller():
                     ##5primary_slots
                     ##6dynamic_slots
                     #7=retireflag
-                    #8=retiring flag
-                    #9=terminate flag
-                    #10=machine
-                    #11=updater
+                    #8=terminate flag
+                    #9=machine
+                    #10=updater
                     #logging.debug("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s" % (resource.group_name, resource.cloud_name, resource.htcondor_fqdn, resource.vmid, resource.hostname, resource[5], resource[6], resource.retire, resource.retiring, resource.terminate, resource.machine))
                     # First check the slots to see if its time to terminate this machine
 
                     #check if retire flag set & a successful retire has happened  and  (htcondor_dynamic_slots<1 || NULL) and htcondor_partitionable_slots>0, issue condor_off and increment retire by 1.
                     if resource.retire >= 1:
                         if (resource[6] is None or resource[6]<1) and (resource[5] is None or resource[5]<1):
-                        #if (resource[6] is None or resource[6]<1): # this statement skips the check from primary slot, normally this code would only execute when it never registered with condor
                             #check if terminate has already been set
-                            if not resource[9] >= 1:
+                            if not resource[8] >= 1:
                               # set terminate=1
                               # need to get vm classad because we can't update via the view.
                               try:
@@ -512,9 +510,6 @@ def command_poller():
                                   logging.error("%s ready to be terminated but unable to locate vm_row" % resource.vmid)
                                   continue
 
-                    if (resource.retire >= 2 and resource.retiring == 1):
-                        #resource has already been retired and is in retiring state, skip it
-                        continue
 
                     if config.categories["csmachines.py"]["retire_off"]:
                         logging.critical("Retires disabled, normal operation would retire %s" % resource.hostname)
