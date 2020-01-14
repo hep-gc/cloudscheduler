c7078648a8bf0418ce5064f8332d61c6b3ad98af
Garet Robertson <grobertson@heplw72.phys.uvic.ca>
2020-01-14 10:26:58 -0800

    Fix handling of user_secret in run_tests and update tests.

--- unit_tests/test_cloud_metadata_collation.py
+++ unit_tests/test_cloud_metadata_collation.py
@@ -49,7 +49,7 @@ def main(gvar, user_secret):
     # 06
     execute_csv2_request(
         gvar, 1, 'CV', 'request contained a bad parameter "invalid-unit-test".',
-        '/cloud/metadata-collation/', group=(ut_id(gvar, 'ctg1')),
+        '/cloud/metadata-collation/', group=ut_id(gvar, 'ctg1'),
         form_data={'invalid-unit-test': 'invalid-unit-test'},
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
@@ -57,7 +57,7 @@ def main(gvar, user_secret):
     # 07
     execute_csv2_request(
         gvar, 0, None, None,
-        '/cloud/metadata-collation/?{}'.format(ut_id(gvar, 'ctg1')),
+        '/cloud/metadata-collation/', group=ut_id(gvar, 'ctg1'),
         list='cloud_metadata_list', filter={'metadata_name': ut_id(gvar, 'cty1'), 'cloud_name': ut_id(gvar, 'ctc2'), 'group_name': ut_id(gvar, 'ctg1')},
         values={'type': 'group'},
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
@@ -66,7 +66,7 @@ def main(gvar, user_secret):
     # 08
     execute_csv2_request(
         gvar, 0, None, None,
-        '/cloud/metadata-collation/?{}'.format(ut_id(gvar, 'ctg1')),
+        '/cloud/metadata-collation/', group=ut_id(gvar, 'ctg1'),
         list='cloud_metadata_list', filter={'metadata_name': ut_id(gvar, 'cty1'), 'cloud_name': ut_id(gvar, 'ctc3'), 'group_name': ut_id(gvar, 'ctg1')},
         values={'type': 'group'},
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
@@ -75,7 +75,7 @@ def main(gvar, user_secret):
     # 09
     execute_csv2_request(
         gvar, 0, None, None,
-        '/cloud/metadata-collation/?{}'.format(ut_id(gvar, 'ctg1')),
+        '/cloud/metadata-collation/', group=ut_id(gvar, 'ctg1'),
         list='cloud_metadata_list', filter={'metadata_name': ut_id(gvar, 'cty1'), 'cloud_name': ut_id(gvar, 'ctc2'), 'group_name': ut_id(gvar, 'ctg1')},
         values={'type': 'group'},
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
--- unit_tests/test_cloud_metadata_list.py
+++ unit_tests/test_cloud_metadata_list.py
@@ -10,37 +10,43 @@ def main(gvar, user_secret):
             initialize_csv2_request(gvar, argv[0], selections=argv[1])
         else:
             initialize_csv2_request(gvar, argv[0])
-    
+
+    # 1
     execute_csv2_request(
         gvar, 2, None, 'HTTP response code 401, unauthorized.',
         '/cloud/metadata-list/?"{}"'.format(ut_id(gvar, 'ctg1')),
         server_user='invalid-unit-test', server_pw=user_secret
     )
 
+    # 2
     execute_csv2_request(
         gvar, 1, None, 'user "{}" is not a member of any group.'.format(ut_id(gvar, 'ctu1')),
         '/cloud/metadata-list/?"{}"'.format(ut_id(gvar, 'ctg1')),
         server_user=ut_id(gvar, 'ctu1'), server_pw=user_secret
     )
 
+    # 3
     execute_csv2_request(
         gvar, 1, None, 'user "{}" is not a member of any group.'.format(ut_id(gvar, 'ctu2')),
         '/cloud/metadata-list/?"{}"'.format(ut_id(gvar, 'ctg1')),
         server_user=ut_id(gvar, 'ctu2'), server_pw=user_secret
     )
 
+    # 4
     execute_csv2_request(
         gvar, 1, None, 'cannot switch to invalid group "invalid-unit-test".',
         '/cloud/metadata-list/?invalid-unit-test',
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
+    # 5
     execute_csv2_request(
         gvar, 1, None, 'cannot switch to invalid group "{}".'.format(ut_id(gvar, 'ctg2')),
         '/cloud/metadata-list/?{}'.format(ut_id(gvar, 'ctg2')),
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
+    # 6
     execute_csv2_request(
         gvar, 1, 'CV', 'request contained a bad parameter "metadata_list_option".',
         '/cloud/metadata-list/', group=(ut_id(gvar, 'ctg1')),
@@ -48,6 +54,7 @@ def main(gvar, user_secret):
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
+    # 7
     execute_csv2_request(
         gvar, 1, 'CV', 'request contained a bad parameter "invalid-unit-test".',
         '/cloud/metadata-list/', group=(ut_id(gvar, 'ctg1')), 
@@ -55,9 +62,10 @@ def main(gvar, user_secret):
         server_user=ut_id(gvar, 'ctu3'), server_pw=user_secret
     )
 
+    # 8
     execute_csv2_request(
         gvar, 0, None, None,
-        '/cloud/metadata-list/?{}'.format(ut_id(gvar, 'ctg1')),
+        '/cloud/metadata-list/', group=ut_id(gvar, 'ctg1'),
         list='cloud_metadata_list', 
         filter={'metadata_name': ut_id(gvar, 'cty1'), 'cloud_name': ut_id(gvar, 'ctc2'), 'group_name': ut_id(gvar, 'ctg1')},
         values={'metadata': '- example: yes', 'enabled': 1, 'mime_type': 'cloud-config', 'priority': 0},
