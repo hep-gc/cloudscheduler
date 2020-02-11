0ddf157a0b05686e9dd7a55ee31aeeeecfeaa6d2
Garet Robertson <grobertson@heplw72.phys.uvic.ca>
2020-02-11 10:01:54 -0800

    Complete creation of alias_add.

--- unit_tests/test_alias_add.py
+++ unit_tests/test_alias_add.py
@@ -12,35 +12,35 @@ def main(gvar):
         gvar['user_secret'] = generate_secret()
 
     # Bad requests.
-    # 0
+    # 01
     execute_csv2_request(
         gvar, 2, None, 'server "unit-test", HTTP response code 401, unauthorized.',
         '/alias/add/', group=ut_id(gvar, 'atg1'),
         server_user='invalid-unit-test'
     )
 
-    # 0
+    # 02
     execute_csv2_request(
         gvar, 1, None, 'user "{}" is not a member of any group.'.format(ut_id(gvar, 'atu2')),
         '/alias/add/', group=ut_id(gvar, 'atg1'),
         server_user=ut_id(gvar, 'atu2')
     )
 
-    # 0
+    # 03
     execute_csv2_request(
         gvar, 2, None, 'server "unit-test", HTTP response code 401, unauthorized.',
         '/alias/add/', group=ut_id(gvar, 'atg1'),
         server_user=ut_id(gvar, 'atu1'), server_pw='invalid-unit-test'
     )
 
-    # 0
+    # 04 Attempt to switch to a group that the user is not in.
     execute_csv2_request(
         gvar, 1, None, 'cannot switch to invalid group "{}".'.format(ut_id(gvar, 'atg2')),
         '/alias/add/', group=ut_id(gvar, 'atg2'),
         server_user=ut_id(gvar, 'atu1')
     )
 
-    # 0
+    # 05
     execute_csv2_request(
         gvar, 1, None, 'cloud alias add, invalid method "GET" specified.',
         '/alias/add/', group=ut_id(gvar, 'atg1'),
@@ -48,30 +48,33 @@ def main(gvar):
     )
 
     PARAMETERS = [
+        # 06 Omit cloud_name.
         ('cloud_name', [
-            #
-            ('', 'TODO'),
-            # 
-            (3.1, 'cloud alias add, value specified for "cloud_name" must be all lower case, numeric digits, and dashes but cannot start or end with dashes.'),
-            # 
-            ('-invalid-unit-test', 'cloud alias add, value specified for "cloud_name" must be all lower case, numeric digits, and dashes but cannot start or end with dashes.'),
-            ('cloud-name-that-is-too-long-for-the-database', 'TODO'),
-            # Name of a non-existent cloud.
+            # 07
+            ('', 'cloud alias add, value specified for "cloud_name" must not be the empty string.'),
+            # 08
+            ('3E1', 'cloud alias add, value specified for "cloud_name" must be all lowercase letters, digits, dashes, underscores, periods, and colons, and cannot contain a more than one consecutive dash or start or end with a dash.'),
+            # 09
+            ('-invalid-unit-test', 'cloud alias add, value specified for "cloud_name" must be all lowercase letters, digits, dashes, underscores, periods, and colons, and cannot contain a more than one consecutive dash or start or end with a dash.'),
+            # 10 Name of a non-existent cloud.
             ('invalid-unit-test', 'cloud alias add, "invalid-unit-test" failed - specified value in list of values does not exist: cloud_name=invalid-unit-test, group_name={}.'.format(ut_id(gvar, 'atg1')))
         ], ut_id(gvar, 'atc1')),
-        # 
+        # 11 Omit alias_name.
         ('alias_name', [
-            ('', 'TODO'),
-            ('\033[92m', 'cloud alias add, value specified for "alias_name" must be all lower case.')
+            # 12
+            ('', 'cloud alias add, value specified for "alias_name" must not be the empty string.'),
+            # 13
+            ('\\invalid-unit-test', 'value specified for "alias_name" must be all lowercase letters, digits, dashes, underscores, periods, and colons, and cannot contain a more than one consecutive dash or start or end with a dash.'),
+            # 14
+            ('alias-name-that-is-too-long-for-the-database', 'Data too long for column \'alias_name\'')
         ], 'invalid-unit-test'),
-        # 
-        ('invalid_unit_test', [('invalid-unit-test', 'cloud alias add, request contained a bad parameter "invalid_unit_test".')])
+        # 15
+        ('invalid-unit-test', [('invalid-unit-test', 'cloud alias add, request contained a bad parameter "invalid-unit-test".')])
     ]
 
     parameters_requests(gvar, '/alias/add/', ut_id(gvar, 'atg1'), ut_id(gvar, 'atu1'), PARAMETERS)
     
-    # Create an alias properly.
-    #
+    # 16 Create an alias properly.
     execute_csv2_request(
         gvar, 0, None, 'cloud alias "{}.{}" successfully added.'.format(ut_id(gvar, 'atg1'), ut_id(gvar, 'atc1a2')),
         '/alias/add/', group=ut_id(gvar, 'atg1'), form_data={
@@ -81,8 +84,7 @@ def main(gvar):
         server_user=ut_id(gvar, 'atu1')
     )
 
-    # Attempt to create an alias with a name that is taken.
-    #
+    # 17 Attempt to create an alias with a name that is taken.
     execute_csv2_request(
         gvar, 1, None, 'cloud alias add "{}.{}" failed - specified alias already exists.'.format(ut_id(gvar, 'atg1'), ut_id(gvar, 'atc1a2')),
         '/alias/add/', group=ut_id(gvar, 'atg1'), form_data={
