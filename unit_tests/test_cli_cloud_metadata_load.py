6a2300a01524eec3d573119be38c9bb3c38fa195
Garet Robertson <15016851+garetethan@users.noreply.github.com>
2020-03-06 18:23:03 -0800

    [WIP] Continue improving cli_cloud_metadata_load.

--- unit_tests/test_cli_cloud_metadata_load.py
+++ unit_tests/test_cli_cloud_metadata_load.py
@@ -1,4 +1,4 @@
-from unit_test_common import execute_csv2_command, initialize_csv2_request, ut_id, sanity_commands
+from unit_test_common import execute_csv2_command, initialize_csv2_request, ut_id, sanity_commands, parameters_commands
 from sys import argv
 
 # lno: CV - error code identifier.
@@ -12,64 +12,71 @@ def main(gvar):
             initialize_csv2_request(gvar)
 
     METADATA_FILEPATH = 'ut.yaml'
+    INVALID_YAML_FILEPATH = 'notyamlfile.txt'
 
     # 01 - 14
     sanity_commands(gvar, 'cloud', 'metadata-load')
 
     parameters = {
-        '--cloud_name': {'valid': ut_id(gvar, 'clc2'), 'test_cases': {
-            '': 'TODO',
+        # 15 Omit --cloud-name.
+        '--cloud-name': {'valid': ut_id(gvar, 'clc2'), 'test_cases': {
+            # 16
+            '': 'cloud metadata-add value specified for "cloud_name" must not be the empty string.',
+            # 17
             'invalid-unit-test': 'cloud name  "invalid-unit-test" does not exist.'
         }, 'mandatory': True},
+        # 18 Omit --metadata-name.
         '--metadata-name': {'valid': ut_id(gvar, 'clm2'), 'test_cases': {
-            '': 'TODO',
-            'invalid-unit-Test': 'TODO',
-            'invalid-unit-test-': 'TODO',
-            'invalid-unit-test!': 'TODO',
-            'metadata-name-that-is-too-long-for-the-database': 'TODO'
+            # 19
+            '': 'cloud metadata-add value specified for "metadata_name" must not be the empty string.',
+            # 20
+            'invalid-unit-Test': 'cloud metadata-add value specified for "metadata_name" must be all lowercase letters, digits, dashes, underscores, periods, and colons, and cannot contain more than one consecutive dash or start or end with a dash.',
+            # 21
+            'invalid-unit-test-': 'cloud metadata-add value specified for "metadata_name" must be all lowercase letters, digits, dashes, underscores, periods, and colons, and cannot contain more than one consecutive dash or start or end with a dash.',
+            # 22
+            'invalid-unit-test!': 'cloud metadata-add value specified for "metadata_name" must be all lowercase letters, digits, dashes, underscores, periods, and colons, and cannot contain more than one consecutive dash or start or end with a dash.',
+            # 23
+            'metadata-name-that-is-too-long-for-the-database_______________________': 'Data too long for column \'metadata_name\''
         }, 'mandatory': True},
+        # 24 Omit --file-path.
         '--file-path': {'valid': METADATA_FILEPATH, 'test_cases': {
-            '': 'TODO',
+            # 25
+            '': 'The specified metadata file "" does not exist.',
+            # 26
             'invalid-unit-test': 'The specified metadata file "invalid-unit-test" does not exist.'
         }, 'mandatory': True},
-        '--metadata-enabled': {'valid': 0, 'test_cases': {'invalid-unit-test': 'boolean value specified for "enabled" must be one of the following: true, false, yes, no, 1, or 0.'}}
-        '--metadata-mime-type': {'valid': 'cloud-config': {'invalid-unit-test': 'value specified for "mime_type" must be one of the following options: [\'cloud-config\', \'ucernvm-config\'].'}}
-        '--priority': {'valid': 0, 'test_cases': {'invalid-unit-test': 'value specified for "priority" must be an integer value.'}}
+        # 27
+        '--metadata-enabled': {'valid': 0, 'test_cases': {'invalid-unit-test': 'boolean value specified for "enabled" must be one of the following: true, false, yes, no, 1, or 0.'}},
+        # 28
+        '--metadata-mime-type': {'valid': 'cloud-config', 'test_cases': {'invalid-unit-test': 'value specified for "mime_type" must be one of the following options: [\'cloud-config\', \'ucernvm-config\'].'}},
+        # 29
+        '--metadata-priority': {'valid': 0, 'test_cases': {'invalid-unit-test': 'value specified for "priority" must be an integer value.'}}
+    }
 
-    # 01
-    execute_csv2_command(
-        gvar, 1, None, 'the following mandatory parameters must be specfied on the command line',
-        ['cloud', 'metadata-load', '-g', ut_id(gvar, 'clg1'), '-su', ut_id(gvar, 'clu4')]
-    )
+    parameters_commands(gvar, 'cloud', 'metadata-load', ut_id(gvar, 'clg1'), ut_id(gvar, 'clu3'), parameters)
 
-    # 03
+    # 30
     execute_csv2_command(
         gvar, 1, None, 'The following command line arguments were invalid: only-keys',
-        ['cloud', 'metadata-load', '-ok', '-su', ut_id(gvar, 'clu4')]
+        ['cloud', 'metadata-load', '-ok', '-su', ut_id(gvar, 'clu3')]
     )
 
-    # 16
+    # 31 Attempt to give text not parsable as YAML as YAML metadata.
     execute_csv2_command(
         gvar, 1, 'CV', 'value specified for "metadata (metadata_name)" is invalid - scanner error',
-        ['cloud', 'metadata-load', '-cn', ut_id(gvar, 'clc2'), '-f', 'notyamlfile.txt', '-mn', 'invalid-unit-test.yaml', '-su', ut_id(gvar, 'clu4')]
+        ['cloud', 'metadata-load', '-cn', ut_id(gvar, 'clc2'), '-f', INVALID_YAML_FILEPATH, '-mn', 'invalid-unit-test.yaml', '-su', ut_id(gvar, 'clu3')]
     )
 
-    # 17
+    # 32 Properly add non-YAML metadata.
     execute_csv2_command(
         gvar, 0, None, 'file "{}::{}::{}" successfully added.'.format(ut_id(gvar, 'clg1'), ut_id(gvar, 'clc2'), ut_id(gvar, 'clm10')),
-        ['cloud', 'metadata-load', '-cn', ut_id(gvar, 'clc2'), '-f', 'notyamlfile.txt', '-mn', ut_id(gvar, 'clm10'), '-su', ut_id(gvar, 'clu4')]
+        ['cloud', 'metadata-load', '-cn', ut_id(gvar, 'clc2'), '-f', INVALID_YAML_FILEPATH, '-mn', ut_id(gvar, 'clm10'), '-su', ut_id(gvar, 'clu3')]
     )
 
-    # 18
+    # 33 Properly add YAML metadata.
     execute_csv2_command(
         gvar, 0, None, 'file "{}::{}::{}" successfully added.'.format(ut_id(gvar, 'clg1'), ut_id(gvar, 'clc2'), ut_id(gvar, 'clm10.yaml')),
-        ['cloud', 'metadata-load', '-cn', ut_id(gvar, 'clc2'), '-f', 'ut.yaml', '-mn', ut_id(gvar, 'clm10.yaml'), '-su', ut_id(gvar, 'clu4')]
-    )
-
-    # 19
-    execute_csv2_command(
-        gvar, 0, None, 'file "{}::{}::{}" successfully added.'.format(ut_id(gvar, 'clg1'), ut_id(gvar, 'clc2'), ut_id(gvar, 'clm11')),
-        ['cloud', 'metadata-load', '-cn', ut_id(gvar, 'clc2'), '-f', 'notyamlfile.txt', '-mn', ut_id(gvar, 'clm11'), '-g', ut_id(gvar, 'clg1'), '-su', ut_id(gvar, 'clu4')]
+        ['cloud', 'metadata-load', '-cn', ut_id(gvar, 'clc2'), '-f', 'ut.yaml', '-mn', ut_id(gvar, 'clm10.yaml'), '-su', ut_id(gvar, 'clu3')]
     )
 
 if __name__ == "__main__":
