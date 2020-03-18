94b1dd8838602e9d727678cba20e8e00c5566dbb
garetethan <15016851+garetethan@users.noreply.github.com>
2020-03-18 16:57:32 -0700

    Start creating web_frontend tests.

---
+++ web_frontend/unit_tests/test_status.py
@@ -0,0 +1,26 @@
+import common
+import unittest
+import selenium
+
+JOB_TABLE_HEADERS = ['Group', 'Jobs', 'Idle', 'Running', 'Completed', 'Held', 'Other', 'Foreign', 'Condor FQDN', 'Condor Status', 'Agent Status', 'Condor Cert', 'Worker Cert']
+
+VM_TABLE_HEADERS = ['Group', 'Clouds', 'RT (μs)', 'VMs', 'Starting', 'Unreg.', 'Idle', 'Running', 'Retiring', 'Manual', 'Error', 'Slots', 'Busy', 'Idle', 'Used', 'Limit', 'RAM']
+
+class TestStatus(unittest.TestCase):
+    def setUpClass():
+        self.server_address = common.setup()
+
+    def setUp(self):
+        self.driver = selenium.webdriver.Firefox()
+        self.driver.get('{}/cloud/status'.format(self.server_address))
+
+    def test_status_display(self):
+        status_tables = self.driver.find_elements_by_class_name('status-tables')
+        system_services = self.driver.find_element_by_id_name('system-services')
+        self.assertEqual(len(status_tables), 2)
+        job_table, vm_table = status_tables
+        job_headers = [elem.get_attribute('innerHTML') for elem in job_table.find_elements_by_tag_name('th')]
+        self.assertEqual(job_headers, JOB_TABLE_HEADERS)
+
+    def tearDown(self):
+        self.driver.close()
