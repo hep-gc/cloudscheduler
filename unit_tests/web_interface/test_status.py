303675586f6f3346483b53bb5719fe90a652231d
garetethan <15016851+garetethan@users.noreply.github.com>
2020-03-20 10:19:19 -0700

    Continue creating web interface tests.

--- unit_tests/web_interface/test_status.py
+++ unit_tests/web_interface/test_status.py
@@ -2,27 +2,37 @@ import web_common
 import unittest
 import selenium
 
-JOB_TABLE_HEADERS = ['Group', 'Jobs', 'Idle', 'Running', 'Completed', 'Held', 'Other', 'Foreign', 'Condor FQDN', 'Condor Status', 'Agent Status', 'Condor Cert', 'Worker Cert']
-
-VM_TABLE_HEADERS = ['Group', 'Clouds', 'RT (μs)', 'VMs', 'Starting', 'Unreg.', 'Idle', 'Running', 'Retiring', 'Manual', 'Error', 'Slots', 'Busy', 'Idle', 'Used', 'Limit', 'RAM']
+EXPECTED_JOB_HEADERS = ['Group', 'Jobs', 'Idle', 'Running', 'Completed', 'Held', 'Other', 'Foreign', 'Condor FQDN', 'Condor Status', 'Agent Status', 'Condor Cert', 'Worker Cert']
+EXPECTED_VM_HEADERS = ['Group', 'Clouds', 'RT (μs)', 'VMs', 'Starting', 'Unreg.', 'Idle', 'Running', 'Retiring', 'Manual', 'Error', 'Slots', 'Busy', 'Idle', 'Used', 'Limit', 'RAM']
+EXPECTED_SYSTEM_HEADERS = ['CS system services', 'CS system resources']
+EXPECTED_SYSTEM_LABELS = ['status', 'main', 'database', 'rabbitmq', 'openstack', 'jobs', 'machines', 'timeseries', 'ec2', 'watch', 'VMdata', 'Cert Info']
 
 class TestStatus(unittest.TestCase):
-    def setUpClass():
-        self.server_address = web_common.setup()['address']
-
-    def setUp(self):
-        self.driver = selenium.webdriver.Firefox()
-        self.driver.get('{}/cloud/status'.format(self.server_address))
+    @classmethod
+    def setUpClass(cls):
+        cls.server_address = web_common.setup()['address']
+        cls.driver = selenium.webdriver.Firefox()
+        cls.driver.get('{}/cloud/status'.format(self.server_address))
 
     def test_status_display(self):
         status_tables = self.driver.find_elements_by_class_name('status-tables')
-        system_services = self.driver.find_element_by_id_name('system-services')
+        system_table = self.driver.find_element_by_id_name('system-services')
         self.assertEqual(len(status_tables), 2)
         job_table, vm_table = status_tables
-        actual_job_headers = [elem.get_attribute('innerHTML') for elem in job_table.find_elements_by_tag_name('th')]
-        self.assertEqual(actual_job_headers, JOB_TABLE_HEADERS)
-        actual_vm_headers = [elem.get_attribute('innerHTML') for elem in vm_table.find_elements_by_tag_name('th')]
-        self.assertEqual(actual_vm_headers, VM_TABLE_HEADERS)
 
-    def tearDown(self):
-        self.driver.close()
+        job_headers = [elem.get_attribute('innerHTML') for elem in job_table.find_elements_by_tag_name('th')]
+        self.assertEqual(job_headers, EXPECTED_JOB_HEADERS)
+        vm_headers = [elem.get_attribute('innerHTML') for elem in vm_table.find_elements_by_tag_name('th')]
+        self.assertEqual(vm_headers, EXPECTED_VM_HEADERS)
+        system_headers = [elem.get_attribute('innerHTML') for elem in system_table.find_elements_by_tag_name('th')]
+        self.assertEqual(system_headers, EXPECTED_SYSTEM_HEADERS)
+        system_rows = system_table.find_elements_by_tag_name('tr')
+        self.assertEqual(len(system_rows), 3)
+        system_labels = [elem.get_attribute('innerHTML') for elem in system_rows[2].find_elements_by_tag_name('td')]
+        self.assertEqual(system_labels, EXPECTED_SYSTEM_LABELS)
+        refresh_buttons = self.driver.find_elements_by_class_name('refresh-button')
+        self.assertEqual(len(refresh_buttons), 1)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
--- unit_tests/web_interface/web_common.py
+++ unit_tests/web_interface/web_common.py
@@ -19,9 +19,10 @@ def setup():
 
     cleanup(gvar)
 
+    server_credentials = ['-su', '{}-wiu1'.format(gvar['user']), '-spw', gvar['user_secret']]
     # To avoid repeating all of this a few times. Only missing mandatory parameter is --cloud-name.
     cloud_template = ['cloud', 'add',
-        '-su', '{}-wig1'.format(gvar['user']), 
+        *server_credentials, 
         '-ca', gvar['cloud_credentials']['authurl'],
         '-cU', gvar['cloud_credentials']['username'],
         '-cpw', gvar['cloud_credentials']['password'],
@@ -40,7 +41,7 @@ def setup():
         # Group to be updated.
         ['group', 'add', '-gn', '{}-wig4'.format(gvar['user']), '-htcf', gvar['fqdn'],
             '--htcondor-container-hostname', 'unit-test.ca',
-            '--htcondor-other-submitters', '{}-wiu1'.format(gvar['user']),
+            '--htcondor-users', '{}-wiu1'.format(gvar['user']),
             '--job-cores', '3',
             '--job-disk', '1',
             '--job-ram', '4',
@@ -68,32 +69,33 @@ def setup():
         # Cloud to be updated.
         cloud_template + ['-cn', '{}-wic3'.format(gvar['user'])],
         # Alias that should always exist.
-        ['alias', 'add', '-su', '{}-wig1'.format(gvar['user']), '-an', '{}-wia1'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user'])],
+        ['alias', 'add', *server_credentials, '-an', '{}-wia1'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user'])],
         # Alias to be updated and deleted.
-        ['alias', 'add', '-su', '{}-wig1'.format(gvar['user']), '-an', '{}-wia2'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user'])],
+        ['alias', 'add', *server_credentials, '-an', '{}-wia2'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user'])],
         # Cloud metadata that should always exist.
-        ['cloud', 'metadata-load', '-su', '{}-wig1'.format(gvar['user']), '-mn', '{}-wicm1'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
+        ['cloud', 'metadata-load', *server_credentials, '-mn', '{}-wicm1'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
         # Cloud metadata to be deleted.
-        ['cloud', 'metadata-load', '-su', '{}-wig1'.format(gvar['user']), '-mn', '{}-wicm2'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
+        ['cloud', 'metadata-load', *server_credentials, '-mn', '{}-wicm2'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
         # Cloud metadata to be updated.
-        ['cloud', 'metadata-load', '-su', '{}-wig1'.format(gvar['user']), '-mn', '{}-wicm3'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
+        ['cloud', 'metadata-load', *server_credentials, '-mn', '{}-wicm3'.format(gvar['user']), '-cn', '{}-wic1'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
         # Group metadata that should always exist.
-        ['group', 'metadata-load', '-su', '{}-wig1'.format(gvar['user']), '-mn', '{}-wigm1'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
+        ['metadata', 'load', *server_credentials, '-mn', '{}-wigm1'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
         # Group metadata to be deleted.
-        ['group', 'metadata-load', '-su', '{}-wig1'.format(gvar['user']), '-mn', '{}-wigm2'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
+        ['metadata', 'load', *server_credentials, '-mn', '{}-wigm2'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE],
         # Group metadata to be updated.
-        ['group', 'metadata-load', '-su', '{}-wig1'.format(gvar['user']), '-mn', '{}-wigm3'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE]
+        ['metadata', 'load', *server_credentials, '-mn', '{}-wigm3'.format(gvar['user']), '-f', NON_YAML_METADATA_FILE]
     ]
 
-    if setup_required(set_to=False):
+    if setup_required():
         print('Creating test objects. Run cleanup.py later to remove them.')
         for command in setup_commands:
             try:
                 process = subprocess.run(['cloudscheduler', *command, '-s', 'unit-test'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, encoding='utf-8', errors='ignore')
+                print(' '.join(command))
             except subprocess.CalledProcessError as err:
-                raise Exception('Error setting up tests.\ncmd={}\nstderr={}\nstdout={}'.format(err.cmd, err.stderr, err.stdout))
-            print('.', end='')
+                raise Exception('Error setting up tests.\ncmd={}\nstderr={}\nstdout={}'.format(format_command(err.cmd), err.stderr, err.stdout))
 
+    setup_required(set_to=False)
     return gvar
 
 def cleanup(gvar):
@@ -105,7 +107,7 @@ def cleanup(gvar):
         process = subprocess.run(['cloudscheduler', *command, '-s', 'unit-test'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding='utf-8', errors='ignore')
         # We want to know if the server returns an unexpected HTTP status code, but not if it failed just because the object did not exist.
         if process.returncode > 1:
-            raise Exception('Error cleaning up tests.\ncmd={}\nstderr={}\nstdout={}'.format(' '.join(process.args), process.stderr, process.stdout))
+            raise Exception('Error cleaning up tests.\ncmd={}\nstderr={}\nstdout={}'.format(format_command(command), process.stderr, process.stdout))
     setup_required(set_to=True)
 
 def setup_required(set_to=None):
@@ -132,3 +134,7 @@ def setup_required(set_to=None):
             setup_required_file.write(str(int(set_to)))
 
     return required
+
+def format_command(command):
+    '''Format a list of parameters so that when the formatted string is printed it can be copy-pasted to re-run the command.'''
+    return ' '.join((word if re.fullmatch(r'\w+', word) else '\'{}\''.format(word) for word in command))
