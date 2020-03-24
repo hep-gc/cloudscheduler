f78b3bc8b76e443a658de1e8e5ca90ea89eee69e
Garet Robertson <15016851+garetethan@users.noreply.github.com>
2020-03-24 14:00:23 -0700

    Add to test_status.

--- unit_tests/web_interface/test_status.py
+++ unit_tests/web_interface/test_status.py
@@ -6,11 +6,12 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 import re
 import os.path
+from time import sleep
 import unittest
 
 EXPECTED_NAV_LINKS = [('/cloud/status/', 'Status'), ('/cloud/list/', 'Clouds'), ('/alias/list/', 'Aliases'), ('/group/defaults/', 'Defaults'), ('/images/', 'Images'), ('/keypairs/', 'Keys'), ('/user/list/', 'Users'), ('/group/list/', 'Groups'), ('/server/config/', 'Config'), ('/user/settings/', 'User Settings'), ('/settings/log-out', 'Log out')]
 EXPECTED_JOB_HEADERS = ['Group', 'Jobs', 'Idle', 'Running', 'Completed', 'Held', 'Other', '&nbsp;', 'Foreign', '&nbsp;', 'Condor FQDN', 'Condor Status', 'Agent Status', 'Condor Cert', 'Worker Cert']
-EXPECTED_VM_HEADERS = ['Group', 'Clouds', 'RT (μs)', '&nbsp;', 'VMs', 'Starting', 'Unreg.', 'Idle', 'Running', 'Retiring', 'Manual', 'Error', '&nbsp;', 'Slots', 'Busy', 'Idle', '&nbsp;', 'Used', 'Limit', 'RAM']
+EXPECTED_VM_HEADERS = ['Group', 'Clouds', 'RT (μs)', '&nbsp;', 'VMs', 'Starting', 'Unreg.', 'Idle', 'Running', 'Retiring', 'Manual', 'Error', '&nbsp;', 'Slots', 'Slot Cores', 'Busy', 'Idle', '&nbsp;', 'Native Cores', 'Used', 'Limit', 'RAM']
 EXPECTED_SYSTEM_HEADERS = ['CS system services', '&nbsp;', 'CS system resources']
 EXPECTED_SYSTEM_LABELS = ['status', 'main', 'database', 'rabbitmq', 'openstack', 'jobs', 'machines', 'timeseries', 'ec2', 'watch', 'VMdata', '<nobr>Cert Info</nobr>', '&nbsp;', 'LOAD', 'RAM', 'SWAP', 'DISK']
 
@@ -28,32 +29,41 @@ class TestStatus(unittest.TestCase):
             cls.driver.quit()
 
     def test_nav(self):
-        top_navs = self.driver.find_elements_by_class_name('top-nav')
-        self.assertEqual(len(top_navs), 1)
-        nav_links = [(re.match(self.settings['address'] + r'([^?]+)', elem.get_attribute('href'))[1], elem.get_attribute('innerHTML')) for elem in top_navs[0].find_elements_by_tag_name('a')]
+        top_nav = self.driver.find_element_by_class_name('top-nav')
+        nav_links = [(re.match(self.settings['address'] + r'([^?]+)', elem.get_attribute('href'))[1], elem.get_attribute('innerHTML')) for elem in top_nav.find_elements_by_tag_name('a')]
         self.assertEqual(nav_links, EXPECTED_NAV_LINKS)
 
     def test_status_tables(self):
         status_tables = self.driver.find_elements_by_class_name('status-table')
         self.assertEqual(len(status_tables), 2)
         job_table, vm_table = status_tables
-        job_headers = [elem.get_attribute('innerHTML') for elem in job_table.find_elements_by_tag_name('th')]
+        job_headers = [header.get_attribute('innerHTML') for header in job_table.find_elements_by_tag_name('th')]
         self.assertEqual(job_headers, EXPECTED_JOB_HEADERS)
-        # vm_headers = [elem.get_attribute('innerHTML') for elem in vm_table.find_elements_by_tag_name('th')]
-        # self.assertEqual(vm_headers, EXPECTED_VM_HEADERS)
+        vm_headers = [header.get_attribute('innerHTML') for header in vm_table.find_elements_by_tag_name('th')]
+        self.assertEqual(vm_headers, EXPECTED_VM_HEADERS)
 
     def test_system_table(self):
         system_table = self.driver.find_element_by_id('system-services')
-        system_headers = [elem.get_attribute('innerHTML') for elem in system_table.find_elements_by_tag_name('th')]
+        system_headers = [header.get_attribute('innerHTML') for header in system_table.find_elements_by_tag_name('th')]
         self.assertEqual(system_headers, EXPECTED_SYSTEM_HEADERS)
         system_rows = system_table.find_elements_by_tag_name('tr')
         self.assertEqual(len(system_rows), 3)
-        system_labels = [elem.get_attribute('innerHTML') for elem in system_rows[2].find_elements_by_tag_name('td')]
+        system_labels = [cell.get_attribute('innerHTML') for cell in system_rows[2].find_elements_by_tag_name('td')]
         self.assertEqual(system_labels, EXPECTED_SYSTEM_LABELS)
+        meter_counts = [len(cell.find_elements_by_tag_name('meter')) for cell in system_rows[1].find_elements_by_tag_name('td')]
+        self.assertEqual(meter_counts, [1] * 4)
 
-    def test_refresh_button(self):
-        refresh_buttons = self.driver.find_elements_by_class_name('refresh-button')
-        self.assertEqual(len(refresh_buttons), 1)
+    def test_stop_refresh_button(self):
+        system_div = self.driver.find_element_by_class('system-div')
+        countdown = system_div.find_element_by_id('CDTimer')
+        stop_buttons = system_div.find_elements_by_class_name('stop-symbol')
+        self.assertEqual(len(stop_buttons), 1)
+        before = int(countdown.get_attribute('innerHTML'))
+        stop_buttons[0].click()
+        sleep(3)
+        after = int(countdown.get_attribute('innerHTML'))
+        # It seems to take up to about a second for the button to realize it has been pressed.
+        self.assertLessEqual(after - before, 1)
 
     @classmethod
     def tearDownClass(cls):
