456766f1849f938b80019b15f98a980e2e17656b
root <root@web-testing.novalocal>
2021-01-08 19:04:43 +0000

    making selenium import work and documenting

--- web_tests/selenium-unittest/sample-test.py
+++ web_tests/selenium-unittest/sample-test.py
@@ -5,8 +5,17 @@ class SampleTest(unittest.TestCase):
     """A class to do a few small, simple tests as a trial run for selenium and
        unittest."""
 
+    #def setUp(self):
+    #    self.driver = webdriver.Firefox()
+
     def test_first(self):
         self.assertTrue(True)
+
+    def find_element(self):
+        pass
+
+    #def tearDown(self):
+    #    self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
