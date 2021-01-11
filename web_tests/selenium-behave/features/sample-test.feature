7aa3f49d5562682c87ac39d85aad5038ba3197b5
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-01-11 18:21:40 +0000

    canary test working

--- web_tests/selenium-behave/features/sample-test.feature
+++ web_tests/selenium-behave/features/sample-test.feature
@@ -1,2 +1,25 @@
 Feature: Sample tests
     A few small, simple tests as a trial run for selenium and behave.
+
+    Scenario: We run a canary test
+        Given we have done the setup correctly
+        When we run a simple test
+        Then it will pass
+
+    Scenario: We find Google
+        Given we have our browser set up
+        When we navigate to Google
+        Then there should be a search bar
+
+    Scenario: We interact with the search bar
+        Given we have our browser set up
+        And we are on Google
+        When we try to type in the search bar
+        Then we should be successful
+
+    Scenario: We make a search
+        Given we have our browser set up
+        And we are on Google
+        When we type in the search bar
+        And search
+        Then we should be successful
--- web_tests/selenium-behave/features/steps/sample-test.py
+++ web_tests/selenium-behave/features/steps/sample-test.py
@@ -1,2 +1,54 @@
 from behave import *
-from selenium import webdriver
+#from selenium import webdriver
+
+@given('we have done the setup correctly')
+def step_impl(context):
+    pass
+
+@when('we run a simple test')
+def step_impl(context):
+    assert True is True
+
+@then('it will pass')
+def step_impl(context):
+    assert context.failed is False
+
+@given('we have our browser set up')
+def step_impl(context):
+    #TODO
+    pass
+
+@when('we navigate to Google')
+def step_impl(context):
+    #TODO
+    pass
+
+@then('there should be a search bar')
+def step_impl(context):
+    #TODO
+    assert True is True
+
+@given('we are on Google')
+def step_impl(context):
+    #TODO
+    pass
+
+@when('we try to type in the search bar')
+def step_impl(context):
+    #TODO
+    pass
+
+@then('we should be successful')
+def step_impl(context):
+    #TODO
+    assert True is True
+
+@when('we type in the search bar')
+def step_impl(context):
+    #TODO
+    pass
+
+@when('search')
+def step_impl(context):
+    #TODO
+    pass
