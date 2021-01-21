4c642836bfdea2ecbbf05591950b23f2de23da4c
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-01-21 23:37:24 +0000

    cleanup edits

--- unit_tests/test_web.py
+++ unit_tests/test_web.py
@@ -5,11 +5,11 @@ import web_tests.create_test_suite
 
 def main(gvar):
     #setup to run tests
-    #subprocess.run(["python3", "-m", "unittest", "discover", "-s", "'web_tests'"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd="/home/centos/cloudscheduler/unit_tests")
-    #unittest.main()
-    #web_tests.create_test_suite.test_suite().run(unittest.TextTestResult(sys.stdout, True, 1))
-    runner = unittest.TextTestRunner()
-    runner.run(web_tests.create_test_suite.test_suite())
+    runner = unittest.TextTestRunner(verbosity=2)
+    suite = web_tests.create_test_suite.test_suite()
+    runner.run(suite)
+    #if unittest allows surfacing error numbers, uncomment and add assignments for skip/error
+    #gvar['ut_count'][0] = suite.countTestCases()
     print()
 
 if __name__ == "__main__":
