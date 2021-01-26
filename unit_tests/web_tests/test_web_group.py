dafba9b7105fa0d9fbca44bd78fe96fc60a1bec0
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-01-26 19:32:56 +0000

    adding javascript actions module

--- unit_tests/web_tests/test_web_group.py
+++ unit_tests/web_tests/test_web_group.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import web_tests.web_test_interactions as wti
+import web_tests.web_test_javascript_interactions as wtjsi
 import web_tests.web_test_setup_cleanup as wtsc
 
 class TestWebGroup(unittest.TestCase):
@@ -18,30 +19,33 @@ class TestWebGroup(unittest.TestCase):
         wti.click_nav_button(TestWebGroup.driver, 'Groups')
 
     def test_web_group_add_checkbox(self):
+        group_name = TestWebGroup.gvar['user'] + '-wig5'
         wti.click_nav_button(TestWebGroup.driver, 'Groups')
         wti.click_nav_button(TestWebGroup.driver, '+')
-        wti.fill_blank(TestWebGroup.driver, "new_group", TestWebGroup.gvar['user'] + '-wig5')
-        self.button = TestWebGroup.driver.find_element_by_class_name("highlight2")
-        wti.javascript_click(TestWebGroup.driver, self.button)
+        wti.fill_blank(TestWebGroup.driver, "new_group", group_name)
+        # deals with Selenium bug (believed to be due to invisible <div> covering element
+        wtjsi.javascript_click_by_value(TestWebGroup.driver, TestWebGroup.gvar['user'] + '-wiu2')
         TestWebGroup.driver.find_element_by_id("new_group").submit()
         self.assertTrue(WebDriverWait(TestWebGroup.driver, 20).until(
-            EC.presence_of_element_located((By.LINK_TEXT, TestWebGroup.gvar['user'] + '-wig5'))))
+            EC.presence_of_element_located((By.LINK_TEXT, group_name))))
 
     def test_web_group_add_search_bar(self):
+        group_name = TestWebGroup.gvar['user'] + '-wig6'
         wti.click_nav_button(TestWebGroup.driver, 'Groups')
         wti.click_nav_button(TestWebGroup.driver, '+')
-        wti.fill_blank(TestWebGroup.driver, "new_group", TestWebGroup.gvar['user'] + '-wig6')
+        wti.fill_blank(TestWebGroup.driver, "new_group", group_name)
         wti.fill_blank(TestWebGroup.driver, "search-users-", TestWebGroup.gvar['user'] + '-wiu2')
         TestWebGroup.driver.find_element_by_id("new_group").submit()
         self.assertTrue(WebDriverWait(TestWebGroup.driver, 20).until(
-            EC.presence_of_element_located((By.LINK_TEXT, TestWebGroup.gvar['user'] + '-wig6'))))
+            EC.presence_of_element_located((By.LINK_TEXT, group_name))))
 
     def test_web_group_delete(self):
         group_name = TestWebGroup.gvar['user'] + '-wig4'
         wti.click_nav_button(TestWebGroup.driver, 'Groups')
-        wti.click_nav_button(TestWebGroup.driver, group_name)
-        wti.click_nav_button(TestWebGroup.driver, '-')
-        TestWebGroup.find_element_by_name(group_name).submit()
+        #wti.click_nav_button(TestWebGroup.driver, group_name)
+        wti.click_menu_button(TestWebGroup.driver, 'testing')
+        #wti.click_nav_button(TestWebGroup.driver, '-')
+        #TestWebGroup.find_element_by_name(group_name).submit()
 
     def test_web_group_user_add_search_bar(self):
         group_name = TestWebGroup.gvar['user'] + '-wig1'
@@ -57,7 +61,6 @@ class TestWebGroup(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        print("Unittest Teardown:")
         wtsc.cleanup(cls)
 
 if __name__ == "__main__":
---
+++ unit_tests/web_tests/web_test_javascript_interactions.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# IMPORTANT: This module contains functions to use JavaScript to override 
+# Selenium's view of elements being non-interactable. These functions should not
+# be used as a substitute for Selenium's default functions - the functions in 
+# web_test_interactions should always be used if possible. These functions 
+# should only be used where Selenium errors are preventing interaction with an 
+# element the user can interact with (this should be manually tested). Every use
+# of these  functions should contain a documented reason why the Selenium 
+# functions cannot be used in the comments above the use of these functions
+
+def javascript_click(driver, element):
+    # wrapper function for clicking the item via JavaScript
+    driver.execute_script("arguments[0].click();", element)
+
+def javascript_click_by_value(driver, text):
+    xpath = "//input[@value='" + text + "']"
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, xpath)))
+    box = driver.find_element_by_xpath(xpath)
+    javascript_click(driver, box)
