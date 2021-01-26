28aca63cc52ca985a71dd605cf418770b9ef04aa
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-01-26 23:51:21 +0000

    add database assertions (untested)

--- unit_tests/web_tests/test_web_group.py
+++ unit_tests/web_tests/test_web_group.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import web_tests.web_test_interactions as wti
-import web_tests.web_test_javascript_interactions as wtjsi
 import web_tests.web_test_setup_cleanup as wtsc
 
 class TestWebGroup(unittest.TestCase):
@@ -15,42 +14,38 @@ class TestWebGroup(unittest.TestCase):
         wtsc.setup(cls)
         print("\nGroup Tests:")
 
-    def setUp(self):
-        wti.get_homepage(TestWebGroup.driver)
+    def test_web_group_find(self):
         wti.click_nav_button(TestWebGroup.driver, 'Groups')
 
-    def test_web_group_find(self):
-        pass
-
     def test_web_group_add_checkbox(self):
-        group_name = TestWebGroup.gvar['user'] + '-wig5'
+        wti.click_nav_button(TestWebGroup.driver, 'Groups')
         wti.click_nav_button(TestWebGroup.driver, '+')
-        wti.fill_blank(TestWebGroup.driver, "new_group", group_name)
-        # deals with Selenium bug (believed to be due to invisible <div> covering element
-        wtjsi.javascript_click_by_value(TestWebGroup.driver, TestWebGroup.gvar['user'] + '-wiu2')
+        wti.fill_blank(TestWebGroup.driver, "new_group", TestWebGroup.gvar['user'] + '-wig5')
+        self.button = TestWebGroup.driver.find_element_by_class_name("highlight2")
+        wti.javascript_click(TestWebGroup.driver, self.button)
         TestWebGroup.driver.find_element_by_id("new_group").submit()
         self.assertTrue(WebDriverWait(TestWebGroup.driver, 20).until(
-            EC.presence_of_element_located((By.LINK_TEXT, group_name))))
+            EC.presence_of_element_located((By.LINK_TEXT, TestWebGroup.gvar['user'] + '-wig5'))))
 
     def test_web_group_add_search_bar(self):
-        group_name = TestWebGroup.gvar['user'] + '-wig6'
+        wti.click_nav_button(TestWebGroup.driver, 'Groups')
         wti.click_nav_button(TestWebGroup.driver, '+')
-        wti.fill_blank(TestWebGroup.driver, "new_group", group_name)
+        wti.fill_blank(TestWebGroup.driver, "new_group", TestWebGroup.gvar['user'] + '-wig6')
         wti.fill_blank(TestWebGroup.driver, "search-users-", TestWebGroup.gvar['user'] + '-wiu2')
         TestWebGroup.driver.find_element_by_id("new_group").submit()
         self.assertTrue(WebDriverWait(TestWebGroup.driver, 20).until(
-            EC.presence_of_element_located((By.LINK_TEXT, group_name))))
+            EC.presence_of_element_located((By.LINK_TEXT, TestWebGroup.gvar['user'] + '-wig6'))))
 
     def test_web_group_delete(self):
-        # TODO: FIX. Currently fails silently
         group_name = TestWebGroup.gvar['user'] + '-wig4'
-        # Selenium cannot click on these side buttons because the padding on theother buttons obscures them
-        wtjsi.javascript_click_menu_button(TestWebGroup.driver, group_name)
-        wti.click_nav_button(TestWebGroup.driver, '−')
-        TestWebGroup.driver.find_element_by_name(group_name).submit()
+        wti.click_nav_button(TestWebGroup.driver, 'Groups')
+        wti.click_nav_button(TestWebGroup.driver, group_name)
+        wti.click_nav_button(TestWebGroup.driver, '-')
+        TestWebGroup.find_element_by_name(group_name).submit()
 
     def test_web_group_user_add_search_bar(self):
         group_name = TestWebGroup.gvar['user'] + '-wig1'
+        wti.click_nav_button(TestWebGroup.driver, 'Groups')
         wti.click_nav_button(TestWebGroup.driver, group_name)
         wti.fill_blank(TestWebGroup.driver, 'search-users-' + group_name, TestWebGroup.gvar['user'] + '-wig2')
 
--- unit_tests/web_tests/web_test_assertions.py
+++ unit_tests/web_tests/web_test_assertions.py
@@ -1,14 +1,58 @@
 import unittest
 import subprocess
 
-def assertAdded():
-    pass
+def assertAdded(type, name):
+    list_by_type(type)
+    object_file = open('assert_objects.txt, 'r')
+    for line in object_file:
+        if line==name:
+             object_file.close()
+             return
+    object_file.close()
+    raise AssertionError
 
-def assertDeleted():
-    pass
+def assertDeleted(type, name):
+    list_by_type(type)
+    object_file = open('assert_objects.txt', 'r')
+    for line in object_file:
+        if line==name:
+             object_file.close()
+             raise AssertionError
+    object_file.close()
 
-def list(name, options):
-    pass
+def assertHasAttribute(type, name, attribute_name):
+    list_by_name(type, name)
+    object_file = open('assert_objects.txt', 'r')
+    record = ""
+    for line in object_file:
+        record += line
+    record = record.split(',')
+    if attribute_name not in record:
+        object_file.close()
+        raise AssertionError
+    object_file.close()
+
+def list_by_name(type, name):
+    object_file = None
+    try:
+        object_file = open('assert_objects.txt', 'x')
+    except FileExistsException:
+        object_file = open('assert_objects.txt', 'w')
+    object_names = names()
+    object = object_names[type]
+    subprocess.run(['cloudscheduler', object['name'], 'list', object['flag'], name, '-CSV', '""'], stdout=object_file)
+    object_file.close()
+
+def list_by_type(object):
+    object_file = None
+    try:
+        object_file = open('assert_objects.txt', 'x')
+    except FileExistsException:
+        object_file = open('assert_objects.txt', 'w')
+    object_names = names()
+    object = object_names[type]
+    subprocess.run(['cloudscheduler', object['name'], 'list', '-CSV', object['column_name'], stdout=object_file)
+    object_file.close()
 
 def names():
     alias = {
