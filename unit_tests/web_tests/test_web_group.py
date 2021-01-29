b53991918077f54fdc27f5f2f25920b0ad5b4b9a
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-01-29 19:00:29 +0000

    change class attribute references to use self

--- unit_tests/web_tests/test_web_group.py
+++ unit_tests/web_tests/test_web_group.py
@@ -19,41 +19,41 @@ class TestWebGroup(unittest.TestCase):
         print("\nGroup Tests:")
         
     def setUp(self):
-        wti.get_homepage(TestWebGroup.driver)
-        wti.click_nav_button(TestWebGroup.driver, 'Groups')
+        wti.get_homepage(self.driver)
+        wti.click_nav_button(self.driver, 'Groups')
 
     def test_web_group_find(self):
         pass
 
     def test_web_group_add_without_user(self):
-        group_name = TestWebGroup.gvar['user'] + '-wig7'
-        wti.click_nav_button(TestWebGroup.driver, '+')
-        wti.fill_blank(TestWebGroup.driver, 'new_group', group_name)
-        TestWebGroup.driver.find_element_by_id('new_group').submit()
-        self.assertTrue(WebDriverWait(TestWebGroup.driver, 20).until(
+        group_name = self.gvar['user'] + '-wig7'
+        wti.click_nav_button(self.driver, '+')
+        wti.fill_blank(self.driver, 'new_group', group_name)
+        self.driver.find_element_by_id('new_group').submit()
+        self.assertTrue(WebDriverWait(self.driver, 20).until(
             EC.presence_of_element_located((By.LINK_TEXT, group_name))))
         wta.assertAdded('group', group_name)
 
     def test_web_group_add_checkbox(self):
-        group_name = TestWebGroup.gvar['user'] + '-wig5'
-        user_name = TestWebGroup.gvar['user'] + '-wiu2'
-        wti.click_nav_button(TestWebGroup.driver, '+')
-        wti.fill_blank(TestWebGroup.driver, "new_group", group_name)
-        wti.click_by_xpath(TestWebGroup.driver, wtxs.user_checkbox('add_group', user_name))
-        TestWebGroup.driver.find_element_by_id("new_group").submit()
-        self.assertTrue(WebDriverWait(TestWebGroup.driver, 20).until(
+        group_name = self.gvar['user'] + '-wig5'
+        user_name = self.gvar['user'] + '-wiu2'
+        wti.click_nav_button(self.driver, '+')
+        wti.fill_blank(self.driver, "new_group", group_name)
+        wti.click_by_xpath(self.driver, wtxs.user_checkbox('add_group', user_name))
+        self.driver.find_element_by_id("new_group").submit()
+        self.assertTrue(WebDriverWait(self.driver, 20).until(
             EC.presence_of_element_located((By.LINK_TEXT, group_name))))
         wta.assertHasAttribute('user', user_name, 'user_groups', group_name)
 
     @unittest.skip("Not working in production")
     def test_web_group_add_search_bar(self):
-        group_name = TestWebGroup.gvar['user'] + '-wig6'
-        user_name = TestWebGroup.gvar['user'] + '-wiu2'
-        wti.click_nav_button(TestWebGroup.driver, '+')
-        wti.fill_blank(TestWebGroup.driver, "new_group", group_name)
-        wti.fill_blank(TestWebGroup.driver, "search-users-", user_name)
-        TestWebGroup.driver.find_element_by_id("new_group").submit()
-        self.assertTrue(WebDriverWait(TestWebGroup.driver, 20).until(
+        group_name = self.gvar['user'] + '-wig6'
+        user_name = self.gvar['user'] + '-wiu2'
+        wti.click_nav_button(self.driver, '+')
+        wti.fill_blank(self.driver, "new_group", group_name)
+        wti.fill_blank(self.driver, "search-users-", user_name)
+        self.driver.find_element_by_id("new_group").submit()
+        self.assertTrue(WebDriverWait(self.driver, 20).until(
             EC.presence_of_element_located((By.LINK_TEXT, group_name))))
         wta.assertHasAttribute('user', user_name, 'user_groups', group_name)
 
@@ -61,38 +61,38 @@ class TestWebGroup(unittest.TestCase):
         group_name = TestWebGroup.gvar['user'] + '-wig4'
         # JavaScript click is used here because of an issue where Selenium
         # cannot click on an item through the padding of another item.
-        wtjsi.javascript_click_nav_button(TestWebGroup.driver, group_name)
-        wti.click_nav_button(TestWebGroup.driver, '−')
-        wti.click_by_xpath(TestWebGroup.driver, wtxs.delete_button(group_name))
-        WebDriverWait(TestWebGroup.driver, 5)
+        wtjsi.javascript_click_nav_button(self.driver, group_name)
+        wti.click_nav_button(self.driver, '−')
+        wti.click_by_xpath(self.driver, wtxs.delete_button(group_name))
+        WebDriverWait(self.driver, 5)
         with self.assertRaises(TimeoutException):
-            WebDriverWait(TestWebGroup.driver, 10).until(
+            WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.LINK_TEXT, group_name)))
         wta.assertDeleted('group', group_name)
 
     @unittest.skip("Not working in production")
     def test_web_group_user_add_search_bar(self):
-        group_name = TestWebGroup.gvar['user'] + '-wig1'
-        user_name = TestWebGroup.gvar['user'] + '-wiu2'
-        wtjsi.javascript_click_nav_button(TestWebGroup.driver, group_name)
-        wti.fill_blank(TestWebGroup.driver, 'search-users-' + group_name, user_name)
-        TestWebGroup.driver.find_element_by_name(group_name).submit()
+        group_name = self.gvar['user'] + '-wig1'
+        user_name = self.gvar['user'] + '-wiu2'
+        wtjsi.javascript_click_nav_button(self.driver, group_name)
+        wti.fill_blank(self.driver, 'search-users-' + group_name, user_name)
+        self.driver.find_element_by_name(group_name).submit()
         wta.assertHasAttribute('user', user_name, 'user_groups', group_name)
 
     def test_web_group_user_add_checkbox(self):
-        group_name = TestWebGroup.gvar['user'] + '-wig2'
-        user_name = TestWebGroup.gvar['user'] + '-wiu1'
-        wtjsi.javascript_click_nav_button(TestWebGroup.driver, group_name)
-        wti.click_by_xpath(TestWebGroup.driver, wtxs.user_checkbox(group_name, user_name))
-        TestWebGroup.driver.find_element_by_name(group_name).submit()
+        group_name = self.gvar['user'] + '-wig2'
+        user_name = self.gvar['user'] + '-wiu1'
+        wtjsi.javascript_click_nav_button(self.driver, group_name)
+        wti.click_by_xpath(self.driver, wtxs.user_checkbox(group_name, user_name))
+        self.driver.find_element_by_name(group_name).submit()
         wta.assertHasAttribute('user', user_name, 'user_groups', group_name)
 
     def test_web_group_user_remove(self):
-        group_name = TestWebGroup.gvar['user'] + '-wig1'
-        user_name = TestWebGroup.gvar['user'] + '-wiu1'
-        wtjsi.javascript_click_nav_button(TestWebGroup.driver, group_name)
-        wti.click_by_xpath(TestWebGroup.driver, wtxs.user_checkbox(group_name, user_name))
-        TestWebGroup.driver.find_element_by_name(group_name).submit()
+        group_name = self.gvar['user'] + '-wig1'
+        user_name = self.gvar['user'] + '-wiu1'
+        wtjsi.javascript_click_nav_button(self.driver, group_name)
+        wti.click_by_xpath(self.driver, wtxs.user_checkbox(group_name, user_name))
+        self.driver.find_element_by_name(group_name).submit()
         wta.assertHasNotAttribute('user', user_name, 'user_groups', group_name)
         
 
