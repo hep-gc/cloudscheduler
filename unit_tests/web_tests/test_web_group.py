828c40e327477f72b981c52675cd31aa196d62ab
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-02-12 17:29:09 +0000

    add group name verification tests

--- unit_tests/web_tests/test_web_group.py
+++ unit_tests/web_tests/test_web_group.py
@@ -71,6 +71,54 @@ class TestWebGroup(unittest.TestCase):
         self.assertTrue(self.page.side_button_exists(group_name))
         wta.assertHasAttribute('user', user_name, 'user_groups', group_name)
 
+    def test_web_group_add_name_with_symbols(self):
+        # Tries to add a group with symbols in its name
+        group_name = 'inv@|id-web-te$t'
+        user_name = self.gvar['user'] + '-wiu2'
+        self.page.click_add_button()
+        self.page.type_group_name(group_name)
+        self.page.click_user_checkbox(user_name)
+        self.page.click_add_group()
+        self.assertTrue(self.page.error_message_displayed())
+        self.assertFalse(self.page.side_button_exists(group_name))
+        wta.assertNotAdded('group', group_name)
+
+    def test_web_group_add_name_with_two_dashes(self):
+        # Tries to add a group with two dashes in its name
+        group_name = 'invalid--web--test'
+        user_name = self.gvar['user'] + '-wiu2'
+        self.page.click_add_button()
+        self.page.type_group_name(group_name)
+        self.page.click_user_checkbox(user_name)
+        self.page.click_add_group()
+        self.assertTrue(self.page.error_message_displayed())
+        self.assertFalse(self.page.side_button_exists(group_name))
+        wta.assertNotAdded('group', group_name)
+
+    def test_web_group_add_name_with_uppercase(self):
+        # Tries to add a group with uppercase letters in its name
+        group_name = 'INVALID-WEB-TEST'
+        user_name = self.gvar['user'] + '-wiu2'
+        self.page.click_add_button()
+        self.page.type_group_name(group_name)
+        self.page.click_user_checkbox(user_name)
+        self.page.click_add_group()
+        self.assertTrue(self.page.error_message_displayed())
+        self.assertFalse(self.page.side_button_exists(group_name))
+        wta.assertNotAdded('group', group_name)
+
+    def test_web_group_add_name_with_starting_ending_dash(self):
+        # Tries to add a group with starting and ending dashes in its name
+        group_name = '-invalid-web-test-'
+        user_name = self.gvar['user'] + '-wiu2'
+        self.page.click_add_button()
+        self.page.type_group_name(group_name)
+        self.page.click_user_checkbox(user_name)
+        self.page.click_add_group()
+        self.assertTrue(self.page.error_message_displayed())
+        self.assertFalse(self.page.side_button_exists(group_name))
+        wta.assertNotAdded('group', group_name)
+
     def test_web_group_delete(self):
         # Deletes a group
         group_name = self.gvar['user'] + '-wig4'
