8f3523b1047044fd7b97c4d05e07c0c4f6bc6431
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-02-17 23:51:56 +0000

    set up framework for default tests

--- unit_tests/web_tests/create_test_suite.py
+++ unit_tests/web_tests/create_test_suite.py
@@ -3,6 +3,7 @@ from .test_web_group import TestWebGroup
 from .test_web_user import TestWebUser
 from .test_web_cloud import TestWebCloudSuperUser, TestWebCloudRegularUser
 from .test_web_alias import TestWebAliasSuperUser, TestWebAliasRegularUser
+from .test_web_default import TestWebDefaultSuperUser
 
 #IMPORTANT: All web tests must be added as suites here using `suite.addTest` in order for the `run_tests` script to pick them up
 def test_suite():
@@ -13,4 +14,5 @@ def test_suite():
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestWebCloudRegularUser))
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestWebAliasSuperUser))
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestWebAliasRegularUser))
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestWebDefaultSuperUser))
     return suite
