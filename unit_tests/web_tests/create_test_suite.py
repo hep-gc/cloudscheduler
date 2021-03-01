8fec42434ca6ed77b84405f8876c0d3f7d970f1f
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-03-01 19:00:14 +0000

    add regular user settings tests

--- unit_tests/web_tests/create_test_suite.py
+++ unit_tests/web_tests/create_test_suite.py
@@ -5,7 +5,7 @@ from .test_web_cloud import TestWebCloudSuperUser, TestWebCloudRegularUser
 from .test_web_alias import TestWebAliasSuperUser, TestWebAliasRegularUser
 from .test_web_default import TestWebDefaultSuperUser, TestWebDefaultRegularUser
 from .test_web_image import TestWebImageSuperUser
-from .test_web_setting import TestWebSettingSuperUser
+from .test_web_setting import TestWebSettingSuperUser, TestWebSettingRegularUser
 
 #IMPORTANT: All web tests must be added as suites here using `suite.addTest` in order for the `run_tests` script to pick them up
 def test_suite():
@@ -20,4 +20,5 @@ def test_suite():
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestWebDefaultRegularUser))
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestWebImageSuperUser))
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestWebSettingSuperUser))
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestWebSettingRegularUser))
     return suite
