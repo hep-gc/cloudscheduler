fc587252e759780e34360d34c8a0f7402fc26a17
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-04-07 23:01:19 +0000

    shell script

--- unit_tests/web_tests/web_test_setup.sh
+++ unit_tests/web_tests/web_test_setup.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/bash
 
-read -p 'Please enter your username on the server you wish to address: ' USER
+read -p 'Please enter your username on the server you wish to address: ' USERNAME
+
+sudo yum -y install epel-release
 
 PYTHON_PATH=`which python3`
 if [ -z "$PYTHON_PATH" ]; then
@@ -12,9 +14,9 @@ if [ -z "$PIP_PATH" ]; then
     sudo yum -y install pip3
 fi
 
-pip3 install selenium
+sudo pip3 install selenium
 
-pip3 install python-openstackclient
+sudo pip3 install python-openstackclient
 XDG_SESSION_TYPE=wayland
 
 WGET_PATH=`which wget`
@@ -22,70 +24,87 @@ if [ -z "$WGET_PATH" ]; then
     sudo yum -y install wget
 fi
 
-wget http://cernvm.cern.ch/releases/production/cernvm4-micro-2020.07-1.hdd -O ~/cloudscheduler/unit_tests/web_tests/misc_files/$USER-wii1.hdd
+wget http://cernvm.cern.ch/releases/production/cernvm4-micro-2020.07-1.hdd -O ~/cloudscheduler/unit_tests/web_tests/misc_files/$USERNAME-wii1.hdd
 cd ~/cloudscheduler/unit_tests/web_tests/misc_files
-cp $USER-wii1.hdd $USER-wii2.hdd
-cp $USER-wii1.hdd $USER-wii3.hdd
-cp $USER-wii1.hdd $USER-wii4.hdd
+cp $USERNAME-wii1.hdd $USERNAME-wii2.hdd
+cp $USERNAME-wii1.hdd $USERNAME-wii3.hdd
+cp $USERNAME-wii1.hdd $USERNAME-wii4.hdd
 
-ssh-keygen -f ~/cloudscheduler/unit_tests/web_tests/misc_files/$USER-wik3 -N ''
+ssh-keygen -f ~/cloudscheduler/unit_tests/web_tests/misc_files/$USERNAME-wik3 -N ''
 
 ssh-keygen -f ~/cloudscheduler/unit_tests/web_tests/misc_files/invalid-web-test -N ''
 
+cd /usr/bin
+
 read -p 'Install Firefox? [y/n]: ' FIREFOX
-if [ "$FIREFOX" ="y" ]; then
+if [ "$FIREFOX" = "y" ]; then
     FIREFOX_PATH=`which firefox`
-    if [ $FIREFOX_PATH ="\n" ]; then
+    if [ -z "$FIREFOX_PATH" ]; then
         sudo yum -y install firefox
     fi
     GECKODRIVER_PATH=`which geckodriver`
-    if [ $GECKODRIVER_PATH ="\n" ]; then
+    if [ -z "$GECKODRIVER_PATH" ]; then
         GECKODRIVER_TAG=`curl https://github.com/mozilla/geckodriver/releases | grep "<a href=\"/mozilla/geckodriver/releases/tag/v[0-9]\.[0-9][0-9]\.[0-9]\">[0-9]\.[0-9][0-9]\.[0-9]</a>" | head -1`
-        [[ "$GECKODRIVER_TAG" =~ '(<.*>)(.*)(<.*>)' ]]
+        PATTERN='(<.*>)(.*)(<.*>)'
+        [[ "$GECKODRIVER_TAG" =~ $PATTERN ]]
         GECKODRIVER_VERSION="${BASH_REMATCH[2]}"
-        wget https://github.com/mozilla/geckodriver/releases/download/v$GECKODRIVER_VERSION/geckodriver-v$GECKODRIVER_VERSION-linux64.tar.gz
-        tar xvzf ~/Downloads/geckodriver-v$GECKODRIVER_VERSION-linux64.tar.gz
+        sudo wget https://github.com/mozilla/geckodriver/releases/download/v$GECKODRIVER_VERSION/geckodriver-v$GECKODRIVER_VERSION-linux64.tar.gz
+        sudo tar xvzf /usr/bin/geckodriver-v$GECKODRIVER_VERSION-linux64.tar.gz
     fi
 fi
 
 read -p 'Install Chromium? [y/n]: ' CHROMIUM
-if [ "$CHROMIUM" ="y" ]; then
+if [ "$CHROMIUM" = "y" ]; then
     CHROMIUM_PATH=`which chromium-browser`
-    if [ $CHROMIUM_PATH ="\n" ]; then
-        sudo yum -y install chromium-browser
+    if [ -z $CHROMIUM_PATH ]; then
+        sudo yum -y install chromium.x86_64
     fi
     CHROMEDRIVER_PATH=`which chromedriver`
-    if [ $CHROMEDRIVER_PATH ="\n" ]; then
+    if [ -z "$CHROMEDRIVER_PATH" ]; then
         sudo yum -y install chromedriver
     fi
  fi
 
 read -p 'Install Opera? [y/n]: ' OPERA
-if [ "$OPERA" ="y" ]; then
+if [ "$OPERA" = "y" ]; then
     UNZIP_PATH=`which unzip`
-    if [ $UNZIP_PATH ="\n" ]; then
+    if [ -z "$UNZIP_PATH" ]; then
         sudo yum -y install unzip
     fi
     OPERA_PATH=`which opera`
-    if [ $OPERA_PATH ="\n" ]; then
+    if [ -z "$OPERA_PATH" ]; then
         sudo rpm --import https://rpm.opera.com/rpmrepo.key
         sudo yum -y install opera-stable
     fi
     OPERACHROMIUMDRIVER_PATH=`which operadriver`
     if [ $OPERACHROMIUMDRIVER_PATH ="\n" ]; then
-        wget https://github.com/operasoftware/operachromiumdriver/releases/latest/download/operadriver_linux64.zip 
+        sudo wget https://github.com/operasoftware/operachromiumdriver/releases/latest/download/operadriver_linux64.zip 
+       sudo unzip operadriver_linux64.zip
     fi
 fi
 
 read -p 'Install Chrome? [y/n]: ' CHROME
-if [ "$CHROME" ="y" ]; then
+if [ "$CHROME" = "y" ]; then
     CHROME_PATH=`which google-chrome`
-    if [ $CHROME_PATH ="\n" ]; then
+    if [ -z "$CHROME_PATH" ]; then
         wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-        sudo yum localinstall google-chrome-stable_current_x86_64.rpm
+        sudo yum -y localinstall google-chrome-stable_current_x86_64.rpm
     fi
     CHROMEDRIVER_PATH=`which chromedriver`
-    if [ $CHROMEDRIVER_PATH ="\n" ]; then
+    if [ -z "$CHROMEDRIVER_PATH" ]; then
         sudo yum install chromedriver
     fi
+fi
+
+OPENSTACK_PATH=`which openstack`
+if [ -z "$OPENSTACK_PATH" ]; then
+    sudo pip3 install python-openstackclient
+fi
+
+cd ~/cloudscheduler/unit_tests/web_tests/misc_files
+read -p 'Openstack URL: ' OPENSTACK_URL
+read -p 'Openstack username: ' OPENSTACK_USERNAME
+read -s -p 'Openstack password: ' OPENSTACK_PASSWORD
+if [ ! -e "testing-openrc.sh" ]; then
+    wget https://$OPENSTACK_USERNAME:$OPENSTACK_PASSWORD@$OPENSTACK_URL/dashboard/project/api_access/openrc/
 fi
