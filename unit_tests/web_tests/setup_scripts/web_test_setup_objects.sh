38ed66214639b1ca16ba197b08526ce983964a63
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-04-22 19:42:21 +0000

    script edits

--- unit_tests/web_tests/setup_scripts/web_test_setup_objects.sh
+++ unit_tests/web_tests/setup_scripts/web_test_setup_objects.sh
@@ -22,32 +22,11 @@ read -p 'Please enter your server username: ' server_username
 read -p 'Please enter the server you wish to address: ' server
 read -p 'Please enter the port the server is on: ' server_port
 
-sudo ssh -oStrictHostKeyChecking=no $server_username@$server -p $server_port -i $keypath "cat > job.condor <<EOF1
-Universe   = vanilla
-Executable = job.sh
-dir           = \$ENV(HOME)/logs
-# dir           = /var/tmp/apf-logs
-output        = \$\(dir\)/\$\(Cluster\).\$\(Process\).out
-error         = \$\(dir\)/\$\(Cluster\).\$\(Process\).err
-log           = \$\(dir\)/\$\(Cluster\).\$\(Process\).log
-priority       = 10
-Requirements = group_name =?= \"$username-wig0\" && TARGET.Arch == \"x86_64\"
-should_transfer_files = YES
-when_to_transfer_output = ON_EXIT
-request_cpus = 1
-request_memory = 1500
-request_disk = 20G
-RunAsOwner = False
-getenv = False
-queue 4
-EOF1
-"
+cd ~/cloudscheduler/unit_tests/web_tests/misc_files
 
-sudo ssh $server_username@$server -p $server_port -i $keypath "cat > job.sh <<EOF2
-o $HOSTNAME
-date
-cat /var/lib/cloud_type
-cat /var/lib/cloud_name
-sleep 780
-EOF2
-"
+cp job_sample.condor job.condor
+cp job_sample.sh job.sh
+sed -i "s/{user}/$username/" job.condor
+
+sudo scp -i $keypath -P $server_port job.condor $server_username@$server:~
+sudo scp -i $keypath -P $server_port job.sh $server_username@$server:~
