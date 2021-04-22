d2bf4ac0df82023406e2dae497bff3038ae389e4
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-04-22 10:22:29 -0700

    update to include second ut count

--- unit_tests/web_tests/csv2_runner.py
+++ unit_tests/web_tests/csv2_runner.py
@@ -23,33 +23,40 @@ class Csv2TestResult(unittest.TextTestResult):
     def addError(self, test, err):
         super(Csv2TestResult, self).addError(test, err)
         self.gvar['ut_count'][0] += 1
+        self.gvar['ut_count'][1] += 1
         self.gvar['ut_failed'] += 1
 
     def addFailure(self, test, err):
         super(Csv2TestResult, self).addFailure(test, err)
         self.gvar['ut_count'][0] += 1
+        self.gvar['ut_count'][1] += 1
         self.gvar['ut_failed'] += 1
 
     def addSuccess(self, test):
         super(Csv2TestResult, self).addSuccess(test)
         self.gvar['ut_count'][0] += 1
+        self.gvar['ut_count'][1] += 1
 
     def addSkip(self, test, reason):
         super(Csv2TestResult, self).addSkip(test, reason)
         self.gvar['ut_count'][0] += 1
+        self.gvar['ut_count'][1] += 1
         self.gvar['ut_skipped'] += 1
 
     def addExpectedFailure(self, test, err):
         super(Csv2TestResult, self).addExpectedFailure(test, err)
         self.gvar['ut_count'][0] += 1
+        self.gvar['ut_count'][1] += 1
 
     def addUnexpectedSuccess(self, test):
         super(Csv2TestResult, self).addUnexpectedSuccess(test)
         self.gvar['ut_count'][0] += 1
+        self.gvar['ut_count'][1] += 1
         self.gvar['ut_failed'] += 1
 
     def addSubTest(self, test, subtest, outcome):
         super(Csv2TestResult, self).addSubTest(test, subtest, outcome)
         self.gvar['ut_count'][0] += 1
+        self.gvar['ut_count'][1] += 1
         if outcome:
             self.gvar['ut_failed'] += 1
