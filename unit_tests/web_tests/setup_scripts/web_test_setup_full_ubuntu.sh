7c3e0ecc8fbe732e605e8e553cba0dedc4981837
Elisabeth Klassen <elisabethklassen@uvic.ca>
2021-04-28 17:34:32 +0000

    more script bug fixes

--- unit_tests/web_tests/setup_scripts/web_test_setup_full_ubuntu.sh
+++ unit_tests/web_tests/setup_scripts/web_test_setup_full_ubuntu.sh
@@ -112,6 +112,10 @@ if [ -z "$openstack_path" ]; then
     pip3 install python-openstackclient
 fi
 
+sudo ln -s /home/centos/cloudscheduler/cli/bin/cloudscheduler cloudscheduler
+echo 'Please save the following server settings as "unit-test":'
+cloudscheduler defaults set
+
 read -p 'Please enter the path to a private ssh key with no password that allows you to ssh onto the server (ex. /home/ubuntu/.ssh/web_testing): ' keypath
 read -p 'Please enter your server username (ex. sampleuser): ' server_username
 read -p 'Please enter the server you wish to address (ex. csv2-dev.heprc.uvic.ca: ' server
