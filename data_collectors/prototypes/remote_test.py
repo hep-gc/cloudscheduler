079d3fca47d56555ca43df6361e59ab945aa865a
root <root@csv2-dev.heprc.uvic.ca>
2022-08-24 12:04:13 -0700

    Clean up old and useless files

--- data_collectors/prototypes/remote_test.py
+++
@@ -1,8 +0,0 @@
-import htcondor
-import classad
-
-coll = htcondor.Collector("csv2-dev.heprc.uvic.ca")
-scheddAd = coll.locate(htcondor.DaemonTypes.Schedd, "csv2-dev.heprc.uvic.ca")
-schedd = htcondor.Schedd(scheddAd)
-jobs = schedd.xquery()
-print(jobs)
--- web_tests/selenium-behave/features/environment.py
+++
@@ -1,7 +0,0 @@
-from behave import fixture, use_fixture
-from selenium import webdriver
-
-@fixture
-def after_scenario(context, scenario):
-    if hasattr(context, 'driver'):
-        context.driver.quit()
