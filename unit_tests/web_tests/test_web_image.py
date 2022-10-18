2c09bd409f4d0d1238e06f9af0a98d6dae8c34fc
cindy <wuxinxin801@gmail.com>
2022-10-18 14:52:09 -0700

    comment delete part in test upload url in test_web_image.py

--- unit_tests/web_tests/test_web_image.py
+++ unit_tests/web_tests/test_web_image.py
@@ -187,6 +187,7 @@ class TestWebImageCommon(unittest.TestCase):
         wta.assertExists('image', image_name, group=self.gvar['base_group'], image_cloud=cloud_name)
         print("\ntest-os-image-raw.hdd.reorganized.compressed.qcow2 file successfully uploaded")
 
+        '''
         # delete web image upload file_name ends with .reorganized.compressed.qcow2-------------------------
         self.page.click_top_nav('Images')
         image_name = 'test-os-image-raw.hdd.reorganized.compressed.qcow2'
@@ -195,6 +196,7 @@ class TestWebImageCommon(unittest.TestCase):
             self.page.click_cloud_button(image_name, cloud_name)
             self.page.click_delete_ok()
         print("test-os-image-raw.hdd.reorganized.compressed.qcow2 file successfully deleted")
+        '''
     # Cindy test the checkboxes-------------------------------------------------
 
 
