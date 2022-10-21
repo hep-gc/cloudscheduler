9372c0e945234b760363ffd4e2d77b228851d9dd
cindy <wuxinxin801@gmail.com>
2022-10-21 15:17:51 -0700

    test in glint_views.py

--- web_frontend/cloudscheduler/glintwebui/glint_views.py
+++ web_frontend/cloudscheduler/glintwebui/glint_views.py
@@ -843,6 +843,7 @@ def upload(request, group_name=None):
         virt_sparsify = bool(request.POST.get('operation1'))
         with_compression = bool(request.POST.get('operation2'))
 
+        '''
         if virt_sparsify or with_compression:
             file_path = convert_sparsify_compress(file_path, virt_sparsify, with_compression)
             if virt_sparsify:
@@ -850,12 +851,17 @@ def upload(request, group_name=None):
             if with_compression:
                 image_file.name += '.compressed'
             image_file.name += '.qcow2'
+        '''
+        if virt_sparsify:
+            image_file.name += '.reT'
+        if with_compression:
+            image_file.name += '.cmT'
 
         if not virt_sparsify:
-            image_file.name += '.re'
+            image_file.name += '.reF'
 
         if not with_compression:
-            image_file.name += '.cm'
+            image_file.name += '.cmF'
 
         '''
         if virt_sparsify or with_compression:
@@ -1160,7 +1166,7 @@ def upload(request, group_name=None):
             if with_compression:
                 image_name += '.compressed'
             image_name += '.qcow2'
-        '''
+        
 
         virt_sparsify = bool(request.POST.get('operation1'))
         with_compression = bool(request.POST.get('operation2'))
@@ -1168,6 +1174,7 @@ def upload(request, group_name=None):
         if virt_sparsify or with_compression:
             file_path, added_image_name = convert_sparsify_compress(file_path, virt_sparsify, with_compression)
             image_name += added_image_name
+        '''
         # added code -----------------------------------------------------
 
         disk_format = request.POST.get('disk_format')
