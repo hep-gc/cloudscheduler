4367815add343086eeaa20ae47da6eab91e32cef
Travis Lee <travislee2k15@gmail.com>
2024-10-23 20:03:25 +0000

    changed where group is switched

--- web_frontend/cloudscheduler/csv2/group_views.py
+++ web_frontend/cloudscheduler/csv2/group_views.py
@@ -19,7 +19,7 @@ from cloudscheduler.lib.view_utils import \
     validate_by_filtered_table_entries, \
     validate_fields, \
     get_file_checksum, \
-    set_user_groups_delete
+    set_user_groups_on_delete
 from collections import defaultdict
 import bcrypt
 
@@ -659,15 +659,19 @@ def delete(request):
         if rc != 0:
             config.db_close()
             return group_list(request, active_user=active_user, response_code=1, message='%s group flavors delete "%s" failed - %s.' % (lno(MODID), fields['group_name'], msg))
+        
+        # Switch away from group being deleted
+        if fields['group_name'] == active_user.active_group:
+            rc, msg, active_user = set_user_groups_on_delete(config, request)
+            if rc != 0:
+                config.db_close()
+                return group_list(request, active_user=active_user, response_code=1, message='%s %s' % (lno(MODID), msg))
 
         # Delete the group.
         table = 'csv2_groups'
         rc, msg = config.db_delete(table, where=where_clause)
         if rc == 0:
             # Commit the deletions, configure firewall and return.
-            if fields['group_name'] == active_user.active_group:
-                rc2, msg2, active_user = set_user_groups_delete(config, request)
-                # active_user = None
             config.db_commit()
             configure_fw(config)
             config.db_close()
