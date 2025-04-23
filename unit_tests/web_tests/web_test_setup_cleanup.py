71ada4037ad628976dadf4549722bcd172279b53
Payne Jones <paynejones01@gmail.com>
2025-04-23 15:45:06 -0700

    update web test vm image and add key

--- unit_tests/web_tests/web_test_setup_cleanup.py
+++ unit_tests/web_tests/web_test_setup_cleanup.py
@@ -243,7 +243,7 @@ def setup_objects(objects=[], browser='firefox'):
         for i in range(1, 3):
             if subprocess.run(['cloudscheduler', 'my', 'settings', '-sri', '60','-sfv', 'true', '-s', gvar['user'] + '-wis' + str(i)], stdout=subprocess.DEVNULL).returncode != 0:
                 raise SetUpException("user update failed - check the server status and try again")
-            helpers.wait_for_openstack_poller(gvar['user'] + '-wic' + str(i), ['-g', gvar['base_group'], '-vi', 'CentOS-7-x86_64-GenericCloud-1907.qcow2c', '-vn', 'private'], output=True)
+            helpers.wait_for_openstack_poller(gvar['user'] + '-wic' + str(i), ['-g', gvar['base_group'], '-vi', 'AlmaLinux9-grid', '-vn', 'private', '-vk', 'ci-server'], output=True)
 
     if 'jobs' in objects:
         server_account = gvar['server_username'] + '@' + gvar['fqdn']
@@ -331,7 +331,7 @@ def cleanup_objects(browser='firefox', interrupt=False):
         except IndexError:
             continue
         if not name == '' and not name[0] == '-' and not name == 'Name':
-            subprocess.run(['openstack', 'server', 'delete', name])
+            subprocess.run(['openstack', 'server', 'delete', name], stderr=subprocess.STDOUT)
 
     object_log.close()
    
