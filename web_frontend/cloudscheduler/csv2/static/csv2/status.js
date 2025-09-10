bfe09f80dcd96a961abc1b540fb1bc490328f7dd
Seojin Lee <seojinlee0070@gmail.com>
2025-09-10 13:23:31 -0700

    fixed issue 390

--- web_frontend/cloudscheduler/csv2/static/csv2/status.js
+++ web_frontend/cloudscheduler/csv2/static/csv2/status.js
@@ -452,10 +452,11 @@ function createPlot(to, from) {
         var current_url = window.location.href;
         const regex = /(.*\/\/[^\/]*)(\/.*)/;
         var root_url = current_url.split(regex)[1];
+	console.log("seeojjj");
+	   console.log(query);
         var newpath = root_url + ":8086/query";
-        newpath += "?q=" + query + "&db=csv2_timeseries&epoch=ms&u=csv2_read&p=csv2_public";
-        newpath = encodeURI(newpath)
-        newpath = newpath.replace(';', '%3B')
+	    newpath += "?q=" + encodeURIComponent(query) + "&db=csv2_timeseries&epoch=ms&u=csv2_read&p=csv2_public";
+	    newpath = newpath.replace(';', '%3B')
         fetch(newpath,{
             method: 'GET',
             headers: {'Accept': 'application/json', 'Content-Type':'application/json'},
