1ad2509eb9c67ac209c085844e4175250e429e13
Seojin Lee <seojinlee0070@gmail.com>
2025-10-17 15:18:31 -0700

    Replace IP address assignment with CRC32 calculation

--- lib/view_utils.py
+++ lib/view_utils.py
@@ -14,6 +14,7 @@ from cloudscheduler.lib.oracle_functions import loadOracleConfig
 
 import keystoneclient.v2_0.client as v2c
 import keystoneclient.v3.client as v3c
+from crccheck.crc import Crc32
 
 '''
 UTILITY FUNCTIONS
@@ -1411,7 +1412,7 @@ def validate_fields(config, request, fields, tables, active_user):
                         try:
                             current_hostname = socket.gethostbyname(value)
                             if len(words) > 1:
-                                Fields[words[1]] = int(ipaddress.IPv4Address(current_hostname))
+                                Fields[words[1]] = int(Crc32.calc(value.encode("utf-8")))
                         except Exception as exc:
                             return 1, 'The value specified for %s (%s) is not a valid FQDN.' % (field, value), None, None, None
 
