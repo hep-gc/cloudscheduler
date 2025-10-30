8da1904dd144d965f6f41ed5f6b74af7fc47da4d
Seojin Lee <seojinlee0070@gmail.com>
2025-10-30 12:53:07 -0700

    Fix where clause for csv2_service_catalog query

--- lib/view_utils.py
+++ lib/view_utils.py
@@ -112,7 +112,7 @@ def cleanup_stale_service_catalog(config, cleanup_timeout = None):
 
         for table, col in tables.items():
             if table == 'csv2_service_catalog':
-                where_clause = "UNIX_TIMESTAMP(last_updated) < %s and 'condor_poller.py'" % stale_time
+                where_clause = "UNIX_TIMESTAMP(last_updated) < %s and provider ='condor_poller.py'" % stale_time
             else:
                 where_clause = "UNIX_TIMESTAMP(last_updated) < %s" % stale_time
             rc, msg, stale_entries = config.db_query(table, where=where_clause)
