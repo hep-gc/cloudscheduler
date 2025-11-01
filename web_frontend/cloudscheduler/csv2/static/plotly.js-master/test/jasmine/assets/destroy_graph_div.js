bb2b3b6efebe8f66c07fe3db17ecbf70fef4a334
root <root@elephant178.heprc.uvic.ca>
2025-10-31 17:30:14 -0700

    removed files

--- web_frontend/cloudscheduler/csv2/static/plotly.js-master/test/jasmine/assets/destroy_graph_div.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function destroyGraphDiv() {
-    // remove both plain graphs and shadow DOM graph containers
-    ['graph', 'shadowcontainer'].forEach(function(id) {
-        var el = document.getElementById(id);
-        if(el) document.body.removeChild(el);
-    });
-};
